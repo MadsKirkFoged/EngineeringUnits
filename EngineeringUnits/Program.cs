a4d80f909dbaef795cf3416bd5fbd4c6a7536379
Mads Foged <jfogedm@jci.com>
2021-05-28 15:52:46 +0200

    ..

--- EngineeringUnits/Program.cs
+++ EngineeringUnits/Program.cs
@@ -10,12 +10,8 @@ namespace EngineeringUnits
         {
 
 
-            //
+            //Lav Energy færdig
 
-            //Hvordan fixer jeg at specifienergy bliver [j/kg] i stedet for [m2/s2]?
-            //Begge er rigtige enheder, den første er bare den mest brugte..
-            //Lenght is done-ish
-            //Amount is done-ish
 
 
 
