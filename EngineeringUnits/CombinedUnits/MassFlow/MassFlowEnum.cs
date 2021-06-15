9f3532093472cdedf402220812814a88c5bd1d8c
Mads Foged <jfogedm@jci.com>
2021-06-15 08:58:27 +0200

    ..

--- EngineeringUnits/CombinedUnits/MassFlow/MassFlowEnum.cs
+++ EngineeringUnits/CombinedUnits/MassFlow/MassFlowEnum.cs
@@ -14,13 +14,13 @@ namespace EngineeringUnits
     {
 
 
-        public static MassFlowUnit SI = new MassFlowUnit(MassUnit.SI, DurationUnit.SI, "");
-        public static MassFlowUnit KilogramPerSecond = new MassFlowUnit(MassUnit.Kilogram, DurationUnit.Second, "");
-        public static MassFlowUnit KilogramPerMinute = new MassFlowUnit(MassUnit.Kilogram, DurationUnit.Minute, "");
-        public static MassFlowUnit KilogramPerHour = new MassFlowUnit(MassUnit.Kilogram, DurationUnit.Hour, "");
-        public static MassFlowUnit PoundPerSecond = new MassFlowUnit(MassUnit.Pound, DurationUnit.SI, "");
-        public static MassFlowUnit PoundPerMinute = new MassFlowUnit(MassUnit.Pound, DurationUnit.Minute, "");
-        public static MassFlowUnit PoundPerHour = new MassFlowUnit(MassUnit.Pound, DurationUnit.Hour, "");
+        public static MassFlowUnit SI = new MassFlowUnit(MassUnit.SI, DurationUnit.SI);
+        public static MassFlowUnit KilogramPerSecond = new MassFlowUnit(MassUnit.Kilogram, DurationUnit.Second);
+        public static MassFlowUnit KilogramPerMinute = new MassFlowUnit(MassUnit.Kilogram, DurationUnit.Minute);
+        public static MassFlowUnit KilogramPerHour = new MassFlowUnit(MassUnit.Kilogram, DurationUnit.Hour);
+        public static MassFlowUnit PoundPerSecond = new MassFlowUnit(MassUnit.Pound, DurationUnit.SI);
+        public static MassFlowUnit PoundPerMinute = new MassFlowUnit(MassUnit.Pound, DurationUnit.Minute);
+        public static MassFlowUnit PoundPerHour = new MassFlowUnit(MassUnit.Pound, DurationUnit.Hour);
 
         public MassFlowUnit(MassUnit mass, DurationUnit duration, string NewSymbol = "", decimal correction = 1)
         {
