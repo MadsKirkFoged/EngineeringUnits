47e9e79ae760e48c09bf12df2a978f7dcae79797
Mads Foged <jfogedm@jci.com>
2021-06-28 12:34:39 +0200

    temperatura

--- EngineeringUnits/BaseUnits/Temperature/TemperatureEnum.cs
+++ EngineeringUnits/BaseUnits/Temperature/TemperatureEnum.cs
@@ -13,30 +13,17 @@ namespace EngineeringUnits
     public class TemperatureUnit : Enumeration
     {
 
-
-
-        public static TemperatureUnit Kelvin = new TemperatureUnit("k", 1,1,0);
-        public static TemperatureUnit SI = new TemperatureUnit("k", 1, 1, 0);
-        public static TemperatureUnit DegreeCelsius = new TemperatureUnit("°C", 1,1, -273.15m);
-        public static TemperatureUnit DegreeFahrenheit = new TemperatureUnit("°F", 5/9m, 1, (-273.15m*(9/5m))+ 32m);
-        public static TemperatureUnit DegreeRankine = new TemperatureUnit("°R", 5/9m, 1, 0);
+        public static TemperatureUnit Kelvin =              new TemperatureUnit("K",    1,      1,  0);
+        public static TemperatureUnit SI =                  new TemperatureUnit("K",    1,      1,  0);
+        public static TemperatureUnit DegreeCelsius =       new TemperatureUnit("°C",   1,      1,  -273.15m);
+        public static TemperatureUnit DegreeFahrenheit =    new TemperatureUnit("°F",   5/9m,   1,  (-273.15m*(9/5m))+ 32m);
+        public static TemperatureUnit DegreeRankine =       new TemperatureUnit("°R",    5/9m,  1,  0);
 
 
         public TemperatureUnit() { }
 
 
         public TemperatureUnit(string symbol, decimal a1, decimal a2, decimal b) : base(symbol, a1, a2,b)
-        {
-            SetUnitSystem();
-        }
-
-
-        public TemperatureUnit(PreFix SI, BaseUnits baseunit) : base(SI, baseunit)
-        {
-            SetUnitSystem();
-        }
-
-        public void SetUnitSystem()
         {
             Unit = new UnitSystem();
             Unit.Temperature = (TemperatureUnit)Clone();
@@ -46,10 +33,22 @@ namespace EngineeringUnits
         }
 
 
-        public static IEnumerable<TemperatureUnit> List()
+        public TemperatureUnit(PreFix SI, BaseUnits baseunit) : base(SI, baseunit)
         {
-            return new[] { DegreeCelsius, DegreeFahrenheit, DegreeRankine, Kelvin, };
+            Unit = new UnitSystem();
+            Unit.Temperature = (TemperatureUnit)Clone();
+
+            //Beta
+            Unit.UnitListBeta.Add(this);
         }
+
+     
+
+
+        //public static IEnumerable<TemperatureUnit> List()
+        //{
+        //    return new[] { DegreeCelsius, DegreeFahrenheit, DegreeRankine, Kelvin, };
+        //}
        
     }
 
--- UnitTests/ComparToUnitNet.cs
+++ UnitTests/ComparToUnitNet.cs
@@ -713,51 +713,60 @@ namespace UnitTests
 
         }
 
-        //[TestMethod]
-        //public void TemperatureCompareAutoTest()
-        //{
-        //    UnitsNet.Temperature A1 = new UnitsNet.Temperature(65.743, UnitsNet.Units.TemperatureUnit.DegreeCelsius);
-        //    EngineeringUnits.Temperature A2 = new EngineeringUnits.Temperature(65.743, EngineeringUnits.TemperatureUnit.DegreeCelsius);
 
-        //    var EU11 = EngineeringUnits.TemperatureUnit.List();
-        //    var UN11 = UnitsNet.Temperature.Units;
+        [TestMethod]
+        public void TemperatureAutoTest()
+        {
+            var A1 = new UnitsNet.Temperature(65.743, UnitsNet.Units.TemperatureUnit.DegreeCelsius);
+            var A2 = new EngineeringUnits.Temperature(65.743, EngineeringUnits.TemperatureUnit.DegreeCelsius);
 
-
-        //    int DiffCount = 0;
-
-        //    for (int i = 0; i < UnitsNet.Temperature.Units.Length; i++)
-        //    {
-
-        //        if (UnitsNet.Temperature.Units[i] == UnitsNet.Units.TemperatureUnit.DegreeDelisle ||
-        //            UnitsNet.Temperature.Units[i] == UnitsNet.Units.TemperatureUnit.DegreeReaumur ||
-        //            UnitsNet.Temperature.Units[i] == UnitsNet.Units.TemperatureUnit.DegreeRoemer ||
-        //            UnitsNet.Temperature.Units[i] == UnitsNet.Units.TemperatureUnit.MillidegreeCelsius ||
-        //            UnitsNet.Temperature.Units[i] == UnitsNet.Units.TemperatureUnit.SolarTemperature)
-        //        {
-        //            DiffCount++;
-        //            continue;
-        //        }
+            int WorkingCompares = 0;
 
 
+            foreach (var EU in Enumeration.ListOf<TemperatureUnit>())
+            {
 
-        //        //Getting Units
-        //        var EU = EngineeringUnits.TemperatureUnit.List().ToList()[i - DiffCount];
-        //        var UN = UnitsNet.Temperature.Units[i];
 
-        //        //All units absolute difference
-        //        Assert.AreEqual(0, A2.As(EU) - A1.As(UN), 0.008);
+                double Error = 1E-5;
+                double RelError = 1E-5;
 
-        //        //All units relative difference
-        //        Assert.AreEqual(0, HelperClass.Percent(A2.As(EU),
-        //                                                A1.As(UN)),
-        //                                                1E-3);
-        //        //All units symbol compare
-        //        //Assert.AreEqual(A2.ToUnit(EU).DisplaySymbol(),
-        //                       // A1.ToUnit(UN).ToString("a"));
+                var UNList = UnitsNet.Temperature.Units.Where(x => x.ToString() == EU.QuantityName);
 
-        //    }
-        //}
 
+                if (UNList.Count() == 1)
+                {
+                    var UN = UNList.Single();
+
+                    //if (UN == UnitsNet.Units.TemperatureUnit.SquareMicrometer) Error = 2629720.0009765625;
+
+
+                    Debug.Print($"");
+                    Debug.Print($"UnitsNets:       {UN} {A1.As(UN)}");
+                    Debug.Print($"EngineeringUnit: {EU.QuantityName} {A2.As(EU)}");
+                    Debug.Print($"ABS:    {A2.As(EU) - A1.As(UN):F6}");
+                    Debug.Print($"REF[%]: {HelperClass.Percent(A2.As(EU), A1.As(UN)):P6}");
+
+                    //All units absolute difference
+                    Assert.AreEqual(0, A2.As(EU) - A1.As(UN), Error);
+
+                    //All units relative difference
+                    Assert.AreEqual(0, HelperClass.Percent(A2.As(EU),
+                                                            A1.As(UN)),
+                                                            RelError);
+                    //All units symbol compare
+                    //Assert.AreEqual(A2.ToUnit(EU).DisplaySymbol(),
+                    //               A1.ToUnit(UN).ToString("a"));
+
+                    WorkingCompares++;
+
+                }
+
+            }
+
+            //Number of comparables units
+            Assert.AreEqual(4, WorkingCompares);
+
+        }
 
 
     }
