ab13d1163833815255619cf8d0bfc326a161d6e5
Mads Foged <jfogedm@jci.com>
2021-06-28 15:02:46 +0200

    Speed

--- UnitTests/ComparToUnitNetCombinedUnits.cs
+++ UnitTests/ComparToUnitNetCombinedUnits.cs
@@ -749,45 +749,101 @@ namespace UnitTests
         }
 
 
+        //[TestMethod]
+        //public void SpeedCompareAutoTest()
+        //{
+        //    UnitsNet.Speed A1 = new UnitsNet.Speed(1, UnitsNet.Units.SpeedUnit.FootPerHour);
+        //    EngineeringUnits.Speed A2 = new EngineeringUnits.Speed(1, EngineeringUnits.SpeedUnit.FootPerHour);
+
+        //    var EU11 = EngineeringUnits.SpeedUnit.List();
+        //    var UN11 = UnitsNet.Speed.Units;
+
+
+        //    int DiffCount = 0;
+
+        //    for (int i = 0; i < UnitsNet.Speed.Units.Length; i++)
+        //    {
+
+        //        //if (UnitsNet.Energy.Units[i] == UnitsNet.Units.EnergyUnit.UsSurveySquareFoot)
+        //        //{
+        //        //    DiffCount++;
+        //        //    continue;
+        //        //}
+
+
+
+        //        //Getting Units
+        //        var EU = EngineeringUnits.SpeedUnit.List().ToList()[i - DiffCount];
+        //        var UN = UnitsNet.Speed.Units[i];
+
+        //        //All units absolute difference
+        //        Assert.AreEqual(0, A2.As(EU) - A1.As(UN), 1024);
+
+        //        //All units relative difference
+        //        Assert.AreEqual(0, HelperClass.Percent(A2.As(EU),
+        //                                                A1.As(UN)),
+        //                                                1E-3);
+        //        //All units symbol compare
+        //        Assert.AreEqual(A2.ToUnit(EU).DisplaySymbol(),
+        //                        A1.ToUnit(UN).ToString("a").Replace("/min", "/m"));
+
+        //    }
+        //}
+
         [TestMethod]
-        public void SpeedCompareAutoTest()
+        public void SpeedAutoTest()
         {
-            UnitsNet.Speed A1 = new UnitsNet.Speed(1, UnitsNet.Units.SpeedUnit.FootPerHour);
-            EngineeringUnits.Speed A2 = new EngineeringUnits.Speed(1, EngineeringUnits.SpeedUnit.FootPerHour);
+            var A1 = new UnitsNet.Speed(65.743, UnitsNet.Units.SpeedUnit.FootPerHour);
+            var A2 = new EngineeringUnits.Speed(65.743, EngineeringUnits.SpeedUnit.FootPerHour);
 
-            var EU11 = EngineeringUnits.SpeedUnit.List();
-            var UN11 = UnitsNet.Speed.Units;
+            int WorkingCompares = 0;
 
 
-            int DiffCount = 0;
-
-            for (int i = 0; i < UnitsNet.Speed.Units.Length; i++)
+            foreach (var EU in Enumeration.ListOf<SpeedUnit>())
             {
 
-                //if (UnitsNet.Energy.Units[i] == UnitsNet.Units.EnergyUnit.UsSurveySquareFoot)
-                //{
-                //    DiffCount++;
-                //    continue;
-                //}
+
+                double Error = 1E-5;
+                double RelError = 1E-4;
+
+                var UNList = UnitsNet.Speed.Units.Where(x => x.ToString() == EU.QuantityName);
 
 
+                if (UNList.Count() == 1)
+                {
+                    var UN = UNList.Single();
 
-                //Getting Units
-                var EU = EngineeringUnits.SpeedUnit.List().ToList()[i - DiffCount];
-                var UN = UnitsNet.Speed.Units[i];
+                    //if (UN == UnitsNet.Units.SpeedUnit.SquareMicrometer) Error = 2629720.0009765625;
 
-                //All units absolute difference
-                Assert.AreEqual(0, A2.As(EU) - A1.As(UN), 1024);
 
-                //All units relative difference
-                Assert.AreEqual(0, HelperClass.Percent(A2.As(EU),
-                                                        A1.As(UN)),
-                                                        1E-3);
-                //All units symbol compare
-                Assert.AreEqual(A2.ToUnit(EU).DisplaySymbol(),
-                                A1.ToUnit(UN).ToString("a").Replace("/min", "/m"));
+                    Debug.Print($"");
+                    Debug.Print($"UnitsNets:       {UN} {A1.As(UN)}");
+                    Debug.Print($"EngineeringUnit: {EU.QuantityName} {A2.As(EU)}");
+                    Debug.Print($"ABS:    {A2.As(EU) - A1.As(UN):F6}");
+                    Debug.Print($"REF[%]: {HelperClass.Percent(A2.As(EU), A1.As(UN)):P6}");
+
+                    //All units absolute difference
+                    Assert.AreEqual(0, A2.As(EU) - A1.As(UN), Error);
+
+                    //All units relative difference
+                    Assert.AreEqual(0, HelperClass.Percent(A2.As(EU),
+                                                            A1.As(UN)),
+                                                            RelError);
+                    //All units symbol compare
+                    Assert.AreEqual(A2.ToUnit(EU).DisplaySymbol(),
+                                    A1.ToUnit(UN).ToString("a")
+                                    .Replace("/min", "/m")
+                                    );
+
+                    WorkingCompares++;
+
+                }
 
             }
+
+            //Number of comparables units
+            Assert.AreEqual(32, WorkingCompares);
+
         }
 
         [TestMethod]
