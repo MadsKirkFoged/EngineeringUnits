2d3bf237b71554b34a590949d1c540c75fa88ef1
Mads Foged <jfogedm@jci.com>
2021-06-28 10:39:33 +0200

    duration

--- EngineeringUnits/BaseUnits/Duration/DurationEnum.cs
+++ EngineeringUnits/BaseUnits/Duration/DurationEnum.cs
@@ -14,18 +14,18 @@ namespace EngineeringUnits
     {
 
 
-        public static DurationUnit Nanosecond = new DurationUnit(PreFix.nano, BaseUnits.time);
-        public static DurationUnit Millisecond = new DurationUnit(PreFix.milli, BaseUnits.time);
-        public static DurationUnit Microsecond = new DurationUnit(PreFix.micro, BaseUnits.time);
-        public static DurationUnit Second = new DurationUnit(PreFix.SI, BaseUnits.time);
-        public static DurationUnit SI = new DurationUnit(PreFix.SI, BaseUnits.time);
+        public static DurationUnit Nanosecond =     new DurationUnit(PreFix.nano, BaseUnits.time);
+        public static DurationUnit Millisecond =    new DurationUnit(PreFix.milli, BaseUnits.time);
+        public static DurationUnit Microsecond =    new DurationUnit(PreFix.micro, BaseUnits.time);
+        public static DurationUnit Second =         new DurationUnit(PreFix.SI, BaseUnits.time);
+        public static DurationUnit SI =             new DurationUnit(PreFix.SI, BaseUnits.time);
 
-        public static DurationUnit Minute = new DurationUnit("m", 1, 60m);
-        public static DurationUnit Hour = new DurationUnit("h", 1, 3600m);
-        public static DurationUnit Day = new DurationUnit("d", 1, (24*3600m));
-        public static DurationUnit Week = new DurationUnit("wk", 1, (7*24*3600m));
-        public static DurationUnit Month30 = new DurationUnit("mo", 1, (30 * 24 * 3600m));
-        public static DurationUnit Year365 = new DurationUnit("yr", 1, (365*24*3600m));
+        public static DurationUnit Minute =         new DurationUnit("m", 1, 60m);
+        public static DurationUnit Hour =           new DurationUnit("h", 1, 3600m);
+        public static DurationUnit Day =            new DurationUnit("d", 1, (24*3600m));
+        public static DurationUnit Week =           new DurationUnit("wk", 1, (7*24*3600m));
+        public static DurationUnit Month30 =        new DurationUnit("mo", 1, (30 * 24 * 3600m));
+        public static DurationUnit Year365 =        new DurationUnit("yr", 1, (365*24*3600m));
 
 
 
@@ -33,17 +33,6 @@ namespace EngineeringUnits
 
 
         public DurationUnit(string symbol, decimal a1, decimal a2) : base(symbol, a1, a2)
-        {
-            SetUnitSystem();
-        }
-
-
-        public DurationUnit(PreFix SI, BaseUnits baseunit) : base(SI, baseunit)
-        {
-            SetUnitSystem();
-        }
-
-        public void SetUnitSystem()
         {
             Unit = new UnitSystem();
             Unit.Duration = (DurationUnit)Clone();
@@ -54,10 +43,15 @@ namespace EngineeringUnits
         }
 
 
-        public static IEnumerable<DurationUnit> List()
+        public DurationUnit(PreFix SI, BaseUnits baseunit) : base(SI, baseunit)
         {
-            return new[] { Day, Hour, Microsecond, Millisecond, Minute, Month30, Nanosecond, Second, Week, Year365, };
-        }
+            Unit = new UnitSystem();
+            Unit.Duration = (DurationUnit)Clone();
+
+
+            //Beta
+            Unit.UnitListBeta.Add(this);
+        }      
 
     }
 
--- UnitTests/ComparToUnitNet.cs
+++ UnitTests/ComparToUnitNet.cs
@@ -271,31 +271,57 @@ namespace UnitTests
 
 
         [TestMethod]
-        public void DurationCompareAutoTest()
+        public void DurationCompDurationutoTest()
         {
+            var A1 = new UnitsNet.Duration(65.743, UnitsNet.Units.DurationUnit.Hour);
+            var A2 = new EngineeringUnits.Duration(65.743, EngineeringUnits.DurationUnit.Hour);
 
-            UnitsNet.Duration A1 = new UnitsNet.Duration(65.743, UnitsNet.Units.DurationUnit.Hour);
-            EngineeringUnits.Duration A2 = new EngineeringUnits.Duration(65.743, EngineeringUnits.DurationUnit.Hour);
+            int WorkingCompares = 0;
 
-            for (int i = 0; i < UnitsNet.Duration.Units.Length; i++)
+
+            foreach (var EU in Enumeration.ListOf<DurationUnit>())
             {
 
-                //Getting Units
-                var EU = EngineeringUnits.DurationUnit.List().ToList()[i];
-                var UN = UnitsNet.Duration.Units[i];
 
-                //All units absolute difference
-                Assert.AreEqual(0, A2.As(EU) - A1.As(UN),0.04);
+                double Error = 1E-5;
+                double RelError = 1E-5;
 
-                //All units relative difference
-                Assert.AreEqual(0, HelperClass.Percent(A2.As(EU),
-                                                        A1.As(UN)),
-                                                        1E-10);
-                //All units symbol compare
-                Assert.AreEqual(A2.ToUnit(EU).DisplaySymbol(),
-                                A1.ToUnit(UN).ToString("a"));
+                var UNList = UnitsNet.Duration.Units.Where(x => x.ToString() == EU.QuantityName);
+
+
+                if (UNList.Count() == 1)
+                {
+                    var UN = UNList.Single();
+
+                    if (UN == UnitsNet.Units.DurationUnit.Nanosecond) Error = 0.03125;
+
+
+                    //Debug.Print($"");
+                    //Debug.Print($"UnitsNets:       {UN} {A1.As(UN)}");
+                    //Debug.Print($"EngineeringUnit: {EU.QuantityName} {A2.As(EU)}");
+                    //Debug.Print($"ABS:    {A2.As(EU) - A1.As(UN):F6}");
+                    //Debug.Print($"REF[%]: {HelperClass.Percent(A2.As(EU), A1.As(UN)):P6}");
+
+                    //All units absolute difference
+                    Assert.AreEqual(0, A2.As(EU) - A1.As(UN), Error);
+
+                    //All units relative difference
+                    Assert.AreEqual(0, HelperClass.Percent(A2.As(EU),
+                                                            A1.As(UN)),
+                                                            RelError);
+                    //All units symbol compare
+                    Assert.AreEqual(A2.ToUnit(EU).DisplaySymbol(),
+                                    A1.ToUnit(UN).ToString("a"));
+
+                    WorkingCompares++;
+
+                }
 
             }
+
+            //Number of comparables units
+            Assert.AreEqual(10, WorkingCompares);
+
         }
 
 
