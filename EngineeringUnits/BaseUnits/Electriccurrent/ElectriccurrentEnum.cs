1156fef4f4aea6c96de9c1e8e82575357662e6e3
Mads Foged <jfogedm@jci.com>
2021-06-28 10:43:09 +0200

    ElectricCurrentUnit

--- EngineeringUnits/BaseUnits/Electriccurrent/ElectriccurrentEnum.cs
+++ EngineeringUnits/BaseUnits/Electriccurrent/ElectriccurrentEnum.cs
@@ -14,32 +14,21 @@ namespace EngineeringUnits
     {
 
 
-        public static ElectricCurrentUnit SI = new ElectricCurrentUnit(PreFix.SI, BaseUnits.electricCurrent);
-        public static ElectricCurrentUnit Ampere = new ElectricCurrentUnit(PreFix.SI, BaseUnits.electricCurrent);
+        public static ElectricCurrentUnit SI =          new ElectricCurrentUnit(PreFix.SI, BaseUnits.electricCurrent);
+        public static ElectricCurrentUnit Ampere =      new ElectricCurrentUnit(PreFix.SI, BaseUnits.electricCurrent);
         public static ElectricCurrentUnit Centiampere = new ElectricCurrentUnit(PreFix.centi, BaseUnits.electricCurrent);
-        public static ElectricCurrentUnit Kiloampere = new ElectricCurrentUnit(PreFix.kilo, BaseUnits.electricCurrent);
-        public static ElectricCurrentUnit Megaampere = new ElectricCurrentUnit(PreFix.mega, BaseUnits.electricCurrent);
+        public static ElectricCurrentUnit Kiloampere =  new ElectricCurrentUnit(PreFix.kilo, BaseUnits.electricCurrent);
+        public static ElectricCurrentUnit Megaampere =  new ElectricCurrentUnit(PreFix.mega, BaseUnits.electricCurrent);
         public static ElectricCurrentUnit Microampere = new ElectricCurrentUnit(PreFix.micro, BaseUnits.electricCurrent);
         public static ElectricCurrentUnit Milliampere = new ElectricCurrentUnit(PreFix.milli, BaseUnits.electricCurrent);
-        public static ElectricCurrentUnit Nanoampere = new ElectricCurrentUnit(PreFix.nano, BaseUnits.electricCurrent);
-        public static ElectricCurrentUnit Picoampere = new ElectricCurrentUnit(PreFix.pico, BaseUnits.electricCurrent);
+        public static ElectricCurrentUnit Nanoampere =  new ElectricCurrentUnit(PreFix.nano, BaseUnits.electricCurrent);
+        public static ElectricCurrentUnit Picoampere =  new ElectricCurrentUnit(PreFix.pico, BaseUnits.electricCurrent);
 
 
         public ElectricCurrentUnit() { }
 
 
         public ElectricCurrentUnit(string symbol, decimal a1, decimal a2) : base(symbol, a1, a2)
-        {
-            SetUnitSystem();
-        }
-
-
-        public ElectricCurrentUnit(PreFix SI, BaseUnits baseunit) : base(SI, baseunit)
-        {
-            SetUnitSystem();
-        }
-
-        public void SetUnitSystem()
         {
             Unit = new UnitSystem();
             Unit.Electriccurrent = (ElectricCurrentUnit)Clone();
@@ -50,10 +39,15 @@ namespace EngineeringUnits
         }
 
 
-        public static IEnumerable<ElectricCurrentUnit> List()
+        public ElectricCurrentUnit(PreFix SI, BaseUnits baseunit) : base(SI, baseunit)
         {
-            return new[] { Ampere, Centiampere, Kiloampere, Megaampere, Microampere, Milliampere, Nanoampere, Picoampere };
-        }
+            Unit = new UnitSystem();
+            Unit.Electriccurrent = (ElectricCurrentUnit)Clone();
+
+
+            //Beta
+            Unit.UnitListBeta.Add(this);
+        }     
 
        
 
--- UnitTests/ComparToUnitNet.cs
+++ UnitTests/ComparToUnitNet.cs
@@ -397,31 +397,57 @@ namespace UnitTests
 
 
         [TestMethod]
-        public void ElectriccurrentCompareAutoTest()
+        public void ElectricCurrentCompElectricCurrentutoTest()
         {
+            var A1 = new UnitsNet.ElectricCurrent(65.743, UnitsNet.Units.ElectricCurrentUnit.Kiloampere);
+            var A2 = new EngineeringUnits.ElectricCurrent(65.743, EngineeringUnits.ElectricCurrentUnit.Kiloampere);
 
-            UnitsNet.ElectricCurrent A1 = new UnitsNet.ElectricCurrent(65.743, UnitsNet.Units.ElectricCurrentUnit.Ampere);
-            EngineeringUnits.ElectricCurrent A2 = new EngineeringUnits.ElectricCurrent(65.743, EngineeringUnits.ElectricCurrentUnit.Ampere);
+            int WorkingCompares = 0;
 
-            for (int i = 0; i < UnitsNet.ElectricCurrent.Units.Length; i++)
+
+            foreach (var EU in Enumeration.ListOf<ElectricCurrentUnit>())
             {
 
-                //Getting Units
-                var EU = EngineeringUnits.ElectricCurrentUnit.List().ToList()[i];
-                var UN = UnitsNet.ElectricCurrent.Units[i];
 
-                //All units absolute difference
-                Assert.AreEqual(0, A2.As(EU) - A1.As(UN), 1E-5);
+                double Error = 1E-5;
+                double RelError = 1E-5;
 
-                //All units relative difference
-                Assert.AreEqual(0, HelperClass.Percent(A2.As(EU),
-                                                        A1.As(UN)),
-                                                        1E-10);
-                //All units symbol compare
-                Assert.AreEqual(A2.ToUnit(EU).DisplaySymbol(),
-                                A1.ToUnit(UN).ToString("a"));
+                var UNList = UnitsNet.ElectricCurrent.Units.Where(x => x.ToString() == EU.QuantityName);
+
+
+                if (UNList.Count() == 1)
+                {
+                    var UN = UNList.Single();
+
+                    if (UN == UnitsNet.Units.ElectricCurrentUnit.Nanoampere) Error = 0.0078125;
+
+
+                    Debug.Print($"");
+                    Debug.Print($"UnitsNets:       {UN} {A1.As(UN)}");
+                    Debug.Print($"EngineeringUnit: {EU.QuantityName} {A2.As(EU)}");
+                    Debug.Print($"ABS:    {A2.As(EU) - A1.As(UN):F6}");
+                    Debug.Print($"REF[%]: {HelperClass.Percent(A2.As(EU), A1.As(UN)):P6}");
+
+                    //All units absolute difference
+                    Assert.AreEqual(0, A2.As(EU) - A1.As(UN), Error);
+
+                    //All units relative difference
+                    Assert.AreEqual(0, HelperClass.Percent(A2.As(EU),
+                                                            A1.As(UN)),
+                                                            RelError);
+                    //All units symbol compare
+                    Assert.AreEqual(A2.ToUnit(EU).DisplaySymbol(),
+                                    A1.ToUnit(UN).ToString("a"));
+
+                    WorkingCompares++;
+
+                }
 
             }
+
+            //Number of comparables units
+            Assert.AreEqual(8, WorkingCompares);
+
         }
 
 
