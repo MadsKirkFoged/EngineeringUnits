95068db57077aab24673e2590e1de04307795b4e
Mads Foged <jfogedm@jci.com>
2021-06-28 10:59:32 +0200

    LuminousIntensity

--- EngineeringUnits/BaseUnits/LuminousIntensity/LuminousIntensityEnum.cs
+++ EngineeringUnits/BaseUnits/LuminousIntensity/LuminousIntensityEnum.cs
@@ -8,55 +8,31 @@ using System.Text;
 namespace EngineeringUnits
 {
 
-
-    //public enum LuminousIntensityUnit
-    //{
-
-    //    [Vector(PreFix.SI, BaseUnits.luminousIntensity)] Candela,
-
-
-
-    //}
-
-
     public class LuminousIntensityUnit : Enumeration
     {
-
-
-
 
         public static LuminousIntensityUnit SI = new LuminousIntensityUnit(PreFix.SI, BaseUnits.luminousIntensity);
 
 
 
-        public LuminousIntensityUnit() { }
-
-
         public LuminousIntensityUnit(string symbol, decimal a1, decimal a2) : base(symbol, a1, a2)
-        {
-            SetUnitSystem();
-        }
-
-
-        public LuminousIntensityUnit(PreFix SI, BaseUnits baseunit) : base(SI, baseunit)
-        {
-            SetUnitSystem();
-        }
-
-        public void SetUnitSystem()
         {
             Unit = new UnitSystem();
             Unit.LuminousIntensity = (LuminousIntensityUnit)Clone();
-
 
             //Beta
             Unit.UnitListBeta.Add(this);
         }
 
-        public static IEnumerable<LuminousIntensityUnit> List()
+
+        public LuminousIntensityUnit(PreFix SI, BaseUnits baseunit) : base(SI, baseunit)
         {
-            return new[] { SI };
-        }
+            Unit = new UnitSystem();
+            Unit.LuminousIntensity = (LuminousIntensityUnit)Clone();
+
+            //Beta
+            Unit.UnitListBeta.Add(this);
+        }     
        
     }
 
