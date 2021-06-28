83182adfc55a1c961768484c5d265d9d9fe7e23b
Mads Foged <jfogedm@jci.com>
2021-06-28 14:12:08 +0200

    Entropy

--- UnitTests/ComparToUnitNetCombinedUnits.cs
+++ UnitTests/ComparToUnitNetCombinedUnits.cs
@@ -1217,49 +1217,105 @@ namespace UnitTests
 
         }
 
+        //[TestMethod]
+        //public void EntropyCompareAutoTest()
+        //{
+        //    UnitsNet.Entropy A1 = new UnitsNet.Entropy(1, UnitsNet.Units.EntropyUnit.JoulePerKelvin);
+        //    EngineeringUnits.Entropy A2 = new EngineeringUnits.Entropy(1, EngineeringUnits.EntropyUnit.JoulePerKelvin);
+
+        //    var EU11 = EngineeringUnits.EntropyUnit.List();
+        //    var UN11 = UnitsNet.Entropy.Units;
+
+
+        //    int DiffCount = 0;
+
+        //    for (int i = 0; i < UnitsNet.Entropy.Units.Length; i++)
+        //    {
+
+        //        //if (UnitsNet.Force.Units[i] == UnitsNet.Units.PressureUnit.FootOfElevation ||
+        //        //    UnitsNet.Pressure.Units[i] == UnitsNet.Units.PressureUnit.MeterOfElevation)
+        //        //{
+        //        //    DiffCount++;
+        //        //    continue;
+        //        //}
+
+
+
+        //        //Getting Units
+        //        var EU = EngineeringUnits.EntropyUnit.List().ToList()[i - DiffCount];
+        //        var UN = UnitsNet.Entropy.Units[i];
+
+        //        //All units absolute difference
+        //        Assert.AreEqual(0, A2.As(EU) - A1.As(UN), 1E-5);
+
+        //        //All units relative difference
+        //        Assert.AreEqual(0, HelperClass.Percent(A2.As(EU),
+        //                                                A1.As(UN)),
+        //                                                1E-5);
+        //        //All units symbol compare
+        //        Assert.AreEqual(A2.ToUnit(EU).DisplaySymbol(),
+        //                        A1.ToUnit(UN).ToString("a")
+        //                        .Replace("J/C", "J/K")
+        //                        //.Replace("K","k")
+        //                        );
+
+        //    }
+
+        //}
+
         [TestMethod]
-        public void EntropyCompareAutoTest()
+        public void EntropyAutoTest()
         {
-            UnitsNet.Entropy A1 = new UnitsNet.Entropy(1, UnitsNet.Units.EntropyUnit.JoulePerKelvin);
-            EngineeringUnits.Entropy A2 = new EngineeringUnits.Entropy(1, EngineeringUnits.EntropyUnit.JoulePerKelvin);
+            var A1 = new UnitsNet.Entropy(65.743, UnitsNet.Units.EntropyUnit.JoulePerKelvin);
+            var A2 = new EngineeringUnits.Entropy(65.743, EngineeringUnits.EntropyUnit.JoulePerKelvin);
 
-            var EU11 = EngineeringUnits.EntropyUnit.List();
-            var UN11 = UnitsNet.Entropy.Units;
+            int WorkingCompares = 0;
 
 
-            int DiffCount = 0;
-
-            for (int i = 0; i < UnitsNet.Entropy.Units.Length; i++)
+            foreach (var EU in Enumeration.ListOf<EntropyUnit>())
             {
 
-                //if (UnitsNet.Force.Units[i] == UnitsNet.Units.PressureUnit.FootOfElevation ||
-                //    UnitsNet.Pressure.Units[i] == UnitsNet.Units.PressureUnit.MeterOfElevation)
-                //{
-                //    DiffCount++;
-                //    continue;
-                //}
+
+                double Error = 1E-5;
+                double RelError = 1E-5;
+
+                var UNList = UnitsNet.Entropy.Units.Where(x => x.ToString() == EU.QuantityName);
 
 
+                if (UNList.Count() == 1)
+                {
+                    var UN = UNList.Single();
 
-                //Getting Units
-                var EU = EngineeringUnits.EntropyUnit.List().ToList()[i - DiffCount];
-                var UN = UnitsNet.Entropy.Units[i];
+                    //if (UN == UnitsNet.Units.EntropyUnit.SquareMicrometer) Error = 2629720.0009765625;
 
-                //All units absolute difference
-                Assert.AreEqual(0, A2.As(EU) - A1.As(UN), 1E-5);
 
-                //All units relative difference
-                Assert.AreEqual(0, HelperClass.Percent(A2.As(EU),
-                                                        A1.As(UN)),
-                                                        1E-5);
-                //All units symbol compare
-                Assert.AreEqual(A2.ToUnit(EU).DisplaySymbol(),
-                                A1.ToUnit(UN).ToString("a")
-                                .Replace("J/C", "J/K")
-                                //.Replace("K","k")
-                                );
+                    Debug.Print($"");
+                    Debug.Print($"UnitsNets:       {UN} {A1.As(UN)}");
+                    Debug.Print($"EngineeringUnit: {EU.QuantityName} {A2.As(EU)}");
+                    Debug.Print($"ABS:    {A2.As(EU) - A1.As(UN):F6}");
+                    Debug.Print($"REF[%]: {HelperClass.Percent(A2.As(EU), A1.As(UN)):P6}");
+
+                    //All units absolute difference
+                    Assert.AreEqual(0, A2.As(EU) - A1.As(UN), Error);
+
+                    //All units relative difference
+                    Assert.AreEqual(0, HelperClass.Percent(A2.As(EU),
+                                                            A1.As(UN)),
+                                                            RelError);
+                    //All units symbol compare
+                    Assert.AreEqual(A2.ToUnit(EU).DisplaySymbol(),
+                                    A1.ToUnit(UN).ToString("a")
+                                    .Replace("J/C", "J/K")
+                                    );
+
+                    WorkingCompares++;
+
+                }
 
             }
+
+            //Number of comparables units
+            Assert.AreEqual(7, WorkingCompares);
 
         }
 
