1990223f743424d408c2e10306d3dbc5ef881ee8
Mads Foged <jfogedm@jci.com>
2021-07-05 15:52:20 +0200

    level

--- EngineeringUnits/CombinedUnits/Level/LevelEnum.cs
+++ EngineeringUnits/CombinedUnits/Level/LevelEnum.cs
@@ -10,40 +10,20 @@ namespace EngineeringUnits
     public class LevelUnit : Enumeration
     {
 
-        public static readonly LevelUnit SI = new LevelUnit(LengthUnit.Meter);
-        public static readonly LevelUnit CubicMeter = new LevelUnit(LengthUnit.Meter);
-        public static readonly LevelUnit HectocubicMeter = new LevelUnit(PreFix.hecto, CubicMeter);
-        public static readonly LevelUnit KilocubicMeter = new LevelUnit(PreFix.kilo, CubicMeter);
+        public static readonly LevelUnit SI = new LevelUnit();
+        public static readonly LevelUnit Decibel = new LevelUnit("dB");
+        public static readonly LevelUnit Neper = new LevelUnit("Np", 1/0.115129254m);
 
 
 
-        public LevelUnit(LengthUnit Length, string NewSymbol = "Empty", decimal correction = 1)
+        public LevelUnit(string NewSymbol = "Empty", decimal correction = 1)
         {
-            Unit = Length.Unit * Length.Unit * Length.Unit;
+            Unit = new UnitSystem();
             SetCombined(correction);
             SetNewSymbol(NewSymbol);
         }
 
-        public LevelUnit(AreaUnit Area, LengthUnit Length, string NewSymbol = "Empty", decimal correction = 1)
-        {
-            Unit = Area.Unit * Length.Unit;
-            SetCombined(correction);
-            SetNewSymbol(NewSymbol, $"{Area}-{Length}");
-        }
-
-        public LevelUnit(PreFix SI, LevelUnit unit)
-        {
-            Unit = unit.Unit.Copy();
-            SetCombined(SI);
-            SetNewSymbol(SI);
-        }
-
-        public LevelUnit(LevelUnit unit, string NewSymbol = "Empty", decimal correction = 1)
-        {
-            Unit = unit.Unit.Copy();
-            SetCombined(correction);
-            SetNewSymbol(NewSymbol);
-        }
+       
 
     }
 
