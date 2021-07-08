231bd1940ea2d729cbaf6e799a3bee1a49856de2
Mads Foged <jfogedm@jci.com>
2021-07-08 10:16:47 +0200

    Obsolete

---
+++ EngineeringUnits/CombinedUnits/Obsolete VolumeConcentration/VolumeConcentration.cs
@@ -0,0 +1,17 @@
+﻿using Fractions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EngineeringUnits
+{
+    [Obsolete("VolumeConcentration is deprecated, please use Ratio instead.", true)]
+    public partial class VolumeConcentration : BaseUnit
+    {
+
+       
+
+
+
+    }
+}
---
+++ EngineeringUnits/CombinedUnits/Obsolete VolumeConcentration/VolumeConcentrationEnum.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace EngineeringUnits
+{
+
+    [Obsolete("VolumeConcentration is deprecated, please use Ratio instead.", true)]
+    public class VolumeConcentrationUnit : Enumeration
+    {
+
+       
+
+    }
+
+
+
+
+}
