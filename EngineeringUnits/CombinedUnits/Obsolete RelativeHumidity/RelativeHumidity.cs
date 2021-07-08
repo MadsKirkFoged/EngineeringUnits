433839f027526e298d4fd86039dfac67622e2513
Mads Foged <jfogedm@jci.com>
2021-07-08 10:14:52 +0200

    moving around

---
+++ EngineeringUnits/CombinedUnits/Obsolete RelativeHumidity/RelativeHumidity.cs
@@ -0,0 +1,14 @@
+﻿using Fractions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EngineeringUnits
+{
+    [Obsolete("RelativeHumidity is deprecated, please use Ratio instead.", true)]
+    public partial class RelativeHumidity : BaseUnit
+    {
+
+
+    }
+}
---
+++ EngineeringUnits/CombinedUnits/Obsolete RelativeHumidity/RelativeHumidityEnum.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace EngineeringUnits
+{
+
+    [Obsolete("RelativeHumidity is deprecated, please use Ratio instead.", true)]
+    public class RelativeHumidityUnit : Enumeration
+    {
+
+       
+
+    }
+
+
+
+
+}
