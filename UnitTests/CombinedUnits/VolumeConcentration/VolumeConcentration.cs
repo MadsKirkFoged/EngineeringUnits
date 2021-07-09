edc03e3f6266597ec4759314a18399275cfcb301
Mads Foged <jfogedm@jci.com>
2021-07-09 15:19:15 +0200

    Ready to test Math

--- UnitTests/CombinedUnits/VolumeConcentration/VolumeConcentration.cs
+++ UnitTests/CombinedUnits/VolumeConcentration/VolumeConcentration.cs
@@ -8,71 +8,72 @@ using System.Collections.Generic;
 
 namespace UnitTests
 {
-    //[TestClass]
-    //public class VolumeConcentrationTest
-    //{
+    [TestClass]
+    public class VolumeConcentrationTest
+    {
 
 
 
-    //    [TestMethod]
-    //    public void VolumeConcentrationAutoTest()
-    //    {
-    //        var A1 = new UnitsNet.VolumeConcentration(1, UnitsNet.Units.VolumeConcentrationUnit.LitersPerLiter);
-    //        var A2 = new EngineeringUnits.Ratio(1, EngineeringUnits.RatioUnit.LitersPerLiter);
+        [TestMethod]
+        public void VolumeConcentrationAutoTest()
+        {
+            var A1 = new UnitsNet.VolumeConcentration(1, UnitsNet.Units.VolumeConcentrationUnit.LitersPerLiter);
+            var A2 = new EngineeringUnits.Ratio(1, EngineeringUnits.RatioUnit.LitersPerLiter);
 
-    //        int WorkingCompares = 0;
-
-
-    //        foreach (var EU in Enumeration.ListOf<RatioUnit>())
-    //        {
+            int WorkingCompares = 0;
 
 
-    //            double Error = 3E-3;
-    //            double RelError = 1E-5;
-
-    //            var UNList = UnitsNet.VolumeConcentration.Units.Where(x => x.ToString() == EU.QuantityName);
+            foreach (var EU in Enumeration.ListOf<RatioUnit>())
+            {
 
 
-    //            if (UNList.Count() == 1)
-    //            {
-    //                var UN = UNList.Single();
+                double Error = 3E-3;
+                double RelError = 1E-5;
 
-    //                //if (UN == UnitsNet.Units.VolumeConcentrationUnit.PicowattPerCubicFoot) Error = 0.0625;
-
-
-    //                Debug.Print($"");
-    //                Debug.Print($"UnitsNets:       {UN} {A1.As(UN)}");
-    //                Debug.Print($"EngineeringUnit: {EU.QuantityName} {A2.As(EU)}");
-    //                Debug.Print($"ABS:    {A2.As(EU) - A1.As(UN):F6}");
-    //                Debug.Print($"REF[%]: {HelperClass.Percent(A2.As(EU), A1.As(UN)):P6}");
-
-    //                //All units absolute difference
-    //                Assert.AreEqual(0, A2.As(EU) - A1.As(UN), Error);
-
-    //                //All units relative difference
-    //                Assert.AreEqual(0, HelperClass.Percent(A2.As(EU),
-    //                                                        A1.As(UN)),
-    //                                                        RelError);
+                var UNList = UnitsNet.VolumeConcentration.Units.Where(x => x.ToString() == EU.QuantityName);
 
 
+                if (UNList.Count() == 1)
+                {
+                    var UN = UNList.Single();
 
-    //                //All units symbol compare
-    //                Assert.AreEqual(A2.ToUnit(EU).DisplaySymbol(),
-    //                            A1.ToUnit(UN).ToString("a")
-
-    //                            .Replace("L/L","l/l")
-    //                            );
+                    //if (UN == UnitsNet.Units.VolumeConcentrationUnit.PicowattPerCubicFoot) Error = 0.0625;
 
 
-    //                WorkingCompares++;
+                    Debug.Print($"");
+                    Debug.Print($"UnitsNets:       {UN} {A1.As(UN)}");
+                    Debug.Print($"EngineeringUnit: {EU.QuantityName} {A2.As(EU)}");
+                    Debug.Print($"ABS:    {A2.As(EU) - A1.As(UN):F6}");
+                    Debug.Print($"REF[%]: {HelperClass.Percent(A2.As(EU), A1.As(UN)):P6}");
 
-    //            }
+                    //All units absolute difference
+                    Assert.AreEqual(0, A2.As(EU) - A1.As(UN), Error);
 
-    //        }
+                    //All units relative difference
+                    Assert.AreEqual(0, HelperClass.Percent(A2.As(EU),
+                                                            A1.As(UN)),
+                                                            RelError);
 
-    //        //Number of comparables units
-    //        Assert.AreEqual(21, WorkingCompares);
 
-    //    }
-    //}
+
+                    //All units symbol compare
+                    Assert.AreEqual(A2.ToUnit(EU).DisplaySymbol(),
+                                A1.ToUnit(UN).ToString("a")
+
+                                //.Replace("L/L", "l/l")
+                                .Replace("L", "l")
+                                );
+
+
+                    WorkingCompares++;
+
+                }
+
+            }
+
+            //Number of comparables units
+            Assert.AreEqual(20, WorkingCompares);
+
+        }
+    }
 }
