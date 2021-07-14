9bf31babfea37754adec16a5525475555f2a9603
Mads Foged <jfogedm@jci.com>
2021-07-14 12:54:26 +0200

    ..

--- EngineeringUnits/CombinedUnits/RotationalAcceleration/RotationalAccelerationEnum.cs
+++ EngineeringUnits/CombinedUnits/RotationalAcceleration/RotationalAccelerationEnum.cs
@@ -19,14 +19,14 @@ namespace EngineeringUnits
 
         public RotationalAccelerationUnit(LengthUnit Length, string NewSymbol = "Empty", decimal correction = 1)
         {
-            Unit = Length.Unit * Length.Unit * Length.Unit;
+            Unit = Length.Pow(3);
             SetCombined(correction);
             SetNewSymbol(NewSymbol);
         }
 
         public RotationalAccelerationUnit(AreaUnit Area, LengthUnit Length, string NewSymbol = "Empty", decimal correction = 1)
         {
-            Unit = Area.Unit * Length.Unit;
+            Unit = Area * Length;
             SetCombined(correction);
             SetNewSymbol(NewSymbol, $"{Area}-{Length}");
         }
