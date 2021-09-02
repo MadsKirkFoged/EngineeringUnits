3bf6ad114b87b23ebeb3f989ae21728f38e06765
Jason Cockrell <Jason.Cockrell@analog.com>
2021-09-02 11:15:13 -0400

    fix folder paths to work on Azure

--- CodeGen/Program.cs
+++ CodeGen/Program.cs
@@ -8,10 +8,11 @@ namespace CodeGen
     class Program
     {
         static readonly string LIBRARY_NAME = "EngineeringUnits";
+        static readonly string GENERATOR_NAME = "CodeGen";
         static void Main(string[] args)
         {
             string assemblyDir = Assembly.GetAssembly(typeof(Program)).Location;
-            string solutionDir = assemblyDir.Substring(0, assemblyDir.IndexOf(LIBRARY_NAME) + LIBRARY_NAME.Length);
+            string solutionDir = assemblyDir.Substring(0, assemblyDir.IndexOf(GENERATOR_NAME));
             string libraryDir = Path.Combine(solutionDir, LIBRARY_NAME);
 
             UnknownUnitExtensionsGenerator.Generate(libraryDir);
