c6d57dba88ff2ad08303009ea489d3f6386dfb56
Mads Foged <jfogedm@jci.com>
2021-09-06 14:12:17 +0200

    ..

--- EngineeringUnits/CombinedUnits/HeatFlux/HeatFlux.cs
+++ EngineeringUnits/CombinedUnits/HeatFlux/HeatFlux.cs
@@ -1,39 +1,25 @@
-﻿using Fractions;
-using EngineeringUnits.Units;
-using System.Collections.Generic;
-using System.Text;
-using EngineeringUnits.Units;
+﻿using EngineeringUnits.Units;
+
 
 namespace EngineeringUnits
 {
     public partial class HeatFlux : BaseUnit
     {
 
-        public HeatFlux()
-        {
-            Unit = HeatFluxUnit.SI.Unit.Copy();
-        }
-
+        public HeatFlux() { }
         public HeatFlux(decimal value, HeatFluxUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public HeatFlux(double value, HeatFluxUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public HeatFlux(int value, HeatFluxUnit selectedUnit) : base(value, selectedUnit.Unit) { }
+        public HeatFlux(UnknownUnit value) : base(value) { }
 
+        public HeatFlux(UnknownUnit value, HeatFluxUnit selectedUnit) : base(value, selectedUnit.Unit) { }
 
         public static HeatFlux From(double value, HeatFluxUnit unit) => new HeatFlux(value, unit);
         public double As(HeatFluxUnit ReturnInThisUnit) => (double)ToTheOutSide(ReturnInThisUnit.Unit);
         public HeatFlux ToUnit(HeatFluxUnit selectedUnit) => new HeatFlux(ToTheOutSide(selectedUnit.Unit), selectedUnit);
         public static HeatFlux Zero => new HeatFlux(0, HeatFluxUnit.SI);
 
-        public static implicit operator HeatFlux(UnknownUnit Unit)
-        {
-            HeatFlux local = new HeatFlux(0, HeatFluxUnit.SI);
-
-            local.Transform(Unit);
-            return local;
-        }
-
-
-
+        public static implicit operator HeatFlux(UnknownUnit Unit) => new HeatFlux(Unit, HeatFluxUnit.SI);
 
     }
 }
