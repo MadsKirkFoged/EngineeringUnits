b01330e7b12e6e7d2359959a05ee2fa733552590
Mads Foged <jfogedm@jci.com>
2021-09-06 12:30:19 +0200

    ..

--- EngineeringUnits/CombinedUnits/ThermalConductivity/ThermalConductivity.cs
+++ EngineeringUnits/CombinedUnits/ThermalConductivity/ThermalConductivity.cs
@@ -1,22 +1,20 @@
 ﻿using EngineeringUnits.Units;
-using Fractions;
-using EngineeringUnits.Units;
-using System.Collections.Generic;
-using System.Text;
+
 
 namespace EngineeringUnits
 {
     public partial class ThermalConductivity : BaseUnit
     {
-
         public ThermalConductivity()
         {
-            Unit = ThermalConductivityUnit.SI.Unit.Copy();
+            Unit = ThermalConductivityUnit.SI.Unit;
         }
+
 
         public ThermalConductivity(decimal value, ThermalConductivityUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public ThermalConductivity(double value, ThermalConductivityUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public ThermalConductivity(int value, ThermalConductivityUnit selectedUnit) : base(value, selectedUnit.Unit) { }
+        public ThermalConductivity(UnknownUnit value) : base(value) { }
 
 
         public static ThermalConductivity From(double value, ThermalConductivityUnit unit) => new ThermalConductivity(value, unit);
@@ -24,15 +22,7 @@ namespace EngineeringUnits
         public ThermalConductivity ToUnit(ThermalConductivityUnit selectedUnit) => new ThermalConductivity(ToTheOutSide(selectedUnit.Unit), selectedUnit);
         public static ThermalConductivity Zero => new ThermalConductivity(0, ThermalConductivityUnit.SI);
 
-        public static implicit operator ThermalConductivity(UnknownUnit Unit)
-        {
-            ThermalConductivity local = new ThermalConductivity(0, ThermalConductivityUnit.SI);
-
-            local.Transform(Unit);
-            return local;
-        }
-
-
+        public static implicit operator ThermalConductivity(UnknownUnit Unit) => new ThermalConductivity(Unit);
 
 
     }
