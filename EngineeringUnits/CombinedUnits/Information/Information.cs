21a9bd142a651a7040d371425d4731894117168b
Mads Foged <jfogedm@jci.com>
2021-09-06 14:10:38 +0200

    ...

--- EngineeringUnits/CombinedUnits/Information/Information.cs
+++ EngineeringUnits/CombinedUnits/Information/Information.cs
@@ -1,39 +1,25 @@
-﻿using Fractions;
-using EngineeringUnits.Units;
-using System.Collections.Generic;
-using System.Text;
-using EngineeringUnits.Units;
+﻿using EngineeringUnits.Units;
+
 
 namespace EngineeringUnits
 {
     public partial class Information : BaseUnit
     {
 
-        public Information()
-        {
-            Unit = InformationUnit.SI.Unit.Copy();
-        }
-
+        public Information() { }
         public Information(decimal value, InformationUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public Information(double value, InformationUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public Information(int value, InformationUnit selectedUnit) : base(value, selectedUnit.Unit) { }
+        public Information(UnknownUnit value) : base(value) { }
 
+        public Information(UnknownUnit value, InformationUnit selectedUnit) : base(value, selectedUnit.Unit) { }
 
         public static Information From(double value, InformationUnit unit) => new Information(value, unit);
         public double As(InformationUnit ReturnInThisUnit) => (double)ToTheOutSide(ReturnInThisUnit.Unit);
         public Information ToUnit(InformationUnit selectedUnit) => new Information(ToTheOutSide(selectedUnit.Unit), selectedUnit);
         public static Information Zero => new Information(0, InformationUnit.SI);
 
-        public static implicit operator Information(UnknownUnit Unit)
-        {
-            Information local = new Information(0, InformationUnit.SI);
-
-            local.Transform(Unit);
-            return local;
-        }
-
-
-
+        public static implicit operator Information(UnknownUnit Unit) => new Information(Unit, InformationUnit.SI);
 
     }
 }
