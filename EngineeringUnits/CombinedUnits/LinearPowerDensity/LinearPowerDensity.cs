0e019f29ef5eb366eb9abed435eb38632e7e77fa
Mads Foged <jfogedm@jci.com>
2021-09-06 14:06:01 +0200

    ..

--- EngineeringUnits/CombinedUnits/LinearPowerDensity/LinearPowerDensity.cs
+++ EngineeringUnits/CombinedUnits/LinearPowerDensity/LinearPowerDensity.cs
@@ -1,39 +1,24 @@
-﻿using Fractions;
-using EngineeringUnits.Units;
-using System.Collections.Generic;
-using System.Text;
-using EngineeringUnits.Units;
+﻿using EngineeringUnits.Units;
+
 
 namespace EngineeringUnits
 {
     public partial class LinearPowerDensity : BaseUnit
     {
 
-        public LinearPowerDensity()
-        {
-            Unit = LinearPowerDensityUnit.SI.Unit.Copy();
-        }
-
+        public LinearPowerDensity() { }
         public LinearPowerDensity(decimal value, LinearPowerDensityUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public LinearPowerDensity(double value, LinearPowerDensityUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public LinearPowerDensity(int value, LinearPowerDensityUnit selectedUnit) : base(value, selectedUnit.Unit) { }
+        public LinearPowerDensity(UnknownUnit value) : base(value) { }
 
+        public LinearPowerDensity(UnknownUnit value, LinearPowerDensityUnit selectedUnit) : base(value, selectedUnit.Unit) { }
 
         public static LinearPowerDensity From(double value, LinearPowerDensityUnit unit) => new LinearPowerDensity(value, unit);
         public double As(LinearPowerDensityUnit ReturnInThisUnit) => (double)ToTheOutSide(ReturnInThisUnit.Unit);
         public LinearPowerDensity ToUnit(LinearPowerDensityUnit selectedUnit) => new LinearPowerDensity(ToTheOutSide(selectedUnit.Unit), selectedUnit);
         public static LinearPowerDensity Zero => new LinearPowerDensity(0, LinearPowerDensityUnit.SI);
 
-        public static implicit operator LinearPowerDensity(UnknownUnit Unit)
-        {
-            LinearPowerDensity local = new LinearPowerDensity(0, LinearPowerDensityUnit.SI);
-
-            local.Transform(Unit);
-            return local;
-        }
-
-
-
-
+        public static implicit operator LinearPowerDensity(UnknownUnit Unit) => new LinearPowerDensity(Unit, LinearPowerDensityUnit.SI);
     }
 }
