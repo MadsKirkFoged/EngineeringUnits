7f68ff54912e00a4430aeec397ac048461e8adc2
Mads Foged <jfogedm@jci.com>
2021-09-06 14:33:15 +0200

    ..

--- EngineeringUnits/CombinedUnits/Angle/Angle.cs
+++ EngineeringUnits/CombinedUnits/Angle/Angle.cs
@@ -1,39 +1,25 @@
-﻿using Fractions;
-using EngineeringUnits.Units;
-using System.Collections.Generic;
-using System.Text;
-using EngineeringUnits.Units;
+﻿using EngineeringUnits.Units;
+
 
 namespace EngineeringUnits
 {
     public partial class Angle : BaseUnit
     {
 
-        public Angle()
-        {
-            Unit = AngleUnit.SI.Unit.Copy();
-        }
-
+        public Angle() { }
         public Angle(decimal value, AngleUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public Angle(double value, AngleUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public Angle(int value, AngleUnit selectedUnit) : base(value, selectedUnit.Unit) { }
+        public Angle(UnknownUnit value) : base(value) { }
 
+        public Angle(UnknownUnit value, AngleUnit selectedUnit) : base(value, selectedUnit.Unit) { }
 
         public static Angle From(double value, AngleUnit unit) => new Angle(value, unit);
         public double As(AngleUnit ReturnInThisUnit) => (double)ToTheOutSide(ReturnInThisUnit.Unit);
         public Angle ToUnit(AngleUnit selectedUnit) => new Angle(ToTheOutSide(selectedUnit.Unit), selectedUnit);
         public static Angle Zero => new Angle(0, AngleUnit.SI);
 
-        public static implicit operator Angle(UnknownUnit Unit)
-        {
-            Angle local = new Angle(0, AngleUnit.SI);
-
-            local.Transform(Unit);
-            return local;
-        }
-
-
-
+        public static implicit operator Angle(UnknownUnit Unit) => new Angle(Unit, AngleUnit.SI);
 
     }
 }
