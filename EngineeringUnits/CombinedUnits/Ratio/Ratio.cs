b1beef662c9b411f7b4cfd22103d77c00d619929
Mads Foged <jfogedm@jci.com>
2021-09-06 14:39:24 +0200

    ...

--- EngineeringUnits/CombinedUnits/Ratio/Ratio.cs
+++ EngineeringUnits/CombinedUnits/Ratio/Ratio.cs
@@ -5,23 +5,19 @@ namespace EngineeringUnits
     public partial class Ratio : BaseUnit
     {
 
-        public Ratio()
-        {
-            Unit = RatioUnit.SI.Unit;
-        }
-
-
+        public Ratio() { }
         public Ratio(decimal value, RatioUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public Ratio(double value, RatioUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public Ratio(int value, RatioUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public Ratio(UnknownUnit value) : base(value) { }
 
+        public Ratio(UnknownUnit value, RatioUnit selectedUnit) : base(value, selectedUnit.Unit) { }
 
         public static Ratio From(double value, RatioUnit unit) => new Ratio(value, unit);
         public double As(RatioUnit ReturnInThisUnit) => (double)ToTheOutSide(ReturnInThisUnit.Unit);
         public Ratio ToUnit(RatioUnit selectedUnit) => new Ratio(ToTheOutSide(selectedUnit.Unit), selectedUnit);
         public static Ratio Zero => new Ratio(0, RatioUnit.SI);
 
-        public static implicit operator Ratio(UnknownUnit Unit) => new Ratio(Unit);
+        public static implicit operator Ratio(UnknownUnit Unit) => new Ratio(Unit, RatioUnit.SI);
     }
 }
