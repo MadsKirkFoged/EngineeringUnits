31b5ef9ba881e874b03dd09cec25a42f7cff759f
Mads Foged <jfogedm@jci.com>
2021-09-06 12:31:03 +0200

    ..

--- EngineeringUnits/CombinedUnits/Speed/Speed.cs
+++ EngineeringUnits/CombinedUnits/Speed/Speed.cs
@@ -1,23 +1,20 @@
 ﻿using EngineeringUnits.Units;
-using Fractions;
-using EngineeringUnits.Units;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Text;
+
 
 namespace EngineeringUnits
 {
     public partial class Speed : BaseUnit
     {
-
         public Speed()
         {
-            Unit = SpeedUnit.SI.Unit.Copy();
+            Unit = SpeedUnit.SI.Unit;
         }
+
 
         public Speed(decimal value, SpeedUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public Speed(double value, SpeedUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public Speed(int value, SpeedUnit selectedUnit) : base(value, selectedUnit.Unit) { }
+        public Speed(UnknownUnit value) : base(value) { }
 
 
         public static Speed From(double value, SpeedUnit unit) => new Speed(value, unit);
@@ -25,14 +22,6 @@ namespace EngineeringUnits
         public Speed ToUnit(SpeedUnit selectedUnit) => new Speed(ToTheOutSide(selectedUnit.Unit), selectedUnit);
         public static Speed Zero => new Speed(0, SpeedUnit.SI);
 
-        public static implicit operator Speed(UnknownUnit Unit)
-        {
-            Speed local = new Speed(0, SpeedUnit.SI);
-
-            local.Transform(Unit);
-            return local;
-        }
-
-
+        public static implicit operator Speed(UnknownUnit Unit) => new Speed(Unit);
     }
 }
