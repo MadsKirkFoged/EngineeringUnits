2032ced7aa70f8861ae1f4798aef677c59d8d6c5
Mads Foged <jfogedm@jci.com>
2021-09-06 14:16:26 +0200

    ..

--- EngineeringUnits/CombinedUnits/ElectricSurfaceChargeDensity/ElectricSurfaceChargeDensity.cs
+++ EngineeringUnits/CombinedUnits/ElectricSurfaceChargeDensity/ElectricSurfaceChargeDensity.cs
@@ -1,38 +1,24 @@
-﻿using Fractions;
-using EngineeringUnits.Units;
-using System.Collections.Generic;
-using System.Text;
-using EngineeringUnits.Units;
+﻿using EngineeringUnits.Units;
 
 namespace EngineeringUnits
 {
     public partial class ElectricSurfaceChargeDensity : BaseUnit
     {
 
-        public ElectricSurfaceChargeDensity()
-        {
-            Unit = ElectricSurfaceChargeDensityUnit.SI.Unit.Copy();
-        }
-
+        public ElectricSurfaceChargeDensity() { }
         public ElectricSurfaceChargeDensity(decimal value, ElectricSurfaceChargeDensityUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public ElectricSurfaceChargeDensity(double value, ElectricSurfaceChargeDensityUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public ElectricSurfaceChargeDensity(int value, ElectricSurfaceChargeDensityUnit selectedUnit) : base(value, selectedUnit.Unit) { }
+        public ElectricSurfaceChargeDensity(UnknownUnit value) : base(value) { }
 
+        public ElectricSurfaceChargeDensity(UnknownUnit value, ElectricSurfaceChargeDensityUnit selectedUnit) : base(value, selectedUnit.Unit) { }
 
         public static ElectricSurfaceChargeDensity From(double value, ElectricSurfaceChargeDensityUnit unit) => new ElectricSurfaceChargeDensity(value, unit);
         public double As(ElectricSurfaceChargeDensityUnit ReturnInThisUnit) => (double)ToTheOutSide(ReturnInThisUnit.Unit);
         public ElectricSurfaceChargeDensity ToUnit(ElectricSurfaceChargeDensityUnit selectedUnit) => new ElectricSurfaceChargeDensity(ToTheOutSide(selectedUnit.Unit), selectedUnit);
         public static ElectricSurfaceChargeDensity Zero => new ElectricSurfaceChargeDensity(0, ElectricSurfaceChargeDensityUnit.SI);
 
-        public static implicit operator ElectricSurfaceChargeDensity(UnknownUnit Unit)
-        {
-            ElectricSurfaceChargeDensity local = new ElectricSurfaceChargeDensity(0, ElectricSurfaceChargeDensityUnit.SI);
-
-            local.Transform(Unit);
-            return local;
-        }
-
-
+        public static implicit operator ElectricSurfaceChargeDensity(UnknownUnit Unit) => new ElectricSurfaceChargeDensity(Unit, ElectricSurfaceChargeDensityUnit.SI);
 
 
     }
