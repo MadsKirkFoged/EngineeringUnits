c75b127338d71d00260cee8b691826cedbf7fffa
Mads Foged <jfogedm@jci.com>
2021-09-06 12:32:19 +0200

    ..

--- EngineeringUnits/CombinedUnits/SpecificEntropy/SpecificEntropy.cs
+++ EngineeringUnits/CombinedUnits/SpecificEntropy/SpecificEntropy.cs
@@ -1,25 +1,20 @@
 ﻿using EngineeringUnits.Units;
-using Fractions;
-using EngineeringUnits.Units;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Text;
 
 namespace EngineeringUnits
 {
 
     public partial class SpecificEntropy : BaseUnit
     {
-
         public SpecificEntropy()
         {
-            Unit = SpecificEntropyUnit.SI.Unit.Copy();
+            Unit = SpecificEntropyUnit.SI.Unit;
         }
 
 
         public SpecificEntropy(decimal value, SpecificEntropyUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public SpecificEntropy(double value, SpecificEntropyUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public SpecificEntropy(int value, SpecificEntropyUnit selectedUnit) : base(value, selectedUnit.Unit) { }
+        public SpecificEntropy(UnknownUnit value) : base(value) { }
 
 
         public static SpecificEntropy From(double value, SpecificEntropyUnit unit) => new SpecificEntropy(value, unit);
@@ -27,16 +22,6 @@ namespace EngineeringUnits
         public SpecificEntropy ToUnit(SpecificEntropyUnit selectedUnit) => new SpecificEntropy(ToTheOutSide(selectedUnit.Unit), selectedUnit);
         public static SpecificEntropy Zero => new SpecificEntropy(0, SpecificEntropyUnit.SI);
 
-        public static implicit operator SpecificEntropy(UnknownUnit Unit)
-        {
-            SpecificEntropy local = new SpecificEntropy(0, SpecificEntropyUnit.SI);
-
-            local.Transform(Unit);
-            return local;
-        }
-
-
-
-
+        public static implicit operator SpecificEntropy(UnknownUnit Unit) => new SpecificEntropy(Unit);
     }
 }
