fa433e22075be422a276f4a253ba54f93051bb59
Mads Foged <jfogedm@jci.com>
2021-09-06 14:23:25 +0200

    ...

--- EngineeringUnits/CombinedUnits/ElectricConductance/ElectricConductance.cs
+++ EngineeringUnits/CombinedUnits/ElectricConductance/ElectricConductance.cs
@@ -1,38 +1,25 @@
-﻿using Fractions;
-using EngineeringUnits.Units;
-using System.Collections.Generic;
-using System.Text;
-using EngineeringUnits.Units;
+﻿using EngineeringUnits.Units;
+
 
 namespace EngineeringUnits
 {
     public partial class ElectricConductance : BaseUnit
     {
 
-        public ElectricConductance()
-        {
-            Unit = ElectricConductanceUnit.SI.Unit.Copy();
-        }
-
+        public ElectricConductance() { }
         public ElectricConductance(decimal value, ElectricConductanceUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public ElectricConductance(double value, ElectricConductanceUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public ElectricConductance(int value, ElectricConductanceUnit selectedUnit) : base(value, selectedUnit.Unit) { }
+        public ElectricConductance(UnknownUnit value) : base(value) { }
 
+        public ElectricConductance(UnknownUnit value, ElectricConductanceUnit selectedUnit) : base(value, selectedUnit.Unit) { }
 
         public static ElectricConductance From(double value, ElectricConductanceUnit unit) => new ElectricConductance(value, unit);
         public double As(ElectricConductanceUnit ReturnInThisUnit) => (double)ToTheOutSide(ReturnInThisUnit.Unit);
         public ElectricConductance ToUnit(ElectricConductanceUnit selectedUnit) => new ElectricConductance(ToTheOutSide(selectedUnit.Unit), selectedUnit);
         public static ElectricConductance Zero => new ElectricConductance(0, ElectricConductanceUnit.SI);
 
-        public static implicit operator ElectricConductance(UnknownUnit Unit)
-        {
-            ElectricConductance local = new ElectricConductance(0, ElectricConductanceUnit.SI);
-
-            local.Transform(Unit);
-            return local;
-        }
-
-
+        public static implicit operator ElectricConductance(UnknownUnit Unit) => new ElectricConductance(Unit, ElectricConductanceUnit.SI);
 
 
     }
