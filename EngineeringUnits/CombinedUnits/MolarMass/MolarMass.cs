4d9b4bbb5e1b6ff329b98d0c5fcc9f25e896ebed
Mads Foged <jfogedm@jci.com>
2021-09-06 13:56:50 +0200

    ..

--- EngineeringUnits/CombinedUnits/MolarMass/MolarMass.cs
+++ EngineeringUnits/CombinedUnits/MolarMass/MolarMass.cs
@@ -1,40 +1,24 @@
-﻿using Fractions;
-using EngineeringUnits.Units;
-using System.Collections.Generic;
-using System.Text;
-using EngineeringUnits.Units;
+﻿using EngineeringUnits.Units;
+
 
 namespace EngineeringUnits
 {
     public partial class MolarMass : BaseUnit
     {
 
-        public MolarMass()
-        {
-            Unit = MolarMassUnit.SI.Unit.Copy();
-        }
-
+        public MolarMass() { }
         public MolarMass(decimal value, MolarMassUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public MolarMass(double value, MolarMassUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public MolarMass(int value, MolarMassUnit selectedUnit) : base(value, selectedUnit.Unit) { }
+        public MolarMass(UnknownUnit value) : base(value) { }
 
+        public MolarMass(UnknownUnit value, MolarMassUnit selectedUnit) : base(value, selectedUnit.Unit) { }
 
         public static MolarMass From(double value, MolarMassUnit unit) => new MolarMass(value, unit);
         public double As(MolarMassUnit ReturnInThisUnit) => (double)ToTheOutSide(ReturnInThisUnit.Unit);
         public MolarMass ToUnit(MolarMassUnit selectedUnit) => new MolarMass(ToTheOutSide(selectedUnit.Unit), selectedUnit);
         public static MolarMass Zero => new MolarMass(0, MolarMassUnit.SI);
 
-        public static implicit operator MolarMass(UnknownUnit Unit)
-        {
-            MolarMass local = new MolarMass(0, MolarMassUnit.SI);
-
-            local.Transform(Unit);
-            return local;
-        }
-
-
-
-
-
+        public static implicit operator MolarMass(UnknownUnit Unit) => new MolarMass(Unit, MolarMassUnit.SI);
     }
 }
