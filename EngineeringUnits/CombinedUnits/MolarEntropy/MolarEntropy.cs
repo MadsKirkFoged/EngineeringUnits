0e1aba3668058241a471060100f466979a85eb05
Mads Foged <jfogedm@jci.com>
2021-09-06 13:58:26 +0200

    ...

--- EngineeringUnits/CombinedUnits/MolarEntropy/MolarEntropy.cs
+++ EngineeringUnits/CombinedUnits/MolarEntropy/MolarEntropy.cs
@@ -1,39 +1,25 @@
-﻿using Fractions;
-using EngineeringUnits.Units;
-using System.Collections.Generic;
-using System.Text;
-using EngineeringUnits.Units;
+﻿using EngineeringUnits.Units;
+
 
 namespace EngineeringUnits
 {
     public partial class MolarEntropy : BaseUnit
     {
 
-        public MolarEntropy()
-        {
-            Unit = MolarEntropyUnit.SI.Unit.Copy();
-        }
-
+        public MolarEntropy() { }
         public MolarEntropy(decimal value, MolarEntropyUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public MolarEntropy(double value, MolarEntropyUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public MolarEntropy(int value, MolarEntropyUnit selectedUnit) : base(value, selectedUnit.Unit) { }
+        public MolarEntropy(UnknownUnit value) : base(value) { }
 
+        public MolarEntropy(UnknownUnit value, MolarEntropyUnit selectedUnit) : base(value, selectedUnit.Unit) { }
 
         public static MolarEntropy From(double value, MolarEntropyUnit unit) => new MolarEntropy(value, unit);
         public double As(MolarEntropyUnit ReturnInThisUnit) => (double)ToTheOutSide(ReturnInThisUnit.Unit);
         public MolarEntropy ToUnit(MolarEntropyUnit selectedUnit) => new MolarEntropy(ToTheOutSide(selectedUnit.Unit), selectedUnit);
         public static MolarEntropy Zero => new MolarEntropy(0, MolarEntropyUnit.SI);
 
-        public static implicit operator MolarEntropy(UnknownUnit Unit)
-        {
-            MolarEntropy local = new MolarEntropy(0, MolarEntropyUnit.SI);
-
-            local.Transform(Unit);
-            return local;
-        }
-
-
-
+        public static implicit operator MolarEntropy(UnknownUnit Unit) => new MolarEntropy(Unit, MolarEntropyUnit.SI);
 
     }
 }
