691c167885ef334d30ba1e83b5f948283cb30b85
Mads Foged <jfogedm@jci.com>
2021-09-06 14:45:01 +0200

    ...

--- EngineeringUnits/CombinedUnits/Torque/Torque.cs
+++ EngineeringUnits/CombinedUnits/Torque/Torque.cs
@@ -5,24 +5,20 @@ namespace EngineeringUnits
     public partial class Torque : BaseUnit
     {
 
-        public Torque()
-        {
-            Unit = TorqueUnit.SI.Unit;
-        }
-
-
+        public Torque() { }
         public Torque(decimal value, TorqueUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public Torque(double value, TorqueUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public Torque(int value, TorqueUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public Torque(UnknownUnit value) : base(value) { }
 
+        public Torque(UnknownUnit value, TorqueUnit selectedUnit) : base(value, selectedUnit.Unit) { }
 
         public static Torque From(double value, TorqueUnit unit) => new Torque(value, unit);
         public double As(TorqueUnit ReturnInThisUnit) => (double)ToTheOutSide(ReturnInThisUnit.Unit);
         public Torque ToUnit(TorqueUnit selectedUnit) => new Torque(ToTheOutSide(selectedUnit.Unit), selectedUnit);
         public static Torque Zero => new Torque(0, TorqueUnit.SI);
 
-        public static implicit operator Torque(UnknownUnit Unit) => new Torque(Unit);
+        public static implicit operator Torque(UnknownUnit Unit) => new Torque(Unit, TorqueUnit.SI);
 
     }
 }
