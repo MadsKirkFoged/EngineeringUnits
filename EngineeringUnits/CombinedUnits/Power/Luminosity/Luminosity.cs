935c73a97553292a5370dfaf2f9d5bbcca80fc7f
Mads Foged <jfogedm@jci.com>
2021-09-06 12:42:59 +0200

    ..

--- EngineeringUnits/CombinedUnits/Power/Luminosity/Luminosity.cs
+++ EngineeringUnits/CombinedUnits/Power/Luminosity/Luminosity.cs
@@ -8,21 +8,29 @@ namespace EngineeringUnits
 
         public Luminosity()
         {
-            Unit = LuminosityUnit.SI.Unit;
+            Unit = PowerUnit.SI.Unit.Copy();
+        }
+
+        public Luminosity(decimal value, PowerUnit selectedUnit) : base(value, selectedUnit.Unit) { }
+        public Luminosity(double value, PowerUnit selectedUnit) : base(value, selectedUnit.Unit) { }
+        public Luminosity(int value, PowerUnit selectedUnit) : base(value, selectedUnit.Unit) { }
+
+
+        public static Luminosity From(double value, PowerUnit unit) => new Luminosity(value, unit);
+        public double As(PowerUnit ReturnInThisUnit) => (double)ToTheOutSide(ReturnInThisUnit.Unit);
+        public Luminosity ToUnit(PowerUnit selectedUnit) => new Luminosity(ToTheOutSide(selectedUnit.Unit), selectedUnit);
+        public static Luminosity Zero => new Luminosity(0, PowerUnit.SI);
+
+        public static implicit operator Luminosity(UnknownUnit Unit)
+        {
+            Luminosity local = new Luminosity(0, PowerUnit.SI);
+
+            local.Transform(Unit);
+            return local;
         }
 
 
-        public Luminosity(decimal value, LuminosityUnit selectedUnit) : base(value, selectedUnit.Unit) { }
-        public Luminosity(double value, LuminosityUnit selectedUnit) : base(value, selectedUnit.Unit) { }
-        public Luminosity(int value, LuminosityUnit selectedUnit) : base(value, selectedUnit.Unit) { }
-        public Luminosity(UnknownUnit value) : base(value) { }
 
 
-        public static Luminosity From(double value, LuminosityUnit unit) => new Luminosity(value, unit);
-        public double As(LuminosityUnit ReturnInThisUnit) => (double)ToTheOutSide(ReturnInThisUnit.Unit);
-        public Luminosity ToUnit(LuminosityUnit selectedUnit) => new Luminosity(ToTheOutSide(selectedUnit.Unit), selectedUnit);
-        public static Luminosity Zero => new Luminosity(0, LuminosityUnit.SI);
-
-        public static implicit operator Luminosity(UnknownUnit Unit) => new Luminosity(Unit);
     }
 }
