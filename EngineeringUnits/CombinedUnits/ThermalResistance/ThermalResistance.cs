baf23c772c9069ce64d626bfdcaf4446b95c0b53
Mads Foged <jfogedm@jci.com>
2021-09-06 14:44:34 +0200

    ...

--- EngineeringUnits/CombinedUnits/ThermalResistance/ThermalResistance.cs
+++ EngineeringUnits/CombinedUnits/ThermalResistance/ThermalResistance.cs
@@ -5,23 +5,19 @@ namespace EngineeringUnits
     public partial class ThermalResistance : BaseUnit
     {
 
-        public ThermalResistance()
-        {
-            Unit = ThermalResistanceUnit.SI.Unit;
-        }
-
-
+        public ThermalResistance() { }
         public ThermalResistance(decimal value, ThermalResistanceUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public ThermalResistance(double value, ThermalResistanceUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public ThermalResistance(int value, ThermalResistanceUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public ThermalResistance(UnknownUnit value) : base(value) { }
 
+        public ThermalResistance(UnknownUnit value, ThermalResistanceUnit selectedUnit) : base(value, selectedUnit.Unit) { }
 
         public static ThermalResistance From(double value, ThermalResistanceUnit unit) => new ThermalResistance(value, unit);
         public double As(ThermalResistanceUnit ReturnInThisUnit) => (double)ToTheOutSide(ReturnInThisUnit.Unit);
         public ThermalResistance ToUnit(ThermalResistanceUnit selectedUnit) => new ThermalResistance(ToTheOutSide(selectedUnit.Unit), selectedUnit);
         public static ThermalResistance Zero => new ThermalResistance(0, ThermalResistanceUnit.SI);
 
-        public static implicit operator ThermalResistance(UnknownUnit Unit) => new ThermalResistance(Unit);
+        public static implicit operator ThermalResistance(UnknownUnit Unit) => new ThermalResistance(Unit, ThermalResistanceUnit.SI);
     }
 }
