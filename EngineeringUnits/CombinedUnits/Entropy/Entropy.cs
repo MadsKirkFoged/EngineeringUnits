f92f6c4b7d7878c85ab4422d31faf118543bb4ba
Mads Foged <jfogedm@jci.com>
2021-09-06 14:15:07 +0200

    ..

--- EngineeringUnits/CombinedUnits/Entropy/Entropy.cs
+++ EngineeringUnits/CombinedUnits/Entropy/Entropy.cs
@@ -1,38 +1,25 @@
-﻿using Fractions;
-using EngineeringUnits.Units;
-using System.Collections.Generic;
-using System.Text;
-using EngineeringUnits.Units;
+﻿using EngineeringUnits.Units;
+
 
 namespace EngineeringUnits
 {
     public partial class Entropy : BaseUnit
     {
 
-        public Entropy()
-        {
-            Unit = EntropyUnit.SI.Unit.Copy();
-        }
-
+        public Entropy() { }
         public Entropy(decimal value, EntropyUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public Entropy(double value, EntropyUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public Entropy(int value, EntropyUnit selectedUnit) : base(value, selectedUnit.Unit) { }
+        public Entropy(UnknownUnit value) : base(value) { }
 
+        public Entropy(UnknownUnit value, EntropyUnit selectedUnit) : base(value, selectedUnit.Unit) { }
 
         public static Entropy From(double value, EntropyUnit unit) => new Entropy(value, unit);
         public double As(EntropyUnit ReturnInThisUnit) => (double)ToTheOutSide(ReturnInThisUnit.Unit);
         public Entropy ToUnit(EntropyUnit selectedUnit) => new Entropy(ToTheOutSide(selectedUnit.Unit), selectedUnit);
         public static Entropy Zero => new Entropy(0, EntropyUnit.SI);
 
-        public static implicit operator Entropy(UnknownUnit Unit)
-        {
-            Entropy local = new Entropy(0, EntropyUnit.SI);
-
-            local.Transform(Unit);
-            return local;
-        }
-
-
+        public static implicit operator Entropy(UnknownUnit Unit) => new Entropy(Unit, EntropyUnit.SI);
 
 
     }
