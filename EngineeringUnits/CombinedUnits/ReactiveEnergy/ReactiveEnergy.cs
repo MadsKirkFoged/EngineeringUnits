21c8684f1db06d9c5ba46f5977ab9078afabc265
Mads Foged <jfogedm@jci.com>
2021-09-06 12:38:17 +0200

    ..

--- EngineeringUnits/CombinedUnits/ReactiveEnergy/ReactiveEnergy.cs
+++ EngineeringUnits/CombinedUnits/ReactiveEnergy/ReactiveEnergy.cs
@@ -1,8 +1,5 @@
 ﻿using EngineeringUnits.Units;
-using Fractions;
-using EngineeringUnits.Units;
-using System.Collections.Generic;
-using System.Text;
+
 
 namespace EngineeringUnits
 {
@@ -11,12 +8,14 @@ namespace EngineeringUnits
 
         public ReactiveEnergy()
         {
-            Unit = ReactiveEnergyUnit.SI.Unit.Copy();
+            Unit = ReactiveEnergyUnit.SI.Unit;
         }
+
 
         public ReactiveEnergy(decimal value, ReactiveEnergyUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public ReactiveEnergy(double value, ReactiveEnergyUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public ReactiveEnergy(int value, ReactiveEnergyUnit selectedUnit) : base(value, selectedUnit.Unit) { }
+        public ReactiveEnergy(UnknownUnit value) : base(value) { }
 
 
         public static ReactiveEnergy From(double value, ReactiveEnergyUnit unit) => new ReactiveEnergy(value, unit);
@@ -24,16 +23,6 @@ namespace EngineeringUnits
         public ReactiveEnergy ToUnit(ReactiveEnergyUnit selectedUnit) => new ReactiveEnergy(ToTheOutSide(selectedUnit.Unit), selectedUnit);
         public static ReactiveEnergy Zero => new ReactiveEnergy(0, ReactiveEnergyUnit.SI);
 
-        public static implicit operator ReactiveEnergy(UnknownUnit Unit)
-        {
-            ReactiveEnergy local = new ReactiveEnergy(0, ReactiveEnergyUnit.SI);
-
-            local.Transform(Unit);
-            return local;
-        }
-
-
-
-
+        public static implicit operator ReactiveEnergy(UnknownUnit Unit) => new ReactiveEnergy(Unit);
     }
 }
