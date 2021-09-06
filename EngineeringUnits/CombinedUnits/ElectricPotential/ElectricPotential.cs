0c71f7b40ab1bf3a881ef19a0611ffe1541034c4
Mads Foged <jfogedm@jci.com>
2021-09-06 14:18:30 +0200

    ...

--- EngineeringUnits/CombinedUnits/ElectricPotential/ElectricPotential.cs
+++ EngineeringUnits/CombinedUnits/ElectricPotential/ElectricPotential.cs
@@ -1,38 +1,25 @@
-﻿using Fractions;
-using EngineeringUnits.Units;
-using System.Collections.Generic;
-using System.Text;
-using EngineeringUnits.Units;
+﻿using EngineeringUnits.Units;
+
 
 namespace EngineeringUnits
 {
     public partial class ElectricPotential : BaseUnit
     {
 
-        public ElectricPotential()
-        {
-            Unit = ElectricPotentialUnit.SI.Unit.Copy();
-        }
-
+        public ElectricPotential() { }
         public ElectricPotential(decimal value, ElectricPotentialUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public ElectricPotential(double value, ElectricPotentialUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public ElectricPotential(int value, ElectricPotentialUnit selectedUnit) : base(value, selectedUnit.Unit) { }
+        public ElectricPotential(UnknownUnit value) : base(value) { }
 
+        public ElectricPotential(UnknownUnit value, ElectricPotentialUnit selectedUnit) : base(value, selectedUnit.Unit) { }
 
         public static ElectricPotential From(double value, ElectricPotentialUnit unit) => new ElectricPotential(value, unit);
         public double As(ElectricPotentialUnit ReturnInThisUnit) => (double)ToTheOutSide(ReturnInThisUnit.Unit);
         public ElectricPotential ToUnit(ElectricPotentialUnit selectedUnit) => new ElectricPotential(ToTheOutSide(selectedUnit.Unit), selectedUnit);
         public static ElectricPotential Zero => new ElectricPotential(0, ElectricPotentialUnit.SI);
 
-        public static implicit operator ElectricPotential(UnknownUnit Unit)
-        {
-            ElectricPotential local = new ElectricPotential(0, ElectricPotentialUnit.SI);
-
-            local.Transform(Unit);
-            return local;
-        }
-
-
+        public static implicit operator ElectricPotential(UnknownUnit Unit) => new ElectricPotential(Unit, ElectricPotentialUnit.SI);
 
 
     }
