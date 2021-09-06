f2e838ac2574ebf6070ad911e917fa293f40c752
Mads Foged <jfogedm@jci.com>
2021-09-06 14:07:56 +0200

    ..

--- EngineeringUnits/CombinedUnits/LapseRate/LapseRate.cs
+++ EngineeringUnits/CombinedUnits/LapseRate/LapseRate.cs
@@ -1,39 +1,24 @@
-﻿using Fractions;
-using EngineeringUnits.Units;
-using System.Collections.Generic;
-using System.Text;
-using EngineeringUnits.Units;
+﻿using EngineeringUnits.Units;
 
 namespace EngineeringUnits
 {
     public partial class LapseRate : BaseUnit
     {
 
-        public LapseRate()
-        {
-            Unit = LapseRateUnit.SI.Unit.Copy();
-        }
-
+        public LapseRate() { }
         public LapseRate(decimal value, LapseRateUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public LapseRate(double value, LapseRateUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public LapseRate(int value, LapseRateUnit selectedUnit) : base(value, selectedUnit.Unit) { }
+        public LapseRate(UnknownUnit value) : base(value) { }
 
+        public LapseRate(UnknownUnit value, LapseRateUnit selectedUnit) : base(value, selectedUnit.Unit) { }
 
         public static LapseRate From(double value, LapseRateUnit unit) => new LapseRate(value, unit);
         public double As(LapseRateUnit ReturnInThisUnit) => (double)ToTheOutSide(ReturnInThisUnit.Unit);
         public LapseRate ToUnit(LapseRateUnit selectedUnit) => new LapseRate(ToTheOutSide(selectedUnit.Unit), selectedUnit);
         public static LapseRate Zero => new LapseRate(0, LapseRateUnit.SI);
 
-        public static implicit operator LapseRate(UnknownUnit Unit)
-        {
-            LapseRate local = new LapseRate(0, LapseRateUnit.SI);
-
-            local.Transform(Unit);
-            return local;
-        }
-
-
-
+        public static implicit operator LapseRate(UnknownUnit Unit) => new LapseRate(Unit, LapseRateUnit.SI);
 
     }
 }
