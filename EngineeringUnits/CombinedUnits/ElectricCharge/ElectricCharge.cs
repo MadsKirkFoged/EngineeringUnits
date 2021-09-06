323aa92ed1414522fc055693d71cde4ee40e5c3b
Mads Foged <jfogedm@jci.com>
2021-09-06 14:24:25 +0200

    ..

--- EngineeringUnits/CombinedUnits/ElectricCharge/ElectricCharge.cs
+++ EngineeringUnits/CombinedUnits/ElectricCharge/ElectricCharge.cs
@@ -1,38 +1,25 @@
-﻿using Fractions;
-using EngineeringUnits.Units;
-using System.Collections.Generic;
-using System.Text;
-using EngineeringUnits.Units;
+﻿using EngineeringUnits.Units;
+
 
 namespace EngineeringUnits
 {
     public partial class ElectricCharge : BaseUnit
     {
 
-        public ElectricCharge()
-        {
-            Unit = ElectricChargeUnit.SI.Unit.Copy();
-        }
-
+        public ElectricCharge() { }
         public ElectricCharge(decimal value, ElectricChargeUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public ElectricCharge(double value, ElectricChargeUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public ElectricCharge(int value, ElectricChargeUnit selectedUnit) : base(value, selectedUnit.Unit) { }
+        public ElectricCharge(UnknownUnit value) : base(value) { }
 
+        public ElectricCharge(UnknownUnit value, ElectricChargeUnit selectedUnit) : base(value, selectedUnit.Unit) { }
 
         public static ElectricCharge From(double value, ElectricChargeUnit unit) => new ElectricCharge(value, unit);
         public double As(ElectricChargeUnit ReturnInThisUnit) => (double)ToTheOutSide(ReturnInThisUnit.Unit);
         public ElectricCharge ToUnit(ElectricChargeUnit selectedUnit) => new ElectricCharge(ToTheOutSide(selectedUnit.Unit), selectedUnit);
         public static ElectricCharge Zero => new ElectricCharge(0, ElectricChargeUnit.SI);
 
-        public static implicit operator ElectricCharge(UnknownUnit Unit)
-        {
-            ElectricCharge local = new ElectricCharge(0, ElectricChargeUnit.SI);
-
-            local.Transform(Unit);
-            return local;
-        }
-
-
+        public static implicit operator ElectricCharge(UnknownUnit Unit) => new ElectricCharge(Unit, ElectricChargeUnit.SI);
 
 
     }
