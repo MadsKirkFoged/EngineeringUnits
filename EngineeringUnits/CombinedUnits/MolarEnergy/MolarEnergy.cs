75ad6b63dfcfab6b01b310a28998f9ea278c0b37
Mads Foged <jfogedm@jci.com>
2021-09-06 13:59:05 +0200

    ...

--- EngineeringUnits/CombinedUnits/MolarEnergy/MolarEnergy.cs
+++ EngineeringUnits/CombinedUnits/MolarEnergy/MolarEnergy.cs
@@ -1,39 +1,25 @@
-﻿using Fractions;
-using EngineeringUnits.Units;
-using System.Collections.Generic;
-using System.Text;
-using EngineeringUnits.Units;
+﻿using EngineeringUnits.Units;
+
 
 namespace EngineeringUnits
 {
     public partial class MolarEnergy : BaseUnit
     {
 
-        public MolarEnergy()
-        {
-            Unit = MolarEnergyUnit.SI.Unit.Copy();
-        }
-
+        public MolarEnergy() { }
         public MolarEnergy(decimal value, MolarEnergyUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public MolarEnergy(double value, MolarEnergyUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public MolarEnergy(int value, MolarEnergyUnit selectedUnit) : base(value, selectedUnit.Unit) { }
+        public MolarEnergy(UnknownUnit value) : base(value) { }
 
+        public MolarEnergy(UnknownUnit value, MolarEnergyUnit selectedUnit) : base(value, selectedUnit.Unit) { }
 
         public static MolarEnergy From(double value, MolarEnergyUnit unit) => new MolarEnergy(value, unit);
         public double As(MolarEnergyUnit ReturnInThisUnit) => (double)ToTheOutSide(ReturnInThisUnit.Unit);
         public MolarEnergy ToUnit(MolarEnergyUnit selectedUnit) => new MolarEnergy(ToTheOutSide(selectedUnit.Unit), selectedUnit);
         public static MolarEnergy Zero => new MolarEnergy(0, MolarEnergyUnit.SI);
 
-        public static implicit operator MolarEnergy(UnknownUnit Unit)
-        {
-            MolarEnergy local = new MolarEnergy(0, MolarEnergyUnit.SI);
-
-            local.Transform(Unit);
-            return local;
-        }
-
-
-
+        public static implicit operator MolarEnergy(UnknownUnit Unit) => new MolarEnergy(Unit, MolarEnergyUnit.SI);
 
     }
 }
