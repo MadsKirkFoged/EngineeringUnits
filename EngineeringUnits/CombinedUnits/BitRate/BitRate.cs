6de0bbe72d53b31173f3d3d96dbf0aa603db7f6d
Mads Foged <jfogedm@jci.com>
2021-09-06 14:28:53 +0200

    ..

--- EngineeringUnits/CombinedUnits/BitRate/BitRate.cs
+++ EngineeringUnits/CombinedUnits/BitRate/BitRate.cs
@@ -1,39 +1,25 @@
-﻿using Fractions;
-using EngineeringUnits.Units;
-using System.Collections.Generic;
-using System.Text;
-using EngineeringUnits.Units;
+﻿using EngineeringUnits.Units;
+
 
 namespace EngineeringUnits
 {
     public partial class BitRate : BaseUnit
     {
 
-        public BitRate()
-        {
-            Unit = BitRateUnit.SI.Unit.Copy();
-        }
-
+        public BitRate() { }
         public BitRate(decimal value, BitRateUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public BitRate(double value, BitRateUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public BitRate(int value, BitRateUnit selectedUnit) : base(value, selectedUnit.Unit) { }
+        public BitRate(UnknownUnit value) : base(value) { }
 
+        public BitRate(UnknownUnit value, BitRateUnit selectedUnit) : base(value, selectedUnit.Unit) { }
 
         public static BitRate From(double value, BitRateUnit unit) => new BitRate(value, unit);
         public double As(BitRateUnit ReturnInThisUnit) => (double)ToTheOutSide(ReturnInThisUnit.Unit);
         public BitRate ToUnit(BitRateUnit selectedUnit) => new BitRate(ToTheOutSide(selectedUnit.Unit), selectedUnit);
         public static BitRate Zero => new BitRate(0, BitRateUnit.SI);
 
-        public static implicit operator BitRate(UnknownUnit Unit)
-        {
-            BitRate local = new BitRate(0, BitRateUnit.SI);
-
-            local.Transform(Unit);
-            return local;
-        }
-
-
-
+        public static implicit operator BitRate(UnknownUnit Unit) => new BitRate(Unit, BitRateUnit.SI);
 
     }
 }
