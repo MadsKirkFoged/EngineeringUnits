48cd2c1ce3d54e68995a05b5f60a466dab4ccf14
Mads Foged <jfogedm@jci.com>
2021-09-06 12:30:40 +0200

    ..

--- EngineeringUnits/CombinedUnits/TemperatureChangeRate/TemperatureChangeRate.cs
+++ EngineeringUnits/CombinedUnits/TemperatureChangeRate/TemperatureChangeRate.cs
@@ -1,22 +1,20 @@
 ﻿using EngineeringUnits.Units;
-using Fractions;
-using EngineeringUnits.Units;
-using System.Collections.Generic;
-using System.Text;
+
 
 namespace EngineeringUnits
 {
     public partial class TemperatureChangeRate : BaseUnit
     {
-
         public TemperatureChangeRate()
         {
-            Unit = TemperatureChangeRateUnit.SI.Unit.Copy();
+            Unit = TemperatureChangeRateUnit.SI.Unit;
         }
+
 
         public TemperatureChangeRate(decimal value, TemperatureChangeRateUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public TemperatureChangeRate(double value, TemperatureChangeRateUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public TemperatureChangeRate(int value, TemperatureChangeRateUnit selectedUnit) : base(value, selectedUnit.Unit) { }
+        public TemperatureChangeRate(UnknownUnit value) : base(value) { }
 
 
         public static TemperatureChangeRate From(double value, TemperatureChangeRateUnit unit) => new TemperatureChangeRate(value, unit);
@@ -24,16 +22,6 @@ namespace EngineeringUnits
         public TemperatureChangeRate ToUnit(TemperatureChangeRateUnit selectedUnit) => new TemperatureChangeRate(ToTheOutSide(selectedUnit.Unit), selectedUnit);
         public static TemperatureChangeRate Zero => new TemperatureChangeRate(0, TemperatureChangeRateUnit.SI);
 
-        public static implicit operator TemperatureChangeRate(UnknownUnit Unit)
-        {
-            TemperatureChangeRate local = new TemperatureChangeRate(0, TemperatureChangeRateUnit.SI);
-
-            local.Transform(Unit);
-            return local;
-        }
-
-
-
-
+        public static implicit operator TemperatureChangeRate(UnknownUnit Unit) => new TemperatureChangeRate(Unit);
     }
 }
