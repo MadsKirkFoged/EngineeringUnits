802d2edd9d789ba35c7636fa393f3d0265223200
Mads Foged <jfogedm@jci.com>
2021-09-06 14:09:49 +0200

    ..

--- EngineeringUnits/CombinedUnits/Irradiance/Irradiance.cs
+++ EngineeringUnits/CombinedUnits/Irradiance/Irradiance.cs
@@ -1,39 +1,25 @@
-﻿using Fractions;
-using EngineeringUnits.Units;
-using System.Collections.Generic;
-using System.Text;
-using EngineeringUnits.Units;
+﻿using EngineeringUnits.Units;
+
 
 namespace EngineeringUnits
 {
     public partial class Irradiance : BaseUnit
     {
 
-        public Irradiance()
-        {
-            Unit = IrradianceUnit.SI.Unit.Copy();
-        }
-
+        public Irradiance() { }
         public Irradiance(decimal value, IrradianceUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public Irradiance(double value, IrradianceUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public Irradiance(int value, IrradianceUnit selectedUnit) : base(value, selectedUnit.Unit) { }
+        public Irradiance(UnknownUnit value) : base(value) { }
 
+        public Irradiance(UnknownUnit value, IrradianceUnit selectedUnit) : base(value, selectedUnit.Unit) { }
 
         public static Irradiance From(double value, IrradianceUnit unit) => new Irradiance(value, unit);
         public double As(IrradianceUnit ReturnInThisUnit) => (double)ToTheOutSide(ReturnInThisUnit.Unit);
         public Irradiance ToUnit(IrradianceUnit selectedUnit) => new Irradiance(ToTheOutSide(selectedUnit.Unit), selectedUnit);
         public static Irradiance Zero => new Irradiance(0, IrradianceUnit.SI);
 
-        public static implicit operator Irradiance(UnknownUnit Unit)
-        {
-            Irradiance local = new Irradiance(0, IrradianceUnit.SI);
-
-            local.Transform(Unit);
-            return local;
-        }
-
-
-
+        public static implicit operator Irradiance(UnknownUnit Unit) => new Irradiance(Unit, IrradianceUnit.SI);
 
     }
 }
