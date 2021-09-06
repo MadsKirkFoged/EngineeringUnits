a8d306b37dfacd1873fa87fe2daa66c604a24aab
Mads Foged <jfogedm@jci.com>
2021-09-06 14:32:05 +0200

    ...

--- EngineeringUnits/CombinedUnits/ApparentPower/ApparentPower.cs
+++ EngineeringUnits/CombinedUnits/ApparentPower/ApparentPower.cs
@@ -1,38 +1,25 @@
-﻿using Fractions;
-using EngineeringUnits.Units;
-using System.Collections.Generic;
-using System.Text;
-using EngineeringUnits.Units;
+﻿using EngineeringUnits.Units;
+
 
 namespace EngineeringUnits
 {
     public partial class ApparentPower : BaseUnit
     {
 
-        public ApparentPower()
-        {
-            Unit = ApparentPowerUnit.SI.Unit.Copy();
-        }
-
+        public ApparentPower() { }
         public ApparentPower(decimal value, ApparentPowerUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public ApparentPower(double value, ApparentPowerUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public ApparentPower(int value, ApparentPowerUnit selectedUnit) : base(value, selectedUnit.Unit) { }
+        public ApparentPower(UnknownUnit value) : base(value) { }
 
+        public ApparentPower(UnknownUnit value, ApparentPowerUnit selectedUnit) : base(value, selectedUnit.Unit) { }
 
         public static ApparentPower From(double value, ApparentPowerUnit unit) => new ApparentPower(value, unit);
         public double As(ApparentPowerUnit ReturnInThisUnit) => (double)ToTheOutSide(ReturnInThisUnit.Unit);
         public ApparentPower ToUnit(ApparentPowerUnit selectedUnit) => new ApparentPower(ToTheOutSide(selectedUnit.Unit), selectedUnit);
         public static ApparentPower Zero => new ApparentPower(0, ApparentPowerUnit.SI);
 
-        public static implicit operator ApparentPower(UnknownUnit Unit)
-        {
-            ApparentPower local = new ApparentPower(0, ApparentPowerUnit.SI);
-
-            local.Transform(Unit);
-            return local;
-        }
-
-
+        public static implicit operator ApparentPower(UnknownUnit Unit) => new ApparentPower(Unit, ApparentPowerUnit.SI);
 
 
     }
