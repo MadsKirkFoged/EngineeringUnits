750fc9e88d3959d2b9daa5d75afba4326bc5f231
Mads Foged <jfogedm@jci.com>
2021-09-06 14:22:54 +0200

    ..

--- EngineeringUnits/CombinedUnits/ElectricConductivity/ElectricConductivity.cs
+++ EngineeringUnits/CombinedUnits/ElectricConductivity/ElectricConductivity.cs
@@ -1,38 +1,25 @@
-﻿using Fractions;
-using EngineeringUnits.Units;
-using System.Collections.Generic;
-using System.Text;
-using EngineeringUnits.Units;
+﻿using EngineeringUnits.Units;
+
 
 namespace EngineeringUnits
 {
     public partial class ElectricConductivity : BaseUnit
     {
 
-        public ElectricConductivity()
-        {
-            Unit = ElectricConductivityUnit.SI.Unit.Copy();
-        }
-
+        public ElectricConductivity() { }
         public ElectricConductivity(decimal value, ElectricConductivityUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public ElectricConductivity(double value, ElectricConductivityUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public ElectricConductivity(int value, ElectricConductivityUnit selectedUnit) : base(value, selectedUnit.Unit) { }
+        public ElectricConductivity(UnknownUnit value) : base(value) { }
 
+        public ElectricConductivity(UnknownUnit value, ElectricConductivityUnit selectedUnit) : base(value, selectedUnit.Unit) { }
 
         public static ElectricConductivity From(double value, ElectricConductivityUnit unit) => new ElectricConductivity(value, unit);
         public double As(ElectricConductivityUnit ReturnInThisUnit) => (double)ToTheOutSide(ReturnInThisUnit.Unit);
         public ElectricConductivity ToUnit(ElectricConductivityUnit selectedUnit) => new ElectricConductivity(ToTheOutSide(selectedUnit.Unit), selectedUnit);
         public static ElectricConductivity Zero => new ElectricConductivity(0, ElectricConductivityUnit.SI);
 
-        public static implicit operator ElectricConductivity(UnknownUnit Unit)
-        {
-            ElectricConductivity local = new ElectricConductivity(0, ElectricConductivityUnit.SI);
-
-            local.Transform(Unit);
-            return local;
-        }
-
-
+        public static implicit operator ElectricConductivity(UnknownUnit Unit) => new ElectricConductivity(Unit, ElectricConductivityUnit.SI);
 
 
     }
