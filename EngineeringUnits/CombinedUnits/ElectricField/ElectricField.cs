873ebe77de1898c68d8084ae8701bc9f27067a36
Mads Foged <jfogedm@jci.com>
2021-09-06 14:21:23 +0200

    ..

--- EngineeringUnits/CombinedUnits/ElectricField/ElectricField.cs
+++ EngineeringUnits/CombinedUnits/ElectricField/ElectricField.cs
@@ -1,38 +1,25 @@
-﻿using Fractions;
-using EngineeringUnits.Units;
-using System.Collections.Generic;
-using System.Text;
-using EngineeringUnits.Units;
+﻿using EngineeringUnits.Units;
+
 
 namespace EngineeringUnits
 {
     public partial class ElectricField : BaseUnit
     {
 
-        public ElectricField()
-        {
-            Unit = ElectricFieldUnit.SI.Unit.Copy();
-        }
-
+        public ElectricField() { }
         public ElectricField(decimal value, ElectricFieldUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public ElectricField(double value, ElectricFieldUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public ElectricField(int value, ElectricFieldUnit selectedUnit) : base(value, selectedUnit.Unit) { }
+        public ElectricField(UnknownUnit value) : base(value) { }
 
+        public ElectricField(UnknownUnit value, ElectricFieldUnit selectedUnit) : base(value, selectedUnit.Unit) { }
 
         public static ElectricField From(double value, ElectricFieldUnit unit) => new ElectricField(value, unit);
         public double As(ElectricFieldUnit ReturnInThisUnit) => (double)ToTheOutSide(ReturnInThisUnit.Unit);
         public ElectricField ToUnit(ElectricFieldUnit selectedUnit) => new ElectricField(ToTheOutSide(selectedUnit.Unit), selectedUnit);
         public static ElectricField Zero => new ElectricField(0, ElectricFieldUnit.SI);
 
-        public static implicit operator ElectricField(UnknownUnit Unit)
-        {
-            ElectricField local = new ElectricField(0, ElectricFieldUnit.SI);
-
-            local.Transform(Unit);
-            return local;
-        }
-
-
+        public static implicit operator ElectricField(UnknownUnit Unit) => new ElectricField(Unit, ElectricFieldUnit.SI);
 
 
     }
