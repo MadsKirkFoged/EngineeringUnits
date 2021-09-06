a5594f4e36661f30d52468543d732ae2bb87da49
Mads Foged <jfogedm@jci.com>
2021-09-06 14:46:26 +0200

    ...

--- EngineeringUnits/CombinedUnits/VolumeFlow/VolumeFlow.cs
+++ EngineeringUnits/CombinedUnits/VolumeFlow/VolumeFlow.cs
@@ -6,24 +6,20 @@ namespace EngineeringUnits
     public partial class VolumeFlow : BaseUnit
     {
 
-        public VolumeFlow()
-        {
-            Unit = VolumeFlowUnit.SI.Unit;
-        }
-
-
+        public VolumeFlow() { }
         public VolumeFlow(decimal value, VolumeFlowUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public VolumeFlow(double value, VolumeFlowUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public VolumeFlow(int value, VolumeFlowUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public VolumeFlow(UnknownUnit value) : base(value) { }
 
+        public VolumeFlow(UnknownUnit value, VolumeFlowUnit selectedUnit) : base(value, selectedUnit.Unit) { }
 
         public static VolumeFlow From(double value, VolumeFlowUnit unit) => new VolumeFlow(value, unit);
         public double As(VolumeFlowUnit ReturnInThisUnit) => (double)ToTheOutSide(ReturnInThisUnit.Unit);
         public VolumeFlow ToUnit(VolumeFlowUnit selectedUnit) => new VolumeFlow(ToTheOutSide(selectedUnit.Unit), selectedUnit);
         public static VolumeFlow Zero => new VolumeFlow(0, VolumeFlowUnit.SI);
 
-        public static implicit operator VolumeFlow(UnknownUnit Unit) => new VolumeFlow(Unit);
+        public static implicit operator VolumeFlow(UnknownUnit Unit) => new VolumeFlow(Unit, VolumeFlowUnit.SI);
 
     }
 }
