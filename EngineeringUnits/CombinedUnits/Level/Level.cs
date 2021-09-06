0c7cffa86480d59563a88370e1157bfc3a426a8f
Mads Foged <jfogedm@jci.com>
2021-09-06 14:07:19 +0200

    ..

--- EngineeringUnits/CombinedUnits/Level/Level.cs
+++ EngineeringUnits/CombinedUnits/Level/Level.cs
@@ -1,38 +1,25 @@
-﻿using Fractions;
-using EngineeringUnits.Units;
-using System.Collections.Generic;
-using System.Text;
-using EngineeringUnits.Units;
+﻿using EngineeringUnits.Units;
+
 
 namespace EngineeringUnits
 {
     public partial class Level : BaseUnit
     {
 
-        public Level()
-        {
-            Unit = LevelUnit.SI.Unit.Copy();
-        }
-
+        public Level() { }
         public Level(decimal value, LevelUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public Level(double value, LevelUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public Level(int value, LevelUnit selectedUnit) : base(value, selectedUnit.Unit) { }
+        public Level(UnknownUnit value) : base(value) { }
 
+        public Level(UnknownUnit value, LevelUnit selectedUnit) : base(value, selectedUnit.Unit) { }
 
         public static Level From(double value, LevelUnit unit) => new Level(value, unit);
         public double As(LevelUnit ReturnInThisUnit) => (double)ToTheOutSide(ReturnInThisUnit.Unit);
         public Level ToUnit(LevelUnit selectedUnit) => new Level(ToTheOutSide(selectedUnit.Unit), selectedUnit);
         public static Level Zero => new Level(0, LevelUnit.SI);
 
-        public static implicit operator Level(UnknownUnit Unit)
-        {
-            Level local = new Level(0, LevelUnit.SI);
-
-            local.Transform(Unit);
-            return local;
-        }
-
-
+        public static implicit operator Level(UnknownUnit Unit) => new Level(Unit, LevelUnit.SI);
 
 
     }
