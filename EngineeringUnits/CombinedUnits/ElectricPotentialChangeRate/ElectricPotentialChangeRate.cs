bcdc83fda2a4a75e7b45ab02b975234ea131edea
Mads Foged <jfogedm@jci.com>
2021-09-06 14:18:01 +0200

    ..

--- EngineeringUnits/CombinedUnits/ElectricPotentialChangeRate/ElectricPotentialChangeRate.cs
+++ EngineeringUnits/CombinedUnits/ElectricPotentialChangeRate/ElectricPotentialChangeRate.cs
@@ -1,38 +1,25 @@
-﻿using Fractions;
-using EngineeringUnits.Units;
-using System.Collections.Generic;
-using System.Text;
-using EngineeringUnits.Units;
+﻿using EngineeringUnits.Units;
+
 
 namespace EngineeringUnits
 {
     public partial class ElectricPotentialChangeRate : BaseUnit
     {
 
-        public ElectricPotentialChangeRate()
-        {
-            Unit = ElectricPotentialChangeRateUnit.SI.Unit.Copy();
-        }
-
+        public ElectricPotentialChangeRate() { }
         public ElectricPotentialChangeRate(decimal value, ElectricPotentialChangeRateUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public ElectricPotentialChangeRate(double value, ElectricPotentialChangeRateUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public ElectricPotentialChangeRate(int value, ElectricPotentialChangeRateUnit selectedUnit) : base(value, selectedUnit.Unit) { }
+        public ElectricPotentialChangeRate(UnknownUnit value) : base(value) { }
 
+        public ElectricPotentialChangeRate(UnknownUnit value, ElectricPotentialChangeRateUnit selectedUnit) : base(value, selectedUnit.Unit) { }
 
         public static ElectricPotentialChangeRate From(double value, ElectricPotentialChangeRateUnit unit) => new ElectricPotentialChangeRate(value, unit);
         public double As(ElectricPotentialChangeRateUnit ReturnInThisUnit) => (double)ToTheOutSide(ReturnInThisUnit.Unit);
         public ElectricPotentialChangeRate ToUnit(ElectricPotentialChangeRateUnit selectedUnit) => new ElectricPotentialChangeRate(ToTheOutSide(selectedUnit.Unit), selectedUnit);
         public static ElectricPotentialChangeRate Zero => new ElectricPotentialChangeRate(0, ElectricPotentialChangeRateUnit.SI);
 
-        public static implicit operator ElectricPotentialChangeRate(UnknownUnit Unit)
-        {
-            ElectricPotentialChangeRate local = new ElectricPotentialChangeRate(0, ElectricPotentialChangeRateUnit.SI);
-
-            local.Transform(Unit);
-            return local;
-        }
-
-
+        public static implicit operator ElectricPotentialChangeRate(UnknownUnit Unit) => new ElectricPotentialChangeRate(Unit, ElectricPotentialChangeRateUnit.SI);
 
 
     }
