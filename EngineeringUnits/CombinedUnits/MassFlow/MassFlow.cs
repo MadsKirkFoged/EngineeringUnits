ca0d3fa42d5088ef08a2813b9dfad96e126b6ae2
Mads Foged <jfogedm@jci.com>
2021-09-06 14:01:09 +0200

    ...

--- EngineeringUnits/CombinedUnits/MassFlow/MassFlow.cs
+++ EngineeringUnits/CombinedUnits/MassFlow/MassFlow.cs
@@ -1,9 +1,5 @@
-﻿using Fractions;
-using EngineeringUnits.Units;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Text;
-using EngineeringUnits.Units;
+﻿using EngineeringUnits.Units;
+
 
 namespace EngineeringUnits
 {
@@ -11,34 +7,20 @@ namespace EngineeringUnits
     public partial class MassFlow : BaseUnit
     {
 
-        public MassFlow()
-        {
-            Unit = MassFlowUnit.SI.Unit.Copy();
-        }
-
-
+        public MassFlow() { }
         public MassFlow(decimal value, MassFlowUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public MassFlow(double value, MassFlowUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public MassFlow(int value, MassFlowUnit selectedUnit) : base(value, selectedUnit.Unit) { }
+        public MassFlow(UnknownUnit value) : base(value) { }
 
+        public MassFlow(UnknownUnit value, MassFlowUnit selectedUnit) : base(value, selectedUnit.Unit) { }
 
         public static MassFlow From(double value, MassFlowUnit unit) => new MassFlow(value, unit);
         public double As(MassFlowUnit ReturnInThisUnit) => (double)ToTheOutSide(ReturnInThisUnit.Unit);
         public MassFlow ToUnit(MassFlowUnit selectedUnit) => new MassFlow(ToTheOutSide(selectedUnit.Unit), selectedUnit);
         public static MassFlow Zero => new MassFlow(0, MassFlowUnit.SI);
 
-        public static implicit operator MassFlow(UnknownUnit Unit)
-        {
-            //MassFlow local = new MassFlow(0, MassFlowUnit.SI);
-            MassFlow local = new MassFlow();
-
-            local.Unit = Unit.unitsystem;
-
-            local.Transform(Unit);
-            return local;
-        }
-
-
+        public static implicit operator MassFlow(UnknownUnit Unit) => new MassFlow(Unit, MassFlowUnit.SI);
 
     }
 }
