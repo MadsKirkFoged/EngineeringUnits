863b27b54cac28f728d5c8d9275e8b6d44b3d9b6
Mads Foged <jfogedm@jci.com>
2021-09-06 14:16:57 +0200

    ..

--- EngineeringUnits/CombinedUnits/ElectricResistivity/ElectricResistivity.cs
+++ EngineeringUnits/CombinedUnits/ElectricResistivity/ElectricResistivity.cs
@@ -1,39 +1,25 @@
-﻿using Fractions;
-using EngineeringUnits.Units;
-using System.Collections.Generic;
-using System.Text;
-using EngineeringUnits.Units;
+﻿using EngineeringUnits.Units;
+
 
 namespace EngineeringUnits
 {
     public partial class ElectricResistivity : BaseUnit
     {
 
-        public ElectricResistivity()
-        {
-            Unit = ElectricResistivityUnit.SI.Unit.Copy();
-        }
-
+        public ElectricResistivity() { }
         public ElectricResistivity(decimal value, ElectricResistivityUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public ElectricResistivity(double value, ElectricResistivityUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public ElectricResistivity(int value, ElectricResistivityUnit selectedUnit) : base(value, selectedUnit.Unit) { }
+        public ElectricResistivity(UnknownUnit value) : base(value) { }
 
+        public ElectricResistivity(UnknownUnit value, ElectricResistivityUnit selectedUnit) : base(value, selectedUnit.Unit) { }
 
         public static ElectricResistivity From(double value, ElectricResistivityUnit unit) => new ElectricResistivity(value, unit);
         public double As(ElectricResistivityUnit ReturnInThisUnit) => (double)ToTheOutSide(ReturnInThisUnit.Unit);
         public ElectricResistivity ToUnit(ElectricResistivityUnit selectedUnit) => new ElectricResistivity(ToTheOutSide(selectedUnit.Unit), selectedUnit);
         public static ElectricResistivity Zero => new ElectricResistivity(0, ElectricResistivityUnit.SI);
 
-        public static implicit operator ElectricResistivity(UnknownUnit Unit)
-        {
-            ElectricResistivity local = new ElectricResistivity(0, ElectricResistivityUnit.SI);
-
-            local.Transform(Unit);
-            return local;
-        }
-
-
-
+        public static implicit operator ElectricResistivity(UnknownUnit Unit) => new ElectricResistivity(Unit, ElectricResistivityUnit.SI);
 
     }
 }
