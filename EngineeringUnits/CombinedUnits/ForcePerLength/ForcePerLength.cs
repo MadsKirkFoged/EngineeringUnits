8469a98449c3f5a7378bda104df2aecbee916d17
Mads Foged <jfogedm@jci.com>
2021-09-06 14:13:22 +0200

    ..

--- EngineeringUnits/CombinedUnits/ForcePerLength/ForcePerLength.cs
+++ EngineeringUnits/CombinedUnits/ForcePerLength/ForcePerLength.cs
@@ -1,39 +1,25 @@
-﻿using Fractions;
-using EngineeringUnits.Units;
-using System.Collections.Generic;
-using System.Text;
-using EngineeringUnits.Units;
+﻿using EngineeringUnits.Units;
+
 
 namespace EngineeringUnits
 {
     public partial class ForcePerLength : BaseUnit
     {
 
-        public ForcePerLength()
-        {
-            Unit = ForcePerLengthUnit.SI.Unit.Copy();
-        }
-
+        public ForcePerLength() { }
         public ForcePerLength(decimal value, ForcePerLengthUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public ForcePerLength(double value, ForcePerLengthUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public ForcePerLength(int value, ForcePerLengthUnit selectedUnit) : base(value, selectedUnit.Unit) { }
+        public ForcePerLength(UnknownUnit value) : base(value) { }
 
+        public ForcePerLength(UnknownUnit value, ForcePerLengthUnit selectedUnit) : base(value, selectedUnit.Unit) { }
 
         public static ForcePerLength From(double value, ForcePerLengthUnit unit) => new ForcePerLength(value, unit);
         public double As(ForcePerLengthUnit ReturnInThisUnit) => (double)ToTheOutSide(ReturnInThisUnit.Unit);
         public ForcePerLength ToUnit(ForcePerLengthUnit selectedUnit) => new ForcePerLength(ToTheOutSide(selectedUnit.Unit), selectedUnit);
         public static ForcePerLength Zero => new ForcePerLength(0, ForcePerLengthUnit.SI);
 
-        public static implicit operator ForcePerLength(UnknownUnit Unit)
-        {
-            ForcePerLength local = new ForcePerLength(0, ForcePerLengthUnit.SI);
-
-            local.Transform(Unit);
-            return local;
-        }
-
-
-
+        public static implicit operator ForcePerLength(UnknownUnit Unit) => new ForcePerLength(Unit, ForcePerLengthUnit.SI);
 
     }
 }
