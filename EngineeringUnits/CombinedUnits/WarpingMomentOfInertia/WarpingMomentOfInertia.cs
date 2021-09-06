94c59fba70c4e74919db23e7e82cefe4b4a1493c
Mads Foged <jfogedm@jci.com>
2021-09-06 14:47:55 +0200

    ...

--- EngineeringUnits/CombinedUnits/WarpingMomentOfInertia/WarpingMomentOfInertia.cs
+++ EngineeringUnits/CombinedUnits/WarpingMomentOfInertia/WarpingMomentOfInertia.cs
@@ -6,24 +6,20 @@ namespace EngineeringUnits
     public partial class WarpingMomentOfInertia : BaseUnit
     {
 
-        public WarpingMomentOfInertia()
-        {
-            Unit = WarpingMomentOfInertiaUnit.SI.Unit;
-        }
-
-
+        public WarpingMomentOfInertia() { }
         public WarpingMomentOfInertia(decimal value, WarpingMomentOfInertiaUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public WarpingMomentOfInertia(double value, WarpingMomentOfInertiaUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public WarpingMomentOfInertia(int value, WarpingMomentOfInertiaUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public WarpingMomentOfInertia(UnknownUnit value) : base(value) { }
 
+        public WarpingMomentOfInertia(UnknownUnit value, WarpingMomentOfInertiaUnit selectedUnit) : base(value, selectedUnit.Unit) { }
 
         public static WarpingMomentOfInertia From(double value, WarpingMomentOfInertiaUnit unit) => new WarpingMomentOfInertia(value, unit);
         public double As(WarpingMomentOfInertiaUnit ReturnInThisUnit) => (double)ToTheOutSide(ReturnInThisUnit.Unit);
         public WarpingMomentOfInertia ToUnit(WarpingMomentOfInertiaUnit selectedUnit) => new WarpingMomentOfInertia(ToTheOutSide(selectedUnit.Unit), selectedUnit);
         public static WarpingMomentOfInertia Zero => new WarpingMomentOfInertia(0, WarpingMomentOfInertiaUnit.SI);
 
-        public static implicit operator WarpingMomentOfInertia(UnknownUnit Unit) => new WarpingMomentOfInertia(Unit);
+        public static implicit operator WarpingMomentOfInertia(UnknownUnit Unit) => new WarpingMomentOfInertia(Unit, WarpingMomentOfInertiaUnit.SI);
 
 
 
