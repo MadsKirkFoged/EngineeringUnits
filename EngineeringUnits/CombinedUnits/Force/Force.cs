c009780ea9a2db2052acf434313727d15f7cb1dc
Mads Foged <jfogedm@jci.com>
2021-09-06 14:14:22 +0200

    ..

--- EngineeringUnits/CombinedUnits/Force/Force.cs
+++ EngineeringUnits/CombinedUnits/Force/Force.cs
@@ -1,39 +1,25 @@
-﻿using Fractions;
-using EngineeringUnits.Units;
-using System.Collections.Generic;
-using System.Text;
-using EngineeringUnits.Units;
+﻿using EngineeringUnits.Units;
+
 
 namespace EngineeringUnits
 {
     public partial class Force : BaseUnit
     {
 
-        public Force()
-        {
-            Unit = ForceUnit.SI.Unit.Copy();
-        }
-
+        public Force() { }
         public Force(decimal value, ForceUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public Force(double value, ForceUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public Force(int value, ForceUnit selectedUnit) : base(value, selectedUnit.Unit) { }
+        public Force(UnknownUnit value) : base(value) { }
 
+        public Force(UnknownUnit value, ForceUnit selectedUnit) : base(value, selectedUnit.Unit) { }
 
         public static Force From(double value, ForceUnit unit) => new Force(value, unit);
         public double As(ForceUnit ReturnInThisUnit) => (double)ToTheOutSide(ReturnInThisUnit.Unit);
         public Force ToUnit(ForceUnit selectedUnit) => new Force(ToTheOutSide(selectedUnit.Unit), selectedUnit);
         public static Force Zero => new Force(0, ForceUnit.SI);
 
-        public static implicit operator Force(UnknownUnit Unit)
-        {
-            Force local = new Force(0, ForceUnit.SI);
-
-            local.Transform(Unit);
-            return local;
-        }
-
-
-
+        public static implicit operator Force(UnknownUnit Unit) => new Force(Unit, ForceUnit.SI);
 
     }
 }
