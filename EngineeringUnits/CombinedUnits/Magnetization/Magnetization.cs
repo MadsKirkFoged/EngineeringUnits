efa0941451b9803cafbd0e261a9d0328afd4ed58
Mads Foged <jfogedm@jci.com>
2021-09-06 14:01:50 +0200

    ..

--- EngineeringUnits/CombinedUnits/Magnetization/Magnetization.cs
+++ EngineeringUnits/CombinedUnits/Magnetization/Magnetization.cs
@@ -1,39 +1,25 @@
-﻿using Fractions;
-using EngineeringUnits.Units;
-using System.Collections.Generic;
-using System.Text;
-using EngineeringUnits.Units;
+﻿using EngineeringUnits.Units;
+
 
 namespace EngineeringUnits
 {
     public partial class Magnetization : BaseUnit
     {
 
-        public Magnetization()
-        {
-            Unit = MagnetizationUnit.SI.Unit.Copy();
-        }
-
+        public Magnetization() {}
         public Magnetization(decimal value, MagnetizationUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public Magnetization(double value, MagnetizationUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public Magnetization(int value, MagnetizationUnit selectedUnit) : base(value, selectedUnit.Unit) { }
+        public Magnetization(UnknownUnit value) : base(value) { }
 
+        public Magnetization(UnknownUnit value, MagnetizationUnit selectedUnit) : base(value, selectedUnit.Unit) { }
 
         public static Magnetization From(double value, MagnetizationUnit unit) => new Magnetization(value, unit);
         public double As(MagnetizationUnit ReturnInThisUnit) => (double)ToTheOutSide(ReturnInThisUnit.Unit);
         public Magnetization ToUnit(MagnetizationUnit selectedUnit) => new Magnetization(ToTheOutSide(selectedUnit.Unit), selectedUnit);
         public static Magnetization Zero => new Magnetization(0, MagnetizationUnit.SI);
 
-        public static implicit operator Magnetization(UnknownUnit Unit)
-        {
-            Magnetization local = new Magnetization(0, MagnetizationUnit.SI);
-
-            local.Transform(Unit);
-            return local;
-        }
-
-
-
+        public static implicit operator Magnetization(UnknownUnit Unit) => new Magnetization(Unit, MagnetizationUnit.SI);  
 
     }
 }
