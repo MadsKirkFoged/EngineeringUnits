98420b246b9a0fabb2079db892d94175d84ac075
Mads Foged <jfogedm@jci.com>
2021-09-06 14:44:06 +0200

    ...

--- EngineeringUnits/CombinedUnits/ThermalConductivity/ThermalConductivity.cs
+++ EngineeringUnits/CombinedUnits/ThermalConductivity/ThermalConductivity.cs
@@ -5,24 +5,20 @@ namespace EngineeringUnits
 {
     public partial class ThermalConductivity : BaseUnit
     {
-        public ThermalConductivity()
-        {
-            Unit = ThermalConductivityUnit.SI.Unit;
-        }
-
-
+        public ThermalConductivity() { }
         public ThermalConductivity(decimal value, ThermalConductivityUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public ThermalConductivity(double value, ThermalConductivityUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public ThermalConductivity(int value, ThermalConductivityUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public ThermalConductivity(UnknownUnit value) : base(value) { }
 
+        public ThermalConductivity(UnknownUnit value, ThermalConductivityUnit selectedUnit) : base(value, selectedUnit.Unit) { }
 
         public static ThermalConductivity From(double value, ThermalConductivityUnit unit) => new ThermalConductivity(value, unit);
         public double As(ThermalConductivityUnit ReturnInThisUnit) => (double)ToTheOutSide(ReturnInThisUnit.Unit);
         public ThermalConductivity ToUnit(ThermalConductivityUnit selectedUnit) => new ThermalConductivity(ToTheOutSide(selectedUnit.Unit), selectedUnit);
         public static ThermalConductivity Zero => new ThermalConductivity(0, ThermalConductivityUnit.SI);
 
-        public static implicit operator ThermalConductivity(UnknownUnit Unit) => new ThermalConductivity(Unit);
+        public static implicit operator ThermalConductivity(UnknownUnit Unit) => new ThermalConductivity(Unit, ThermalConductivityUnit.SI);
 
 
     }
