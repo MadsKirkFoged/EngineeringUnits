9a53c599e409d66ffb62f2850260e0ca7b3932dc
Mads Foged <jfogedm@jci.com>
2021-09-06 14:02:19 +0200

    ...

--- EngineeringUnits/CombinedUnits/MagneticFlux/MagneticFlux.cs
+++ EngineeringUnits/CombinedUnits/MagneticFlux/MagneticFlux.cs
@@ -1,39 +1,25 @@
-﻿using Fractions;
-using EngineeringUnits.Units;
-using System.Collections.Generic;
-using System.Text;
-using EngineeringUnits.Units;
+﻿using EngineeringUnits.Units;
+
 
 namespace EngineeringUnits
 {
     public partial class MagneticFlux : BaseUnit
     {
 
-        public MagneticFlux()
-        {
-            Unit = MagneticFluxUnit.SI.Unit.Copy();
-        }
-
+        public MagneticFlux() { }
         public MagneticFlux(decimal value, MagneticFluxUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public MagneticFlux(double value, MagneticFluxUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public MagneticFlux(int value, MagneticFluxUnit selectedUnit) : base(value, selectedUnit.Unit) { }
+        public MagneticFlux(UnknownUnit value) : base(value) { }
 
+        public MagneticFlux(UnknownUnit value, MagneticFluxUnit selectedUnit) : base(value, selectedUnit.Unit) { }
 
         public static MagneticFlux From(double value, MagneticFluxUnit unit) => new MagneticFlux(value, unit);
         public double As(MagneticFluxUnit ReturnInThisUnit) => (double)ToTheOutSide(ReturnInThisUnit.Unit);
         public MagneticFlux ToUnit(MagneticFluxUnit selectedUnit) => new MagneticFlux(ToTheOutSide(selectedUnit.Unit), selectedUnit);
         public static MagneticFlux Zero => new MagneticFlux(0, MagneticFluxUnit.SI);
 
-        public static implicit operator MagneticFlux(UnknownUnit Unit)
-        {
-            MagneticFlux local = new MagneticFlux(0, MagneticFluxUnit.SI);
-
-            local.Transform(Unit);
-            return local;
-        }
-
-
-
+        public static implicit operator MagneticFlux(UnknownUnit Unit) => new MagneticFlux(Unit, MagneticFluxUnit.SI);
 
     }
 }
