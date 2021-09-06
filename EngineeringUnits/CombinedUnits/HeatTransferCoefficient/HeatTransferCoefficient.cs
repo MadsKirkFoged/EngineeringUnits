02f1e9734295cbdba754aaa5476cdb06be9800d5
Mads Foged <jfogedm@jci.com>
2021-09-06 14:11:44 +0200

    ..

--- EngineeringUnits/CombinedUnits/HeatTransferCoefficient/HeatTransferCoefficient.cs
+++ EngineeringUnits/CombinedUnits/HeatTransferCoefficient/HeatTransferCoefficient.cs
@@ -1,39 +1,25 @@
-﻿using Fractions;
-using EngineeringUnits.Units;
-using System.Collections.Generic;
-using System.Text;
-using EngineeringUnits.Units;
+﻿using EngineeringUnits.Units;
+
 
 namespace EngineeringUnits
 {
     public partial class HeatTransferCoefficient : BaseUnit
     {
 
-        public HeatTransferCoefficient()
-        {
-            Unit = HeatTransferCoefficientUnit.SI.Unit.Copy();
-        }
-
+        public HeatTransferCoefficient() { }
         public HeatTransferCoefficient(decimal value, HeatTransferCoefficientUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public HeatTransferCoefficient(double value, HeatTransferCoefficientUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public HeatTransferCoefficient(int value, HeatTransferCoefficientUnit selectedUnit) : base(value, selectedUnit.Unit) { }
+        public HeatTransferCoefficient(UnknownUnit value) : base(value) { }
 
+        public HeatTransferCoefficient(UnknownUnit value, HeatTransferCoefficientUnit selectedUnit) : base(value, selectedUnit.Unit) { }
 
         public static HeatTransferCoefficient From(double value, HeatTransferCoefficientUnit unit) => new HeatTransferCoefficient(value, unit);
         public double As(HeatTransferCoefficientUnit ReturnInThisUnit) => (double)ToTheOutSide(ReturnInThisUnit.Unit);
         public HeatTransferCoefficient ToUnit(HeatTransferCoefficientUnit selectedUnit) => new HeatTransferCoefficient(ToTheOutSide(selectedUnit.Unit), selectedUnit);
         public static HeatTransferCoefficient Zero => new HeatTransferCoefficient(0, HeatTransferCoefficientUnit.SI);
 
-        public static implicit operator HeatTransferCoefficient(UnknownUnit Unit)
-        {
-            HeatTransferCoefficient local = new HeatTransferCoefficient(0, HeatTransferCoefficientUnit.SI);
-
-            local.Transform(Unit);
-            return local;
-        }
-
-
-
+        public static implicit operator HeatTransferCoefficient(UnknownUnit Unit) => new HeatTransferCoefficient(Unit, HeatTransferCoefficientUnit.SI);
 
     }
 }
