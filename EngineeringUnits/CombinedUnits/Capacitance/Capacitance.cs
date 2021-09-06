17eba3f1f1ca1a4134b04fc7e7662916f1f0a639
Mads Foged <jfogedm@jci.com>
2021-09-06 14:27:28 +0200

    ...

--- EngineeringUnits/CombinedUnits/Capacitance/Capacitance.cs
+++ EngineeringUnits/CombinedUnits/Capacitance/Capacitance.cs
@@ -1,38 +1,25 @@
-﻿using Fractions;
-using EngineeringUnits.Units;
-using System.Collections.Generic;
-using System.Text;
-using EngineeringUnits.Units;
+﻿using EngineeringUnits.Units;
+
 
 namespace EngineeringUnits
 {
     public partial class Capacitance : BaseUnit
     {
 
-        public Capacitance()
-        {
-            Unit = CapacitanceUnit.SI.Unit.Copy();
-        }
-
+        public Capacitance() { }
         public Capacitance(decimal value, CapacitanceUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public Capacitance(double value, CapacitanceUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public Capacitance(int value, CapacitanceUnit selectedUnit) : base(value, selectedUnit.Unit) { }
+        public Capacitance(UnknownUnit value) : base(value) { }
 
+        public Capacitance(UnknownUnit value, CapacitanceUnit selectedUnit) : base(value, selectedUnit.Unit) { }
 
         public static Capacitance From(double value, CapacitanceUnit unit) => new Capacitance(value, unit);
         public double As(CapacitanceUnit ReturnInThisUnit) => (double)ToTheOutSide(ReturnInThisUnit.Unit);
         public Capacitance ToUnit(CapacitanceUnit selectedUnit) => new Capacitance(ToTheOutSide(selectedUnit.Unit), selectedUnit);
         public static Capacitance Zero => new Capacitance(0, CapacitanceUnit.SI);
 
-        public static implicit operator Capacitance(UnknownUnit Unit)
-        {
-            Capacitance local = new Capacitance(0, CapacitanceUnit.SI);
-
-            local.Transform(Unit);
-            return local;
-        }
-
-
+        public static implicit operator Capacitance(UnknownUnit Unit) => new Capacitance(Unit, CapacitanceUnit.SI);
 
 
     }
