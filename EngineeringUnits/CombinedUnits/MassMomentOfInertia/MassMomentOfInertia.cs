282f2791bfb19e4c6a87b39f1452ff92a31e756e
Mads Foged <jfogedm@jci.com>
2021-09-06 13:59:38 +0200

    ...

--- EngineeringUnits/CombinedUnits/MassMomentOfInertia/MassMomentOfInertia.cs
+++ EngineeringUnits/CombinedUnits/MassMomentOfInertia/MassMomentOfInertia.cs
@@ -1,39 +1,24 @@
-﻿using Fractions;
-using EngineeringUnits.Units;
-using System.Collections.Generic;
-using System.Text;
-using EngineeringUnits.Units;
+﻿using EngineeringUnits.Units;
+
 
 namespace EngineeringUnits
 {
     public partial class MassMomentOfInertia : BaseUnit
     {
 
-        public MassMomentOfInertia()
-        {
-            Unit = MassMomentOfInertiaUnit.SI.Unit.Copy();
-        }
-
+        public MassMomentOfInertia() { }
         public MassMomentOfInertia(decimal value, MassMomentOfInertiaUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public MassMomentOfInertia(double value, MassMomentOfInertiaUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public MassMomentOfInertia(int value, MassMomentOfInertiaUnit selectedUnit) : base(value, selectedUnit.Unit) { }
+        public MassMomentOfInertia(UnknownUnit value) : base(value) { }
 
+        public MassMomentOfInertia(UnknownUnit value, MassMomentOfInertiaUnit selectedUnit) : base(value, selectedUnit.Unit) { }
 
         public static MassMomentOfInertia From(double value, MassMomentOfInertiaUnit unit) => new MassMomentOfInertia(value, unit);
         public double As(MassMomentOfInertiaUnit ReturnInThisUnit) => (double)ToTheOutSide(ReturnInThisUnit.Unit);
         public MassMomentOfInertia ToUnit(MassMomentOfInertiaUnit selectedUnit) => new MassMomentOfInertia(ToTheOutSide(selectedUnit.Unit), selectedUnit);
         public static MassMomentOfInertia Zero => new MassMomentOfInertia(0, MassMomentOfInertiaUnit.SI);
 
-        public static implicit operator MassMomentOfInertia(UnknownUnit Unit)
-        {
-            MassMomentOfInertia local = new MassMomentOfInertia(0, MassMomentOfInertiaUnit.SI);
-
-            local.Transform(Unit);
-            return local;
-        }
-
-
-
-
+        public static implicit operator MassMomentOfInertia(UnknownUnit Unit) => new MassMomentOfInertia(Unit, MassMomentOfInertiaUnit.SI);
     }
 }
