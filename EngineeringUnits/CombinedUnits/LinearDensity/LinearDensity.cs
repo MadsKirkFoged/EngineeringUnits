294c4a8ce0286f222d98b2a574c0bdd8cc67223f
Mads Foged <jfogedm@jci.com>
2021-09-06 14:06:42 +0200

    ..

--- EngineeringUnits/CombinedUnits/LinearDensity/LinearDensity.cs
+++ EngineeringUnits/CombinedUnits/LinearDensity/LinearDensity.cs
@@ -1,39 +1,25 @@
-﻿using Fractions;
-using EngineeringUnits.Units;
-using System.Collections.Generic;
-using System.Text;
-using EngineeringUnits.Units;
+﻿using EngineeringUnits.Units;
+
 
 namespace EngineeringUnits
 {
     public partial class LinearDensity : BaseUnit
     {
 
-        public LinearDensity()
-        {
-            Unit = LinearDensityUnit.SI.Unit.Copy();
-        }
-
+        public LinearDensity() { }
         public LinearDensity(decimal value, LinearDensityUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public LinearDensity(double value, LinearDensityUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public LinearDensity(int value, LinearDensityUnit selectedUnit) : base(value, selectedUnit.Unit) { }
+        public LinearDensity(UnknownUnit value) : base(value) { }
 
+        public LinearDensity(UnknownUnit value, LinearDensityUnit selectedUnit) : base(value, selectedUnit.Unit) { }
 
         public static LinearDensity From(double value, LinearDensityUnit unit) => new LinearDensity(value, unit);
         public double As(LinearDensityUnit ReturnInThisUnit) => (double)ToTheOutSide(ReturnInThisUnit.Unit);
         public LinearDensity ToUnit(LinearDensityUnit selectedUnit) => new LinearDensity(ToTheOutSide(selectedUnit.Unit), selectedUnit);
         public static LinearDensity Zero => new LinearDensity(0, LinearDensityUnit.SI);
 
-        public static implicit operator LinearDensity(UnknownUnit Unit)
-        {
-            LinearDensity local = new LinearDensity(0, LinearDensityUnit.SI);
-
-            local.Transform(Unit);
-            return local;
-        }
-
-
-
+        public static implicit operator LinearDensity(UnknownUnit Unit) => new LinearDensity(Unit, LinearDensityUnit.SI);
 
     }
 }
