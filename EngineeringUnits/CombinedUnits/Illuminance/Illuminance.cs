604807b13be56889b2b407baaaec71b05994fac9
Mads Foged <jfogedm@jci.com>
2021-09-06 14:11:13 +0200

    ..

--- EngineeringUnits/CombinedUnits/Illuminance/Illuminance.cs
+++ EngineeringUnits/CombinedUnits/Illuminance/Illuminance.cs
@@ -1,39 +1,25 @@
-﻿using Fractions;
-using EngineeringUnits.Units;
-using System.Collections.Generic;
-using System.Text;
-using EngineeringUnits.Units;
+﻿using EngineeringUnits.Units;
+
 
 namespace EngineeringUnits
 {
     public partial class Illuminance : BaseUnit
     {
 
-        public Illuminance()
-        {
-            Unit = IlluminanceUnit.SI.Unit.Copy();
-        }
-
+        public Illuminance() { }
         public Illuminance(decimal value, IlluminanceUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public Illuminance(double value, IlluminanceUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public Illuminance(int value, IlluminanceUnit selectedUnit) : base(value, selectedUnit.Unit) { }
+        public Illuminance(UnknownUnit value) : base(value) { }
 
+        public Illuminance(UnknownUnit value, IlluminanceUnit selectedUnit) : base(value, selectedUnit.Unit) { }
 
         public static Illuminance From(double value, IlluminanceUnit unit) => new Illuminance(value, unit);
         public double As(IlluminanceUnit ReturnInThisUnit) => (double)ToTheOutSide(ReturnInThisUnit.Unit);
         public Illuminance ToUnit(IlluminanceUnit selectedUnit) => new Illuminance(ToTheOutSide(selectedUnit.Unit), selectedUnit);
         public static Illuminance Zero => new Illuminance(0, IlluminanceUnit.SI);
 
-        public static implicit operator Illuminance(UnknownUnit Unit)
-        {
-            Illuminance local = new Illuminance(0, IlluminanceUnit.SI);
-
-            local.Transform(Unit);
-            return local;
-        }
-
-
-
+        public static implicit operator Illuminance(UnknownUnit Unit) => new Illuminance(Unit, IlluminanceUnit.SI);
 
     }
 }
