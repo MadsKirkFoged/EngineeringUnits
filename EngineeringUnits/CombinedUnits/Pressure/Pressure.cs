50e13323e6bc965fc94ec2d1036af5d3140c6110
Mads Foged <jfogedm@jci.com>
2021-09-06 14:38:29 +0200

    ..

--- EngineeringUnits/CombinedUnits/Pressure/Pressure.cs
+++ EngineeringUnits/CombinedUnits/Pressure/Pressure.cs
@@ -6,23 +6,19 @@ namespace EngineeringUnits
     public partial class Pressure : BaseUnit
     {
 
-        public Pressure()
-        {
-            Unit = PressureUnit.SI.Unit;
-        }
-
-
+        public Pressure() { }
         public Pressure(decimal value, PressureUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public Pressure(double value, PressureUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public Pressure(int value, PressureUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public Pressure(UnknownUnit value) : base(value) { }
 
+        public Pressure(UnknownUnit value, PressureUnit selectedUnit) : base(value, selectedUnit.Unit) { }
 
         public static Pressure From(double value, PressureUnit unit) => new Pressure(value, unit);
         public double As(PressureUnit ReturnInThisUnit) => (double)ToTheOutSide(ReturnInThisUnit.Unit);
         public Pressure ToUnit(PressureUnit selectedUnit) => new Pressure(ToTheOutSide(selectedUnit.Unit), selectedUnit);
         public static Pressure Zero => new Pressure(0, PressureUnit.SI);
 
-        public static implicit operator Pressure(UnknownUnit Unit) => new Pressure(Unit);
+        public static implicit operator Pressure(UnknownUnit Unit) => new Pressure(Unit, PressureUnit.SI);
     }
 }
