ae0ebd193b1d468c513e1f33fa221d6d6a63c9ac
Mads Foged <jfogedm@jci.com>
2021-09-06 13:56:14 +0200

    ..

--- EngineeringUnits/CombinedUnits/Permeability/Permeability.cs
+++ EngineeringUnits/CombinedUnits/Permeability/Permeability.cs
@@ -1,39 +1,24 @@
-﻿using Fractions;
-using EngineeringUnits.Units;
-using System.Collections.Generic;
-using System.Text;
-using EngineeringUnits.Units;
+﻿using EngineeringUnits.Units;
+
 
 namespace EngineeringUnits
 {
     public partial class Permeability : BaseUnit
     {
 
-        public Permeability()
-        {
-            Unit = PermeabilityUnit.SI.Unit.Copy();
-        }
-
+        public Permeability() { }
         public Permeability(decimal value, PermeabilityUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public Permeability(double value, PermeabilityUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public Permeability(int value, PermeabilityUnit selectedUnit) : base(value, selectedUnit.Unit) { }
+        public Permeability(UnknownUnit value) : base(value) { }
 
+        public Permeability(UnknownUnit value, PermeabilityUnit selectedUnit) : base(value, selectedUnit.Unit) { }
 
         public static Permeability From(double value, PermeabilityUnit unit) => new Permeability(value, unit);
         public double As(PermeabilityUnit ReturnInThisUnit) => (double)ToTheOutSide(ReturnInThisUnit.Unit);
         public Permeability ToUnit(PermeabilityUnit selectedUnit) => new Permeability(ToTheOutSide(selectedUnit.Unit), selectedUnit);
         public static Permeability Zero => new Permeability(0, PermeabilityUnit.SI);
 
-        public static implicit operator Permeability(UnknownUnit Unit)
-        {
-            Permeability local = new Permeability(0, PermeabilityUnit.SI);
-
-            local.Transform(Unit);
-            return local;
-        }
-
-
-
-
+        public static implicit operator Permeability(UnknownUnit Unit) => new Permeability(Unit, PermeabilityUnit.SI);
     }
 }
