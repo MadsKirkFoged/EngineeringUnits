9b96ef790707e32f3c5251d55b69aed2298658b5
Mads Foged <jfogedm@jci.com>
2021-09-06 14:35:52 +0200

    ..

--- EngineeringUnits/BaseUnits/Electriccurrent/Electriccurrent.cs
+++ EngineeringUnits/BaseUnits/Electriccurrent/Electriccurrent.cs
@@ -1,9 +1,5 @@
 ﻿using EngineeringUnits.Units;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Text;
-using Fractions;
-using EngineeringUnits.Units;
+
 
 namespace EngineeringUnits
 {
@@ -11,31 +7,20 @@ namespace EngineeringUnits
     public partial class ElectricCurrent : BaseUnit
     {
 
-        public ElectricCurrent()
-        {
-            Unit = ElectricCurrentUnit.SI.Unit.Copy();
-        }
-
+        public ElectricCurrent() { }
         public ElectricCurrent(decimal value, ElectricCurrentUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public ElectricCurrent(double value, ElectricCurrentUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public ElectricCurrent(int value, ElectricCurrentUnit selectedUnit) : base(value, selectedUnit.Unit) { }
+        public ElectricCurrent(UnknownUnit value) : base(value) { }
 
+        public ElectricCurrent(UnknownUnit value, ElectricCurrentUnit selectedUnit) : base(value, selectedUnit.Unit) { }
 
         public static ElectricCurrent From(double value, ElectricCurrentUnit unit) => new ElectricCurrent(value, unit);
         public double As(ElectricCurrentUnit ReturnInThisUnit) => (double)ToTheOutSide(ReturnInThisUnit.Unit);
         public ElectricCurrent ToUnit(ElectricCurrentUnit selectedUnit) => new ElectricCurrent(ToTheOutSide(selectedUnit.Unit), selectedUnit);
         public static ElectricCurrent Zero => new ElectricCurrent(0, ElectricCurrentUnit.SI);
 
-        public static implicit operator ElectricCurrent(UnknownUnit Unit)
-        {
-            ElectricCurrent local = new ElectricCurrent(0, ElectricCurrentUnit.SI);
-
-            local.Transform(Unit);
-            return local;
-        }
-
-
-
+        public static implicit operator ElectricCurrent(UnknownUnit Unit) => new ElectricCurrent(Unit, ElectricCurrentUnit.SI);
 
     }
 }
