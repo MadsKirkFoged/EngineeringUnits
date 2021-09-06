a3075d4115a42db43c9cd97e06a5eac5251c28ae
Mads Foged <jfogedm@jci.com>
2021-09-06 14:39:56 +0200

    ...

--- EngineeringUnits/CombinedUnits/ReactiveEnergy/ReactiveEnergy.cs
+++ EngineeringUnits/CombinedUnits/ReactiveEnergy/ReactiveEnergy.cs
@@ -6,23 +6,19 @@ namespace EngineeringUnits
     public partial class ReactiveEnergy : BaseUnit
     {
 
-        public ReactiveEnergy()
-        {
-            Unit = ReactiveEnergyUnit.SI.Unit;
-        }
-
-
+        public ReactiveEnergy() { }
         public ReactiveEnergy(decimal value, ReactiveEnergyUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public ReactiveEnergy(double value, ReactiveEnergyUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public ReactiveEnergy(int value, ReactiveEnergyUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public ReactiveEnergy(UnknownUnit value) : base(value) { }
 
+        public ReactiveEnergy(UnknownUnit value, ReactiveEnergyUnit selectedUnit) : base(value, selectedUnit.Unit) { }
 
         public static ReactiveEnergy From(double value, ReactiveEnergyUnit unit) => new ReactiveEnergy(value, unit);
         public double As(ReactiveEnergyUnit ReturnInThisUnit) => (double)ToTheOutSide(ReturnInThisUnit.Unit);
         public ReactiveEnergy ToUnit(ReactiveEnergyUnit selectedUnit) => new ReactiveEnergy(ToTheOutSide(selectedUnit.Unit), selectedUnit);
         public static ReactiveEnergy Zero => new ReactiveEnergy(0, ReactiveEnergyUnit.SI);
 
-        public static implicit operator ReactiveEnergy(UnknownUnit Unit) => new ReactiveEnergy(Unit);
+        public static implicit operator ReactiveEnergy(UnknownUnit Unit) => new ReactiveEnergy(Unit, ReactiveEnergyUnit.SI);
     }
 }
