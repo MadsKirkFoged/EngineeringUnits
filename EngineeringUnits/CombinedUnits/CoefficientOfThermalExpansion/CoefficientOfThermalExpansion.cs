25f84c3afe52b49fba8c093e9948281a07807043
Mads Foged <jfogedm@jci.com>
2021-09-06 14:26:58 +0200

    ...

--- EngineeringUnits/CombinedUnits/CoefficientOfThermalExpansion/CoefficientOfThermalExpansion.cs
+++ EngineeringUnits/CombinedUnits/CoefficientOfThermalExpansion/CoefficientOfThermalExpansion.cs
@@ -1,38 +1,24 @@
-﻿using Fractions;
-using EngineeringUnits.Units;
-using System.Collections.Generic;
-using System.Text;
-using EngineeringUnits.Units;
+﻿using EngineeringUnits.Units;
 
 namespace EngineeringUnits
 {
     public partial class CoefficientOfThermalExpansion : BaseUnit
     {
 
-        public CoefficientOfThermalExpansion()
-        {
-            Unit = CoefficientOfThermalExpansionUnit.SI.Unit.Copy();
-        }
-
+        public CoefficientOfThermalExpansion() { }
         public CoefficientOfThermalExpansion(decimal value, CoefficientOfThermalExpansionUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public CoefficientOfThermalExpansion(double value, CoefficientOfThermalExpansionUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public CoefficientOfThermalExpansion(int value, CoefficientOfThermalExpansionUnit selectedUnit) : base(value, selectedUnit.Unit) { }
+        public CoefficientOfThermalExpansion(UnknownUnit value) : base(value) { }
 
+        public CoefficientOfThermalExpansion(UnknownUnit value, CoefficientOfThermalExpansionUnit selectedUnit) : base(value, selectedUnit.Unit) { }
 
         public static CoefficientOfThermalExpansion From(double value, CoefficientOfThermalExpansionUnit unit) => new CoefficientOfThermalExpansion(value, unit);
         public double As(CoefficientOfThermalExpansionUnit ReturnInThisUnit) => (double)ToTheOutSide(ReturnInThisUnit.Unit);
         public CoefficientOfThermalExpansion ToUnit(CoefficientOfThermalExpansionUnit selectedUnit) => new CoefficientOfThermalExpansion(ToTheOutSide(selectedUnit.Unit), selectedUnit);
         public static CoefficientOfThermalExpansion Zero => new CoefficientOfThermalExpansion(0, CoefficientOfThermalExpansionUnit.SI);
 
-        public static implicit operator CoefficientOfThermalExpansion(UnknownUnit Unit)
-        {
-            CoefficientOfThermalExpansion local = new CoefficientOfThermalExpansion(0, CoefficientOfThermalExpansionUnit.SI);
-
-            local.Transform(Unit);
-            return local;
-        }
-
-
+        public static implicit operator CoefficientOfThermalExpansion(UnknownUnit Unit) => new CoefficientOfThermalExpansion(Unit, CoefficientOfThermalExpansionUnit.SI);
 
 
     }
