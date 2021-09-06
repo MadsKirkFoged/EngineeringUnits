7c5a3e3a40c8cd636175b31ebb7771eddc1b0ebc
Mads Foged <jfogedm@jci.com>
2021-09-06 14:00:19 +0200

    ...

--- EngineeringUnits/CombinedUnits/MassFlux/MassFlux.cs
+++ EngineeringUnits/CombinedUnits/MassFlux/MassFlux.cs
@@ -1,39 +1,24 @@
-﻿using Fractions;
-using EngineeringUnits.Units;
-using System.Collections.Generic;
-using System.Text;
-using EngineeringUnits.Units;
+﻿using EngineeringUnits.Units;
+
 
 namespace EngineeringUnits
 {
     public partial class MassFlux : BaseUnit
     {
 
-        public MassFlux()
-        {
-            Unit = MassFluxUnit.SI.Unit.Copy();
-        }
-
+        public MassFlux() { }
         public MassFlux(decimal value, MassFluxUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public MassFlux(double value, MassFluxUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public MassFlux(int value, MassFluxUnit selectedUnit) : base(value, selectedUnit.Unit) { }
+        public MassFlux(UnknownUnit value) : base(value) { }
 
+        public MassFlux(UnknownUnit value, MassFluxUnit selectedUnit) : base(value, selectedUnit.Unit) { }
 
         public static MassFlux From(double value, MassFluxUnit unit) => new MassFlux(value, unit);
         public double As(MassFluxUnit ReturnInThisUnit) => (double)ToTheOutSide(ReturnInThisUnit.Unit);
         public MassFlux ToUnit(MassFluxUnit selectedUnit) => new MassFlux(ToTheOutSide(selectedUnit.Unit), selectedUnit);
         public static MassFlux Zero => new MassFlux(0, MassFluxUnit.SI);
 
-        public static implicit operator MassFlux(UnknownUnit Unit)
-        {
-            MassFlux local = new MassFlux(0, MassFluxUnit.SI);
-
-            local.Transform(Unit);
-            return local;
-        }
-
-
-
-
+        public static implicit operator MassFlux(UnknownUnit Unit) => new MassFlux(Unit, MassFluxUnit.SI);
     }
 }
