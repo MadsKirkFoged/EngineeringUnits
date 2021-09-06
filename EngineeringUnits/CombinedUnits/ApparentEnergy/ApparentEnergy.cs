cbfbe2e5ad4a8b9f91469e0d6149ff2077d800a6
Mads Foged <jfogedm@jci.com>
2021-09-06 14:32:38 +0200

    ...

--- EngineeringUnits/CombinedUnits/ApparentEnergy/ApparentEnergy.cs
+++ EngineeringUnits/CombinedUnits/ApparentEnergy/ApparentEnergy.cs
@@ -1,38 +1,25 @@
-﻿using Fractions;
-using EngineeringUnits.Units;
-using System.Collections.Generic;
-using System.Text;
-using EngineeringUnits.Units;
+﻿using EngineeringUnits.Units;
+
 
 namespace EngineeringUnits
 {
     public partial class ApparentEnergy : BaseUnit
     {
 
-        public ApparentEnergy()
-        {
-            Unit = ApparentEnergyUnit.SI.Unit.Copy();
-        }
-
+        public ApparentEnergy() { }
         public ApparentEnergy(decimal value, ApparentEnergyUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public ApparentEnergy(double value, ApparentEnergyUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public ApparentEnergy(int value, ApparentEnergyUnit selectedUnit) : base(value, selectedUnit.Unit) { }
+        public ApparentEnergy(UnknownUnit value) : base(value) { }
 
+        public ApparentEnergy(UnknownUnit value, ApparentEnergyUnit selectedUnit) : base(value, selectedUnit.Unit) { }
 
         public static ApparentEnergy From(double value, ApparentEnergyUnit unit) => new ApparentEnergy(value, unit);
         public double As(ApparentEnergyUnit ReturnInThisUnit) => (double)ToTheOutSide(ReturnInThisUnit.Unit);
         public ApparentEnergy ToUnit(ApparentEnergyUnit selectedUnit) => new ApparentEnergy(ToTheOutSide(selectedUnit.Unit), selectedUnit);
         public static ApparentEnergy Zero => new ApparentEnergy(0, ApparentEnergyUnit.SI);
 
-        public static implicit operator ApparentEnergy(UnknownUnit Unit)
-        {
-            ApparentEnergy local = new ApparentEnergy(0, ApparentEnergyUnit.SI);
-
-            local.Transform(Unit);
-            return local;
-        }
-
-
+        public static implicit operator ApparentEnergy(UnknownUnit Unit) => new ApparentEnergy(Unit, ApparentEnergyUnit.SI);
 
 
     }
