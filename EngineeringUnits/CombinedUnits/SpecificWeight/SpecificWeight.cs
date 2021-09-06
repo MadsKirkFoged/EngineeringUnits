225b337d7a5a8a764a23719cc9782edce8cc79f1
Mads Foged <jfogedm@jci.com>
2021-09-06 14:42:33 +0200

    ...

--- EngineeringUnits/CombinedUnits/SpecificWeight/SpecificWeight.cs
+++ EngineeringUnits/CombinedUnits/SpecificWeight/SpecificWeight.cs
@@ -5,23 +5,19 @@ namespace EngineeringUnits
     public partial class SpecificWeight : BaseUnit
     {
 
-        public SpecificWeight()
-        {
-            Unit = SpecificWeightUnit.SI.Unit;
-        }
-
-
+        public SpecificWeight() { }
         public SpecificWeight(decimal value, SpecificWeightUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public SpecificWeight(double value, SpecificWeightUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public SpecificWeight(int value, SpecificWeightUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public SpecificWeight(UnknownUnit value) : base(value) { }
 
+        public SpecificWeight(UnknownUnit value, SpecificWeightUnit selectedUnit) : base(value, selectedUnit.Unit) { }
 
         public static SpecificWeight From(double value, SpecificWeightUnit unit) => new SpecificWeight(value, unit);
         public double As(SpecificWeightUnit ReturnInThisUnit) => (double)ToTheOutSide(ReturnInThisUnit.Unit);
         public SpecificWeight ToUnit(SpecificWeightUnit selectedUnit) => new SpecificWeight(ToTheOutSide(selectedUnit.Unit), selectedUnit);
         public static SpecificWeight Zero => new SpecificWeight(0, SpecificWeightUnit.SI);
 
-        public static implicit operator SpecificWeight(UnknownUnit Unit) => new SpecificWeight(Unit);
+        public static implicit operator SpecificWeight(UnknownUnit Unit) => new SpecificWeight(Unit, SpecificWeightUnit.SI);
     }
 }
