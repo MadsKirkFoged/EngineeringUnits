2833275eaf0b9ca4fe54cd2b6c9240d7f62e8734
Mads Foged <jfogedm@jci.com>
2021-09-06 14:45:33 +0200

    ...

--- EngineeringUnits/CombinedUnits/TorquePerLength/TorquePerLength.cs
+++ EngineeringUnits/CombinedUnits/TorquePerLength/TorquePerLength.cs
@@ -6,23 +6,19 @@ namespace EngineeringUnits
     public partial class TorquePerLength : BaseUnit
     {
 
-        public TorquePerLength()
-        {
-            Unit = TorquePerLengthUnit.SI.Unit;
-        }
-
-
+        public TorquePerLength() { }
         public TorquePerLength(decimal value, TorquePerLengthUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public TorquePerLength(double value, TorquePerLengthUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public TorquePerLength(int value, TorquePerLengthUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public TorquePerLength(UnknownUnit value) : base(value) { }
 
+        public TorquePerLength(UnknownUnit value, TorquePerLengthUnit selectedUnit) : base(value, selectedUnit.Unit) { }
 
         public static TorquePerLength From(double value, TorquePerLengthUnit unit) => new TorquePerLength(value, unit);
         public double As(TorquePerLengthUnit ReturnInThisUnit) => (double)ToTheOutSide(ReturnInThisUnit.Unit);
         public TorquePerLength ToUnit(TorquePerLengthUnit selectedUnit) => new TorquePerLength(ToTheOutSide(selectedUnit.Unit), selectedUnit);
         public static TorquePerLength Zero => new TorquePerLength(0, TorquePerLengthUnit.SI);
 
-        public static implicit operator TorquePerLength(UnknownUnit Unit) => new TorquePerLength(Unit);
+        public static implicit operator TorquePerLength(UnknownUnit Unit) => new TorquePerLength(Unit, TorquePerLengthUnit.SI);
     }
 }
