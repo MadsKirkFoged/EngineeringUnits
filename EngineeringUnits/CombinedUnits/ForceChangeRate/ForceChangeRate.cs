cb4ae33942735072569779a3ff959e40eea3ef42
Mads Foged <jfogedm@jci.com>
2021-09-06 14:13:52 +0200

    ..

--- EngineeringUnits/CombinedUnits/ForceChangeRate/ForceChangeRate.cs
+++ EngineeringUnits/CombinedUnits/ForceChangeRate/ForceChangeRate.cs
@@ -1,38 +1,25 @@
-﻿using Fractions;
-using EngineeringUnits.Units;
-using System.Collections.Generic;
-using System.Text;
-using EngineeringUnits.Units;
+﻿using EngineeringUnits.Units;
+
 
 namespace EngineeringUnits
 {
     public partial class ForceChangeRate : BaseUnit
     {
 
-        public ForceChangeRate()
-        {
-            Unit = ForceChangeRateUnit.SI.Unit.Copy();
-        }
-
+        public ForceChangeRate() { }
         public ForceChangeRate(decimal value, ForceChangeRateUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public ForceChangeRate(double value, ForceChangeRateUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public ForceChangeRate(int value, ForceChangeRateUnit selectedUnit) : base(value, selectedUnit.Unit) { }
+        public ForceChangeRate(UnknownUnit value) : base(value) { }
 
+        public ForceChangeRate(UnknownUnit value, ForceChangeRateUnit selectedUnit) : base(value, selectedUnit.Unit) { }
 
         public static ForceChangeRate From(double value, ForceChangeRateUnit unit) => new ForceChangeRate(value, unit);
         public double As(ForceChangeRateUnit ReturnInThisUnit) => (double)ToTheOutSide(ReturnInThisUnit.Unit);
         public ForceChangeRate ToUnit(ForceChangeRateUnit selectedUnit) => new ForceChangeRate(ToTheOutSide(selectedUnit.Unit), selectedUnit);
         public static ForceChangeRate Zero => new ForceChangeRate(0, ForceChangeRateUnit.SI);
 
-        public static implicit operator ForceChangeRate(UnknownUnit Unit)
-        {
-            ForceChangeRate local = new ForceChangeRate(0, ForceChangeRateUnit.SI);
-
-            local.Transform(Unit);
-            return local;
-        }
-
-
+        public static implicit operator ForceChangeRate(UnknownUnit Unit) => new ForceChangeRate(Unit, ForceChangeRateUnit.SI);
 
 
     }
