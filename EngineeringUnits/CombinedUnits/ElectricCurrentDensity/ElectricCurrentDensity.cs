62424352f88f70bc600d7478f5ce72242bcb5b4b
Mads Foged <jfogedm@jci.com>
2021-09-06 14:22:26 +0200

    ..

--- EngineeringUnits/CombinedUnits/ElectricCurrentDensity/ElectricCurrentDensity.cs
+++ EngineeringUnits/CombinedUnits/ElectricCurrentDensity/ElectricCurrentDensity.cs
@@ -1,38 +1,25 @@
-﻿using Fractions;
-using EngineeringUnits.Units;
-using System.Collections.Generic;
-using System.Text;
-using EngineeringUnits.Units;
+﻿using EngineeringUnits.Units;
+
 
 namespace EngineeringUnits
 {
     public partial class ElectricCurrentDensity : BaseUnit
     {
 
-        public ElectricCurrentDensity()
-        {
-            Unit = ElectricCurrentDensityUnit.SI.Unit.Copy();
-        }
-
+        public ElectricCurrentDensity() { }
         public ElectricCurrentDensity(decimal value, ElectricCurrentDensityUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public ElectricCurrentDensity(double value, ElectricCurrentDensityUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public ElectricCurrentDensity(int value, ElectricCurrentDensityUnit selectedUnit) : base(value, selectedUnit.Unit) { }
+        public ElectricCurrentDensity(UnknownUnit value) : base(value) { }
 
+        public ElectricCurrentDensity(UnknownUnit value, ElectricCurrentDensityUnit selectedUnit) : base(value, selectedUnit.Unit) { }
 
         public static ElectricCurrentDensity From(double value, ElectricCurrentDensityUnit unit) => new ElectricCurrentDensity(value, unit);
         public double As(ElectricCurrentDensityUnit ReturnInThisUnit) => (double)ToTheOutSide(ReturnInThisUnit.Unit);
         public ElectricCurrentDensity ToUnit(ElectricCurrentDensityUnit selectedUnit) => new ElectricCurrentDensity(ToTheOutSide(selectedUnit.Unit), selectedUnit);
         public static ElectricCurrentDensity Zero => new ElectricCurrentDensity(0, ElectricCurrentDensityUnit.SI);
 
-        public static implicit operator ElectricCurrentDensity(UnknownUnit Unit)
-        {
-            ElectricCurrentDensity local = new ElectricCurrentDensity(0, ElectricCurrentDensityUnit.SI);
-
-            local.Transform(Unit);
-            return local;
-        }
-
-
+        public static implicit operator ElectricCurrentDensity(UnknownUnit Unit) => new ElectricCurrentDensity(Unit, ElectricCurrentDensityUnit.SI);
 
 
     }
