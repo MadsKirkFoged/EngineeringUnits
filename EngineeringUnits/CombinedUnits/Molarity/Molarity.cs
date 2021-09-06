e253ca9029da41937d24cb7edbcbfb3ec8ccbb59
Mads Foged <jfogedm@jci.com>
2021-09-06 13:57:46 +0200

    ..

--- EngineeringUnits/CombinedUnits/Molarity/Molarity.cs
+++ EngineeringUnits/CombinedUnits/Molarity/Molarity.cs
@@ -1,39 +1,25 @@
-﻿using Fractions;
-using EngineeringUnits.Units;
-using System.Collections.Generic;
-using System.Text;
-using EngineeringUnits.Units;
+﻿using EngineeringUnits.Units;
+
 
 namespace EngineeringUnits
 {
     public partial class Molarity : BaseUnit
     {
 
-        public Molarity()
-        {
-            Unit = MolarityUnit.SI.Unit.Copy();
-        }
-
+        public Molarity() { }
         public Molarity(decimal value, MolarityUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public Molarity(double value, MolarityUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public Molarity(int value, MolarityUnit selectedUnit) : base(value, selectedUnit.Unit) { }
+        public Molarity(UnknownUnit value) : base(value) { }
 
+        public Molarity(UnknownUnit value, MolarityUnit selectedUnit) : base(value, selectedUnit.Unit) { }
 
         public static Molarity From(double value, MolarityUnit unit) => new Molarity(value, unit);
         public double As(MolarityUnit ReturnInThisUnit) => (double)ToTheOutSide(ReturnInThisUnit.Unit);
         public Molarity ToUnit(MolarityUnit selectedUnit) => new Molarity(ToTheOutSide(selectedUnit.Unit), selectedUnit);
         public static Molarity Zero => new Molarity(0, MolarityUnit.SI);
 
-        public static implicit operator Molarity(UnknownUnit Unit)
-        {
-            Molarity local = new Molarity(0, MolarityUnit.SI);
-
-            local.Transform(Unit);
-            return local;
-        }
-
-
-
+        public static implicit operator Molarity(UnknownUnit Unit) => new Molarity(Unit, MolarityUnit.SI);
 
     }
 }
