e9f5a8681b6582996216116bcd4f519c4548c6a9
Mads Foged <jfogedm@jci.com>
2021-09-06 14:41:49 +0200

    ..

--- EngineeringUnits/CombinedUnits/SpecificEntropy/SpecificEntropy.cs
+++ EngineeringUnits/CombinedUnits/SpecificEntropy/SpecificEntropy.cs
@@ -5,23 +5,19 @@ namespace EngineeringUnits
 
     public partial class SpecificEntropy : BaseUnit
     {
-        public SpecificEntropy()
-        {
-            Unit = SpecificEntropyUnit.SI.Unit;
-        }
-
-
+        public SpecificEntropy() { }
         public SpecificEntropy(decimal value, SpecificEntropyUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public SpecificEntropy(double value, SpecificEntropyUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public SpecificEntropy(int value, SpecificEntropyUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public SpecificEntropy(UnknownUnit value) : base(value) { }
 
+        public SpecificEntropy(UnknownUnit value, SpecificEntropyUnit selectedUnit) : base(value, selectedUnit.Unit) { }
 
         public static SpecificEntropy From(double value, SpecificEntropyUnit unit) => new SpecificEntropy(value, unit);
         public double As(SpecificEntropyUnit ReturnInThisUnit) => (double)ToTheOutSide(ReturnInThisUnit.Unit);
         public SpecificEntropy ToUnit(SpecificEntropyUnit selectedUnit) => new SpecificEntropy(ToTheOutSide(selectedUnit.Unit), selectedUnit);
         public static SpecificEntropy Zero => new SpecificEntropy(0, SpecificEntropyUnit.SI);
 
-        public static implicit operator SpecificEntropy(UnknownUnit Unit) => new SpecificEntropy(Unit);
+        public static implicit operator SpecificEntropy(UnknownUnit Unit) => new SpecificEntropy(Unit, SpecificEntropyUnit.SI);
     }
 }
