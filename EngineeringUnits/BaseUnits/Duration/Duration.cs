22501a40205bce2c3af6fc1474280b9ba7cc83cc
Mads Foged <jfogedm@jci.com>
2021-09-06 14:47:27 +0200

    ...

--- EngineeringUnits/BaseUnits/Duration/Duration.cs
+++ EngineeringUnits/BaseUnits/Duration/Duration.cs
@@ -1,38 +1,25 @@
 ﻿using EngineeringUnits.Units;
-using System.Collections.Generic;
-using System.Text;
-using Fractions;
-using EngineeringUnits.Units;
+
 
 namespace EngineeringUnits
 {
     public partial class Duration : BaseUnit
     {
 
-        public Duration()
-        {
-            Unit = DurationUnit.SI.Unit.Copy();
-        }
-
-
+        public Duration() { }
         public Duration(decimal value, DurationUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public Duration(double value, DurationUnit selectedUnit) : base(value, selectedUnit.Unit) { }
         public Duration(int value, DurationUnit selectedUnit) : base(value, selectedUnit.Unit) { }
+        public Duration(UnknownUnit value) : base(value) { }
 
+        public Duration(UnknownUnit value, DurationUnit selectedUnit) : base(value, selectedUnit.Unit) { }
 
         public static Duration From(double value, DurationUnit unit) => new Duration(value, unit);
         public double As(DurationUnit ReturnInThisUnit) => (double)ToTheOutSide(ReturnInThisUnit.Unit);
         public Duration ToUnit(DurationUnit selectedUnit) => new Duration(ToTheOutSide(selectedUnit.Unit), selectedUnit);
-
         public static Duration Zero => new Duration(0, DurationUnit.SI);
-        public static implicit operator Duration(UnknownUnit Unit)
-        {
-            Duration local = new Duration(0, DurationUnit.SI);
 
-            local.Transform(Unit);
-            return local;
-        }
-
+        public static implicit operator Duration(UnknownUnit Unit) => new Duration(Unit, DurationUnit.SI);
 
 
     }
