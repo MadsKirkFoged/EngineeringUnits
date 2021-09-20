38bd0eaf7cbc1d6e72499e43feb5d40f11a40f1a
Mads Foged <jfogedm@jci.com>
2021-09-20 12:32:01 +0200

    ..

--- EngineeringUnits/CombinedUnits/KinematicViscosity/KinematicViscosity.cs
+++ EngineeringUnits/CombinedUnits/KinematicViscosity/KinematicViscosity.cs
@@ -15,7 +15,7 @@ namespace EngineeringUnits
         public KinematicViscosity(UnknownUnit value, KinematicViscosityUnit selectedUnit) : base(value, selectedUnit.Unit) { }
 
         public static KinematicViscosity From(double value, KinematicViscosityUnit unit) => new KinematicViscosity(value, unit);
-        public double As(KinematicViscosityUnit ReturnInThisUnit) => (double)ToTheOutSide(ReturnInThisUnit.Unit);
+        public double As(KinematicViscosityUnit ReturnInThisUnit) => ToTheOutSideDouble(ReturnInThisUnit.Unit);
         public KinematicViscosity ToUnit(KinematicViscosityUnit selectedUnit) => new KinematicViscosity(ToTheOutSide(selectedUnit.Unit), selectedUnit);
         public static KinematicViscosity Zero => new KinematicViscosity(0, KinematicViscosityUnit.SI);
 
