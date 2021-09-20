5f7c29ea743d7bb76e2423c844b7c5192db91b30
Mads Foged <jfogedm@jci.com>
2021-09-20 12:37:21 +0200

    ..

--- EngineeringUnits/CombinedUnits/ElectricSurfaceChargeDensity/ElectricSurfaceChargeDensity.cs
+++ EngineeringUnits/CombinedUnits/ElectricSurfaceChargeDensity/ElectricSurfaceChargeDensity.cs
@@ -14,7 +14,7 @@ namespace EngineeringUnits
         public ElectricSurfaceChargeDensity(UnknownUnit value, ElectricSurfaceChargeDensityUnit selectedUnit) : base(value, selectedUnit.Unit) { }
 
         public static ElectricSurfaceChargeDensity From(double value, ElectricSurfaceChargeDensityUnit unit) => new ElectricSurfaceChargeDensity(value, unit);
-        public double As(ElectricSurfaceChargeDensityUnit ReturnInThisUnit) => (double)ToTheOutSide(ReturnInThisUnit.Unit);
+        public double As(ElectricSurfaceChargeDensityUnit ReturnInThisUnit) => ToTheOutSideDouble(ReturnInThisUnit.Unit);
         public ElectricSurfaceChargeDensity ToUnit(ElectricSurfaceChargeDensityUnit selectedUnit) => new ElectricSurfaceChargeDensity(ToTheOutSide(selectedUnit.Unit), selectedUnit);
         public static ElectricSurfaceChargeDensity Zero => new ElectricSurfaceChargeDensity(0, ElectricSurfaceChargeDensityUnit.SI);
 
