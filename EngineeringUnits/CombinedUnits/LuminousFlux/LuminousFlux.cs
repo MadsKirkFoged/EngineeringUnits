fd724606836cf911207002f83a2362b0cb0cf1aa
Mads Foged <jfogedm@jci.com>
2021-09-20 12:30:18 +0200

    ...

--- EngineeringUnits/CombinedUnits/LuminousFlux/LuminousFlux.cs
+++ EngineeringUnits/CombinedUnits/LuminousFlux/LuminousFlux.cs
@@ -15,7 +15,7 @@ namespace EngineeringUnits
         public LuminousFlux(UnknownUnit value, LuminousFluxUnit selectedUnit) : base(value, selectedUnit.Unit) { }
 
         public static LuminousFlux From(double value, LuminousFluxUnit unit) => new LuminousFlux(value, unit);
-        public double As(LuminousFluxUnit ReturnInThisUnit) => (double)ToTheOutSide(ReturnInThisUnit.Unit);
+        public double As(LuminousFluxUnit ReturnInThisUnit) => ToTheOutSideDouble(ReturnInThisUnit.Unit);
         public LuminousFlux ToUnit(LuminousFluxUnit selectedUnit) => new LuminousFlux(ToTheOutSide(selectedUnit.Unit), selectedUnit);
         public static LuminousFlux Zero => new LuminousFlux(0, LuminousFluxUnit.SI);
 
