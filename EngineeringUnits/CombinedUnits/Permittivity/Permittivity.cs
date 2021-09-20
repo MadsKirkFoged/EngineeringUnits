d03827ffbd71dd5bd5bdf0578fcdcb8a07090d38
Mads Foged <jfogedm@jci.com>
2021-09-20 12:25:47 +0200

    ..

--- EngineeringUnits/CombinedUnits/Permittivity/Permittivity.cs
+++ EngineeringUnits/CombinedUnits/Permittivity/Permittivity.cs
@@ -16,7 +16,7 @@ namespace EngineeringUnits
         public Permittivity(UnknownUnit value, PermittivityUnit selectedUnit) : base(value, selectedUnit.Unit) { }
 
         public static Permittivity From(double value, PermittivityUnit unit) => new Permittivity(value, unit);
-        public double As(PermittivityUnit ReturnInThisUnit) => (double)ToTheOutSide(ReturnInThisUnit.Unit);
+        public double As(PermittivityUnit ReturnInThisUnit) => ToTheOutSideDouble(ReturnInThisUnit.Unit);
         public Permittivity ToUnit(PermittivityUnit selectedUnit) => new Permittivity(ToTheOutSide(selectedUnit.Unit), selectedUnit);
         public static Permittivity Zero => new Permittivity(0, PermittivityUnit.SI);
 
