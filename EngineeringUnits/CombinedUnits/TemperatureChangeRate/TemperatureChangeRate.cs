2bddcdec23dea8dc4a744a042c18159f16c607eb
Mads Foged <jfogedm@jci.com>
2021-09-20 12:20:47 +0200

    ..

--- EngineeringUnits/CombinedUnits/TemperatureChangeRate/TemperatureChangeRate.cs
+++ EngineeringUnits/CombinedUnits/TemperatureChangeRate/TemperatureChangeRate.cs
@@ -14,7 +14,7 @@ namespace EngineeringUnits
         public TemperatureChangeRate(UnknownUnit value, TemperatureChangeRateUnit selectedUnit) : base(value, selectedUnit.Unit) { }
 
         public static TemperatureChangeRate From(double value, TemperatureChangeRateUnit unit) => new TemperatureChangeRate(value, unit);
-        public double As(TemperatureChangeRateUnit ReturnInThisUnit) => (double)ToTheOutSide(ReturnInThisUnit.Unit);
+        public double As(TemperatureChangeRateUnit ReturnInThisUnit) => ToTheOutSideDouble(ReturnInThisUnit.Unit);
         public TemperatureChangeRate ToUnit(TemperatureChangeRateUnit selectedUnit) => new TemperatureChangeRate(ToTheOutSide(selectedUnit.Unit), selectedUnit);
         public static TemperatureChangeRate Zero => new TemperatureChangeRate(0, TemperatureChangeRateUnit.SI);
 
