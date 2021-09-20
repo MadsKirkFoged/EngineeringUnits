1ea3c762f20a6d9df6c792f4048c63dbe6d5eaba
Mads Foged <jfogedm@jci.com>
2021-09-20 12:45:52 +0200

    ..

--- EngineeringUnits/CombinedUnits/Area/Area.cs
+++ EngineeringUnits/CombinedUnits/Area/Area.cs
@@ -15,7 +15,7 @@ namespace EngineeringUnits
         public Area(UnknownUnit value, AreaUnit selectedUnit) : base(value, selectedUnit.Unit) { }
 
         public static Area From(double value, AreaUnit unit) => new Area(value, unit);
-        public double As(AreaUnit ReturnInThisUnit) => (double)ToTheOutSide(ReturnInThisUnit.Unit);
+        public double As(AreaUnit ReturnInThisUnit) => ToTheOutSideDouble(ReturnInThisUnit.Unit);
         public Area ToUnit(AreaUnit selectedUnit) => new Area(ToTheOutSide(selectedUnit.Unit), selectedUnit);
         public static Area Zero => new Area(0, AreaUnit.SI);
 
