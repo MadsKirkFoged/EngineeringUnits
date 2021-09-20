ad37616bc48ba5c781424646a699d25bcad517d0
Mads Foged <jfogedm@jci.com>
2021-09-20 12:17:08 +0200

    ..

--- EngineeringUnits/CombinedUnits/VolumePerLength/VolumePerLength.cs
+++ EngineeringUnits/CombinedUnits/VolumePerLength/VolumePerLength.cs
@@ -15,7 +15,7 @@ namespace EngineeringUnits
         public VolumePerLength(UnknownUnit value, VolumePerLengthUnit selectedUnit) : base(value, selectedUnit.Unit) { }
 
         public static VolumePerLength From(double value, VolumePerLengthUnit unit) => new VolumePerLength(value, unit);
-        public double As(VolumePerLengthUnit ReturnInThisUnit) => (double)ToTheOutSide(ReturnInThisUnit.Unit);
+        public double As(VolumePerLengthUnit ReturnInThisUnit) => ToTheOutSideDouble(ReturnInThisUnit.Unit);
         public VolumePerLength ToUnit(VolumePerLengthUnit selectedUnit) => new VolumePerLength(ToTheOutSide(selectedUnit.Unit), selectedUnit);
         public static VolumePerLength Zero => new VolumePerLength(0, VolumePerLengthUnit.SI);
 
