fe04bc9423ffd06708bf63103ff7d189b2cd5afa
Mads Foged <jfogedm@jci.com>
2021-09-20 12:40:43 +0200

    ..

--- EngineeringUnits/CombinedUnits/ElectricCurrentGradient/ElectricCurrentGradient.cs
+++ EngineeringUnits/CombinedUnits/ElectricCurrentGradient/ElectricCurrentGradient.cs
@@ -15,7 +15,7 @@ namespace EngineeringUnits
         public ElectricCurrentGradient(UnknownUnit value, ElectricCurrentGradientUnit selectedUnit) : base(value, selectedUnit.Unit) { }
 
         public static ElectricCurrentGradient From(double value, ElectricCurrentGradientUnit unit) => new ElectricCurrentGradient(value, unit);
-        public double As(ElectricCurrentGradientUnit ReturnInThisUnit) => (double)ToTheOutSide(ReturnInThisUnit.Unit);
+        public double As(ElectricCurrentGradientUnit ReturnInThisUnit) => ToTheOutSideDouble(ReturnInThisUnit.Unit);
         public ElectricCurrentGradient ToUnit(ElectricCurrentGradientUnit selectedUnit) => new ElectricCurrentGradient(ToTheOutSide(selectedUnit.Unit), selectedUnit);
         public static ElectricCurrentGradient Zero => new ElectricCurrentGradient(0, ElectricCurrentGradientUnit.SI);
 
