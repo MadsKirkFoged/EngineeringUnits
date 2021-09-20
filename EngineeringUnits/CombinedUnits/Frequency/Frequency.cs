b1a33f1c50cf3443181dc735ce440be6279b24c8
Mads Foged <jfogedm@jci.com>
2021-09-20 12:35:00 +0200

    ..

--- EngineeringUnits/CombinedUnits/Frequency/Frequency.cs
+++ EngineeringUnits/CombinedUnits/Frequency/Frequency.cs
@@ -15,7 +15,7 @@ namespace EngineeringUnits
         public Frequency(UnknownUnit value, FrequencyUnit selectedUnit) : base(value, selectedUnit.Unit) { }
 
         public static Frequency From(double value, FrequencyUnit unit) => new Frequency(value, unit);
-        public double As(FrequencyUnit ReturnInThisUnit) => (double)ToTheOutSide(ReturnInThisUnit.Unit);
+        public double As(FrequencyUnit ReturnInThisUnit) => ToTheOutSideDouble(ReturnInThisUnit.Unit);
         public Frequency ToUnit(FrequencyUnit selectedUnit) => new Frequency(ToTheOutSide(selectedUnit.Unit), selectedUnit);
         public static Frequency Zero => new Frequency(0, FrequencyUnit.SI);
 
