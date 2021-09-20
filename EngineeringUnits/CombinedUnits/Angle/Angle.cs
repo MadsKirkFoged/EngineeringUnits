d56e20e283956735d8304b1915697bfe6368ad65
Mads Foged <jfogedm@jci.com>
2021-09-20 12:46:44 +0200

    ..

--- EngineeringUnits/CombinedUnits/Angle/Angle.cs
+++ EngineeringUnits/CombinedUnits/Angle/Angle.cs
@@ -15,7 +15,7 @@ namespace EngineeringUnits
         public Angle(UnknownUnit value, AngleUnit selectedUnit) : base(value, selectedUnit.Unit) { }
 
         public static Angle From(double value, AngleUnit unit) => new Angle(value, unit);
-        public double As(AngleUnit ReturnInThisUnit) => (double)ToTheOutSide(ReturnInThisUnit.Unit);
+        public double As(AngleUnit ReturnInThisUnit) => ToTheOutSideDouble(ReturnInThisUnit.Unit);
         public Angle ToUnit(AngleUnit selectedUnit) => new Angle(ToTheOutSide(selectedUnit.Unit), selectedUnit);
         public static Angle Zero => new Angle(0, AngleUnit.SI);
 
