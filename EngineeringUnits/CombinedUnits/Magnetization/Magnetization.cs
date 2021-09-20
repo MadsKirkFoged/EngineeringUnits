1ac0117d0562bc07fc3021a0dfb6bbeb0db6d621
Mads Foged <jfogedm@jci.com>
2021-09-20 12:29:11 +0200

    ...

--- EngineeringUnits/CombinedUnits/Magnetization/Magnetization.cs
+++ EngineeringUnits/CombinedUnits/Magnetization/Magnetization.cs
@@ -15,7 +15,7 @@ namespace EngineeringUnits
         public Magnetization(UnknownUnit value, MagnetizationUnit selectedUnit) : base(value, selectedUnit.Unit) { }
 
         public static Magnetization From(double value, MagnetizationUnit unit) => new Magnetization(value, unit);
-        public double As(MagnetizationUnit ReturnInThisUnit) => (double)ToTheOutSide(ReturnInThisUnit.Unit);
+        public double As(MagnetizationUnit ReturnInThisUnit) => ToTheOutSideDouble(ReturnInThisUnit.Unit);
         public Magnetization ToUnit(MagnetizationUnit selectedUnit) => new Magnetization(ToTheOutSide(selectedUnit.Unit), selectedUnit);
         public static Magnetization Zero => new Magnetization(0, MagnetizationUnit.SI);
 
