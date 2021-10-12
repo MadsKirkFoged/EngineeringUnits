dd360b0fff5fdcefeef2abf6b8daf578d64065ea
Mads Foged <jfogedm@jci.com>
2021-10-12 10:31:54 +0200

    BitRate

--- EngineeringUnits/CombinedUnits/BitRate/BitRate.cs
+++ EngineeringUnits/CombinedUnits/BitRate/BitRate.cs
@@ -21,5 +21,13 @@ namespace EngineeringUnits
 
         public static implicit operator BitRate(UnknownUnit Unit) => new(Unit, BitRateUnit.SI);
 
+        public static implicit operator BitRate(int zero)
+        {
+            if (zero != 0)
+                throw new WrongUnitException($"You need to give it a unit unless you set it to 0 (zero)!");
+
+            return Zero;
+        }
+
     }
 }
