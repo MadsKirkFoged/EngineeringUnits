4b4f2b14e22afd0536b6c7d30c43d2cbf20e0777
Mads Foged <jfogedm@jci.com>
2021-10-12 10:25:24 +0200

    ElectricField

--- EngineeringUnits/CombinedUnits/ElectricField/ElectricField.cs
+++ EngineeringUnits/CombinedUnits/ElectricField/ElectricField.cs
@@ -21,6 +21,14 @@ namespace EngineeringUnits
 
         public static implicit operator ElectricField(UnknownUnit Unit) => new(Unit, ElectricFieldUnit.SI);
 
+        public static implicit operator ElectricField(int zero)
+        {
+            if (zero != 0)
+                throw new WrongUnitException($"You need to give it a unit unless you set it to 0 (zero)!");
+
+            return Zero;
+        }
+
 
     }
 }
