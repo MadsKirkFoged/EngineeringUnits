340eed7732b8d5fb1a04c940011306228e8b287b
Mads Foged <jfogedm@jci.com>
2021-10-12 10:29:46 +0200

    MassConcentration

--- EngineeringUnits/CombinedUnits/Density/MassConcentration/MassConcentration.cs
+++ EngineeringUnits/CombinedUnits/Density/MassConcentration/MassConcentration.cs
@@ -23,6 +23,14 @@ namespace EngineeringUnits
 
         public static implicit operator MassConcentration(UnknownUnit Unit) => new(Unit);
 
+        public static implicit operator MassConcentration(int zero)
+        {
+            if (zero != 0)
+                throw new WrongUnitException($"You need to give it a unit unless you set it to 0 (zero)!");
+
+            return Zero;
+        }
+
 
 
 
