bf29b5b3ed1a6c3ef2abf64b4f5945742640154a
Mads Foged <jfogedm@jci.com>
2021-10-12 10:30:21 +0200

    CoefficientOfThermalExpansion

--- EngineeringUnits/CombinedUnits/CoefficientOfThermalExpansion/CoefficientOfThermalExpansion.cs
+++ EngineeringUnits/CombinedUnits/CoefficientOfThermalExpansion/CoefficientOfThermalExpansion.cs
@@ -20,6 +20,12 @@ namespace EngineeringUnits
 
         public static implicit operator CoefficientOfThermalExpansion(UnknownUnit Unit) => new(Unit, CoefficientOfThermalExpansionUnit.SI);
 
+        public static implicit operator CoefficientOfThermalExpansion(int zero)
+        {
+            if (zero != 0)
+                throw new WrongUnitException($"You need to give it a unit unless you set it to 0 (zero)!");
 
+            return Zero;
+        }
     }
 }
