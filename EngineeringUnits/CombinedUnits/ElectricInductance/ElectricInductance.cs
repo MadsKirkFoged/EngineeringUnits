3a60ce30f866a1bc8479479a83b284284d615dcb
Mads Foged <jfogedm@jci.com>
2021-10-12 10:24:57 +0200

    ElectricInductance

--- EngineeringUnits/CombinedUnits/ElectricInductance/ElectricInductance.cs
+++ EngineeringUnits/CombinedUnits/ElectricInductance/ElectricInductance.cs
@@ -21,6 +21,14 @@ namespace EngineeringUnits
 
         public static implicit operator ElectricInductance(UnknownUnit Unit) => new(Unit, ElectricInductanceUnit.SI);
 
+        public static implicit operator ElectricInductance(int zero)
+        {
+            if (zero != 0)
+                throw new WrongUnitException($"You need to give it a unit unless you set it to 0 (zero)!");
+
+            return Zero;
+        }
+
 
     }
 }
