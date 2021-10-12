f8b3d7958d434364e87fb3a4690f9c7759963d8a
Mads Foged <jfogedm@jci.com>
2021-10-12 10:20:43 +0200

    ForceChangeRate

--- EngineeringUnits/CombinedUnits/ForceChangeRate/ForceChangeRate.cs
+++ EngineeringUnits/CombinedUnits/ForceChangeRate/ForceChangeRate.cs
@@ -21,6 +21,14 @@ namespace EngineeringUnits
 
         public static implicit operator ForceChangeRate(UnknownUnit Unit) => new(Unit, ForceChangeRateUnit.SI);
 
+        public static implicit operator ForceChangeRate(int zero)
+        {
+            if (zero != 0)
+                throw new WrongUnitException($"You need to give it a unit unless you set it to 0 (zero)!");
+
+            return Zero;
+        }
+
 
     }
 }
