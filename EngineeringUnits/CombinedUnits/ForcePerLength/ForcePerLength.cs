b8d6b03af21ad61d339c343e8816af90779dcf1f
Mads Foged <jfogedm@jci.com>
2021-10-12 10:20:19 +0200

    ForcePerLength

--- EngineeringUnits/CombinedUnits/ForcePerLength/ForcePerLength.cs
+++ EngineeringUnits/CombinedUnits/ForcePerLength/ForcePerLength.cs
@@ -21,5 +21,13 @@ namespace EngineeringUnits
 
         public static implicit operator ForcePerLength(UnknownUnit Unit) => new(Unit, ForcePerLengthUnit.SI);
 
+        public static implicit operator ForcePerLength(int zero)
+        {
+            if (zero != 0)
+                throw new WrongUnitException($"You need to give it a unit unless you set it to 0 (zero)!");
+
+            return Zero;
+        }
+
     }
 }
