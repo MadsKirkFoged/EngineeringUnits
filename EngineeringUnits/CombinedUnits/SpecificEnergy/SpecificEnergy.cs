4e89d36967f707bdc4973f0032d3952442915f6a
Mads Foged <jfogedm@jci.com>
2021-10-12 12:38:25 +0200

    SpecificEnergy

--- EngineeringUnits/CombinedUnits/SpecificEnergy/SpecificEnergy.cs
+++ EngineeringUnits/CombinedUnits/SpecificEnergy/SpecificEnergy.cs
@@ -21,6 +21,12 @@ namespace EngineeringUnits
 
         public static implicit operator SpecificEnergy(UnknownUnit Unit) => new(Unit, SpecificEnergyUnit.SI);
 
+        public static implicit operator SpecificEnergy(int zero)
+        {
+            if (zero != 0)
+                throw new WrongUnitException($"You need to give it a unit unless you set it to 0 (zero)!");
 
+            return Zero;
+        }
     }
 }
