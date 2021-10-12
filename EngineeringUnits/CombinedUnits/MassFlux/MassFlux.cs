a7d0c093b12d2a91ff85ec3f577eaef1323cf8a8
Mads Foged <jfogedm@jci.com>
2021-10-12 10:09:38 +0200

    MassFlux

--- EngineeringUnits/CombinedUnits/MassFlux/MassFlux.cs
+++ EngineeringUnits/CombinedUnits/MassFlux/MassFlux.cs
@@ -20,5 +20,13 @@ namespace EngineeringUnits
         public static MassFlux Zero => new(0, MassFluxUnit.SI);
 
         public static implicit operator MassFlux(UnknownUnit Unit) => new(Unit, MassFluxUnit.SI);
+
+        public static implicit operator MassFlux(int zero)
+        {
+            if (zero != 0)
+                throw new WrongUnitException($"You need to give it a unit unless you set it to 0 (zero)!");
+
+            return Zero;
+        }
     }
 }
