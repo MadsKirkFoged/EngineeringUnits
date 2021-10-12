98c70fd625b43388e1812eca6818596e16d82016
Mads Foged <jfogedm@jci.com>
2021-10-12 10:26:44 +0200

    ElectricConductivity

--- EngineeringUnits/CombinedUnits/ElectricConductivity/ElectricConductivity.cs
+++ EngineeringUnits/CombinedUnits/ElectricConductivity/ElectricConductivity.cs
@@ -21,6 +21,14 @@ namespace EngineeringUnits
 
         public static implicit operator ElectricConductivity(UnknownUnit Unit) => new(Unit, ElectricConductivityUnit.SI);
 
+        public static implicit operator ElectricConductivity(int zero)
+        {
+            if (zero != 0)
+                throw new WrongUnitException($"You need to give it a unit unless you set it to 0 (zero)!");
+
+            return Zero;
+        }
+
 
     }
 }
