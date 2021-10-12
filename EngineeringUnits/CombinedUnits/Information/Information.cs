3dd5cdc172df0fd634733becab14cf523e19fffd
Mads Foged <jfogedm@jci.com>
2021-10-12 10:16:41 +0200

    Information

--- EngineeringUnits/CombinedUnits/Information/Information.cs
+++ EngineeringUnits/CombinedUnits/Information/Information.cs
@@ -21,5 +21,13 @@ namespace EngineeringUnits
 
         public static implicit operator Information(UnknownUnit Unit) => new(Unit, InformationUnit.SI);
 
+        public static implicit operator Information(int zero)
+        {
+            if (zero != 0)
+                throw new WrongUnitException($"You need to give it a unit unless you set it to 0 (zero)!");
+
+            return Zero;
+        }
+
     }
 }
