0e4569a5a9b967dbf9a04e3661a27d091df2bc35
Mads Foged <jfogedm@jci.com>
2021-10-12 09:52:15 +0200

    ..

--- EngineeringUnits/CombinedUnits/VolumePerLength/VolumePerLength.cs
+++ EngineeringUnits/CombinedUnits/VolumePerLength/VolumePerLength.cs
@@ -20,5 +20,13 @@ namespace EngineeringUnits
         public static VolumePerLength Zero => new(0, VolumePerLengthUnit.SI);
 
         public static implicit operator VolumePerLength(UnknownUnit Unit) => new(Unit, VolumePerLengthUnit.SI);
+
+        public static implicit operator VolumePerLength(int zero)
+        {
+            if (zero != 0)
+                throw new WrongUnitException($"You need to give it a unit unless you set it to 0 (zero)!");
+
+            return Zero;
+        }
     }
 }
