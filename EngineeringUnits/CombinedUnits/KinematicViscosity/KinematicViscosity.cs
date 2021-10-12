82e084a7b9e1723d7cd62d7f9e967d1fd3c9d167
Mads Foged <jfogedm@jci.com>
2021-10-12 10:14:29 +0200

    KinematicViscosity

--- EngineeringUnits/CombinedUnits/KinematicViscosity/KinematicViscosity.cs
+++ EngineeringUnits/CombinedUnits/KinematicViscosity/KinematicViscosity.cs
@@ -21,7 +21,7 @@ namespace EngineeringUnits
 
         public static implicit operator KinematicViscosity(UnknownUnit Unit) => new(Unit, KinematicViscosityUnit.SI);
 
-        public static implicit operator MKinematicViscosityass(int zero)
+        public static implicit operator KinematicViscosity(int zero)
         {
             if (zero != 0)
                 throw new WrongUnitException($"You need to give it a unit unless you set it to 0 (zero)!");
