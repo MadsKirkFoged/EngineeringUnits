ddc6d4c559c6740b594684e1440468e982efd8fb
Mads Foged <jfogedm@jci.com>
2021-10-12 10:12:57 +0200

    LinearDensity

--- EngineeringUnits/CombinedUnits/LinearDensity/LinearDensity.cs
+++ EngineeringUnits/CombinedUnits/LinearDensity/LinearDensity.cs
@@ -21,5 +21,13 @@ namespace EngineeringUnits
 
         public static implicit operator LinearDensity(UnknownUnit Unit) => new(Unit, LinearDensityUnit.SI);
 
+        public static implicit operator LinearDensity(int zero)
+        {
+            if (zero != 0)
+                throw new WrongUnitException($"You need to give it a unit unless you set it to 0 (zero)!");
+
+            return Zero;
+        }
+
     }
 }
