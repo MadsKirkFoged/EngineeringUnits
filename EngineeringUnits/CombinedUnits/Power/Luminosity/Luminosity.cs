eb44ae6e59a062f6dacb018e0e520ac485d14e8e
Mads Foged <jfogedm@jci.com>
2021-10-12 10:03:40 +0200

    Luminosity

--- EngineeringUnits/CombinedUnits/Power/Luminosity/Luminosity.cs
+++ EngineeringUnits/CombinedUnits/Power/Luminosity/Luminosity.cs
@@ -24,6 +24,14 @@ namespace EngineeringUnits
 
         public static implicit operator Luminosity(UnknownUnit Unit) => new(Unit);
 
+        public static implicit operator Luminosity(int zero)
+        {
+            if (zero != 0)
+                throw new WrongUnitException($"You need to give it a unit unless you set it to 0 (zero)!");
+
+            return Zero;
+        }
+
 
 
 
