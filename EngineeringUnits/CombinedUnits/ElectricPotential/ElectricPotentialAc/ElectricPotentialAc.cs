d0594a6cc3d422703d6097834796e983c7c98402
Mads Foged <jfogedm@jci.com>
2021-10-12 10:24:37 +0200

    ElectricPotentialAc

--- EngineeringUnits/CombinedUnits/ElectricPotential/ElectricPotentialAc/ElectricPotentialAc.cs
+++ EngineeringUnits/CombinedUnits/ElectricPotential/ElectricPotentialAc/ElectricPotentialAc.cs
@@ -23,6 +23,14 @@ namespace EngineeringUnits
 
         public static implicit operator ElectricPotentialAc(UnknownUnit Unit) => new(Unit);
 
+        public static implicit operator ElectricPotentialAc(int zero)
+        {
+            if (zero != 0)
+                throw new WrongUnitException($"You need to give it a unit unless you set it to 0 (zero)!");
+
+            return Zero;
+        }
+
 
 
 
