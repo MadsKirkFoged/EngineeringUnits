23da5d8bcdd55f9b68026314634cb3b21037f8ff
Mads Foged <jfogedm@jci.com>
2021-10-12 10:31:27 +0200

    BrakeSpecificFuelConsumption

--- EngineeringUnits/CombinedUnits/BrakeSpecificFuelConsumption/BrakeSpecificFuelConsumption.cs
+++ EngineeringUnits/CombinedUnits/BrakeSpecificFuelConsumption/BrakeSpecificFuelConsumption.cs
@@ -21,6 +21,14 @@ namespace EngineeringUnits
 
         public static implicit operator BrakeSpecificFuelConsumption(UnknownUnit Unit) => new(Unit, BrakeSpecificFuelConsumptionUnit.SI);
 
+        public static implicit operator BrakeSpecificFuelConsumption(int zero)
+        {
+            if (zero != 0)
+                throw new WrongUnitException($"You need to give it a unit unless you set it to 0 (zero)!");
+
+            return Zero;
+        }
+
 
     }
 }
