c1fbc69efb6e351a4ec5df1efb2a0dcd999317fe
Mads Foged <jfogedm@jci.com>
2021-10-12 10:46:50 +0200

    AmountOfSubstance

--- EngineeringUnits/BaseUnits/Amount/Amount.cs
+++ EngineeringUnits/BaseUnits/Amount/Amount.cs
@@ -26,6 +26,14 @@ namespace EngineeringUnits
 
         public static implicit operator AmountOfSubstance(UnknownUnit Unit) => new(Unit);
 
+        public static implicit operator AmountOfSubstance(int zero)
+        {
+            if (zero != 0)
+                throw new WrongUnitException($"You need to give it a unit unless you set it to 0 (zero)!");
+
+            return Zero;
+        }
+
     }
 }
 
