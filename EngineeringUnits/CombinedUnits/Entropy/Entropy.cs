98486237221f8dbc1ce40f72b67bf224daa8f669
Mads Foged <jfogedm@jci.com>
2021-10-12 10:21:29 +0200

    Entropy

--- EngineeringUnits/CombinedUnits/Entropy/Entropy.cs
+++ EngineeringUnits/CombinedUnits/Entropy/Entropy.cs
@@ -21,6 +21,14 @@ namespace EngineeringUnits
 
         public static implicit operator Entropy(UnknownUnit Unit) => new(Unit, EntropyUnit.SI);
 
+        public static implicit operator Entropy(int zero)
+        {
+            if (zero != 0)
+                throw new WrongUnitException($"You need to give it a unit unless you set it to 0 (zero)!");
+
+            return Zero;
+        }
+
 
     }
 }
