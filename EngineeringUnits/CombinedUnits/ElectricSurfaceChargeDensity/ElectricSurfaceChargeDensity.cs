26d8d265bbd1f07dd1bb7f12af64ed2d423dcf08
Mads Foged <jfogedm@jci.com>
2021-10-12 10:22:15 +0200

    ElectricSurfaceChargeDensity

--- EngineeringUnits/CombinedUnits/ElectricSurfaceChargeDensity/ElectricSurfaceChargeDensity.cs
+++ EngineeringUnits/CombinedUnits/ElectricSurfaceChargeDensity/ElectricSurfaceChargeDensity.cs
@@ -20,6 +20,14 @@ namespace EngineeringUnits
 
         public static implicit operator ElectricSurfaceChargeDensity(UnknownUnit Unit) => new(Unit, ElectricSurfaceChargeDensityUnit.SI);
 
+        public static implicit operator ElectricSurfaceChargeDensity(int zero)
+        {
+            if (zero != 0)
+                throw new WrongUnitException($"You need to give it a unit unless you set it to 0 (zero)!");
+
+            return Zero;
+        }
+
 
     }
 }
