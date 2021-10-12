9565616e698a31d4946960153107f685f550a12f
Mads Foged <jfogedm@jci.com>
2021-10-12 09:54:40 +0200

    ..

--- EngineeringUnits/CombinedUnits/Torque/Torque.cs
+++ EngineeringUnits/CombinedUnits/Torque/Torque.cs
@@ -20,5 +20,13 @@ namespace EngineeringUnits
 
         public static implicit operator Torque(UnknownUnit Unit) => new(Unit, TorqueUnit.SI);
 
+        public static implicit operator Torque(int zero)
+        {
+            if (zero != 0)
+                throw new WrongUnitException($"You need to give it a unit unless you set it to 0 (zero)!");
+
+            return Zero;
+        }
+
     }
 }
