8956f9868ddea970392bf3f4a660d5c3fed3c307
Mads Foged <jfogedm@jci.com>
2021-10-12 10:22:37 +0200

    ElectricResistivity

--- EngineeringUnits/CombinedUnits/ElectricResistivity/ElectricResistivity.cs
+++ EngineeringUnits/CombinedUnits/ElectricResistivity/ElectricResistivity.cs
@@ -21,5 +21,13 @@ namespace EngineeringUnits
 
         public static implicit operator ElectricResistivity(UnknownUnit Unit) => new(Unit, ElectricResistivityUnit.SI);
 
+        public static implicit operator ElectricResistivity(int zero)
+        {
+            if (zero != 0)
+                throw new WrongUnitException($"You need to give it a unit unless you set it to 0 (zero)!");
+
+            return Zero;
+        }
+
     }
 }
