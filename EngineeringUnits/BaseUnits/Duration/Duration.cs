d5608dcd6605cbd2d842ad576c9b91bc3acf22a5
Mads Foged <jfogedm@jci.com>
2021-10-12 10:46:30 +0200

    Duration

--- EngineeringUnits/BaseUnits/Duration/Duration.cs
+++ EngineeringUnits/BaseUnits/Duration/Duration.cs
@@ -21,6 +21,14 @@ namespace EngineeringUnits
 
         public static implicit operator Duration(UnknownUnit Unit) => new(Unit, DurationUnit.SI);
 
+        public static implicit operator Duration(int zero)
+        {
+            if (zero != 0)
+                throw new WrongUnitException($"You need to give it a unit unless you set it to 0 (zero)!");
+
+            return Zero;
+        }
+
 
     }
 }
