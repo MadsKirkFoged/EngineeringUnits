62fa65144873b1dd5c344da2820fd92218cd72a4
Mads Foged <jfogedm@jci.com>
2021-10-12 10:28:30 +0200

    ElectricAdmittance

--- EngineeringUnits/CombinedUnits/ElectricAdmittance/ElectricAdmittance.cs
+++ EngineeringUnits/CombinedUnits/ElectricAdmittance/ElectricAdmittance.cs
@@ -21,6 +21,14 @@ namespace EngineeringUnits
 
         public static implicit operator ElectricAdmittance(UnknownUnit Unit) => new(Unit, ElectricAdmittanceUnit.SI);
 
+        public static implicit operator ElectricAdmittance(int zero)
+        {
+            if (zero != 0)
+                throw new WrongUnitException($"You need to give it a unit unless you set it to 0 (zero)!");
+
+            return Zero;
+        }
+
 
     }
 }
