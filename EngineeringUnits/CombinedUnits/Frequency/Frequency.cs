6b343723ee1a92613c3bc8eacf2c3f7b2c0ee12b
Mads Foged <jfogedm@jci.com>
2021-10-12 10:19:48 +0200

    Frequency

--- EngineeringUnits/CombinedUnits/Frequency/Frequency.cs
+++ EngineeringUnits/CombinedUnits/Frequency/Frequency.cs
@@ -21,6 +21,14 @@ namespace EngineeringUnits
 
         public static implicit operator Frequency(UnknownUnit Unit) => new(Unit, FrequencyUnit.SI);
 
+        public static implicit operator Frequency(int zero)
+        {
+            if (zero != 0)
+                throw new WrongUnitException($"You need to give it a unit unless you set it to 0 (zero)!");
+
+            return Zero;
+        }
+
 
 
     }
