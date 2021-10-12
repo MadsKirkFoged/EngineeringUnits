286fb62299775c5a71ed3777d966523cc8c81843
Mads Foged <jfogedm@jci.com>
2021-10-12 09:55:37 +0200

    ..

--- EngineeringUnits/CombinedUnits/ThermalConductivity/ThermalConductivity.cs
+++ EngineeringUnits/CombinedUnits/ThermalConductivity/ThermalConductivity.cs
@@ -20,6 +20,12 @@ namespace EngineeringUnits
 
         public static implicit operator ThermalConductivity(UnknownUnit Unit) => new(Unit, ThermalConductivityUnit.SI);
 
+        public static implicit operator ThermalConductivity(int zero)
+        {
+            if (zero != 0)
+                throw new WrongUnitException($"You need to give it a unit unless you set it to 0 (zero)!");
 
+            return Zero;
+        }
     }
 }
