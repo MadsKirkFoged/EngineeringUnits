0e9d359dbc4b13538cb0d8a23f1d6196a7941a85
Mads Foged <jfogedm@jci.com>
2021-10-12 09:55:06 +0200

    ..

--- EngineeringUnits/CombinedUnits/ThermalResistance/ThermalResistance.cs
+++ EngineeringUnits/CombinedUnits/ThermalResistance/ThermalResistance.cs
@@ -19,5 +19,13 @@ namespace EngineeringUnits
         public static ThermalResistance Zero => new(0, ThermalResistanceUnit.SI);
 
         public static implicit operator ThermalResistance(UnknownUnit Unit) => new(Unit, ThermalResistanceUnit.SI);
+
+        public static implicit operator ThermalResistance(int zero)
+        {
+            if (zero != 0)
+                throw new WrongUnitException($"You need to give it a unit unless you set it to 0 (zero)!");
+
+            return Zero;
+        }
     }
 }
