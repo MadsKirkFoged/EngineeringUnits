ec6b06e8a2f5363a57e2f8cc76e4c381ab388410
Mads Foged <jfogedm@jci.com>
2021-10-12 10:35:10 +0200

    Acceleration

--- EngineeringUnits/CombinedUnits/Acceleration/Acceleration.cs
+++ EngineeringUnits/CombinedUnits/Acceleration/Acceleration.cs
@@ -21,6 +21,14 @@ namespace EngineeringUnits
 
         public static implicit operator Acceleration(UnknownUnit Unit) => new(Unit, AccelerationUnit.SI);
 
+        public static implicit operator Acceleration(int zero)
+        {
+            if (zero != 0)
+                throw new WrongUnitException($"You need to give it a unit unless you set it to 0 (zero)!");
+
+            return Zero;
+        }
+
 
     }
 }
