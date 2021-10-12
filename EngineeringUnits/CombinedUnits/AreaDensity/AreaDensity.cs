ca1fbd94a3937608a1e3de9198cf9cf5f4aa51b0
Mads Foged <jfogedm@jci.com>
2021-10-12 10:32:53 +0200

    AreaDensity

--- EngineeringUnits/CombinedUnits/AreaDensity/AreaDensity.cs
+++ EngineeringUnits/CombinedUnits/AreaDensity/AreaDensity.cs
@@ -21,6 +21,14 @@ namespace EngineeringUnits
 
         public static implicit operator AreaDensity(UnknownUnit Unit) => new(Unit, AreaDensityUnit.SI);
 
+        public static implicit operator AreaDensity(int zero)
+        {
+            if (zero != 0)
+                throw new WrongUnitException($"You need to give it a unit unless you set it to 0 (zero)!");
+
+            return Zero;
+        }
+
 
     }
 }
