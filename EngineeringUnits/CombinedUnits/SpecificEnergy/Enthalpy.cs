d3a4ecf5e257debe2b306ab7048b2cd20994ca79
Mads Foged <jfogedm@jci.com>
2021-10-12 09:58:46 +0200

    ..

--- EngineeringUnits/CombinedUnits/SpecificEnergy/Enthalpy.cs
+++ EngineeringUnits/CombinedUnits/SpecificEnergy/Enthalpy.cs
@@ -24,6 +24,13 @@ namespace EngineeringUnits
 
         public static implicit operator Enthalpy(UnknownUnit Unit) => new(Unit, SpecificEnergyUnit.SI);
 
+        public static implicit operator Enthalpy(int zero)
+        {
+            if (zero != 0)
+                throw new WrongUnitException($"You need to give it a unit unless you set it to 0 (zero)!");
+
+            return Zero;
+        }
 
         //Alias
         public static implicit operator Enthalpy(SpecificEnergy Unit) => new(Unit);
