f0b9796b53b8581fd83f8f7bd25ea97d04da3523
Mads Foged <jfogedm@jci.com>
2021-10-12 10:01:47 +0200

    ..

--- EngineeringUnits/CombinedUnits/PressureChangeRate/PressureChangeRate.cs
+++ EngineeringUnits/CombinedUnits/PressureChangeRate/PressureChangeRate.cs
@@ -20,5 +20,13 @@ namespace EngineeringUnits
         public static PressureChangeRate Zero => new(0, PressureChangeRateUnit.SI);
 
         public static implicit operator PressureChangeRate(UnknownUnit Unit) => new(Unit, PressureChangeRateUnit.SI);
+
+        public static implicit operator PressureChangeRate(int zero)
+        {
+            if (zero != 0)
+                throw new WrongUnitException($"You need to give it a unit unless you set it to 0 (zero)!");
+
+            return Zero;
+        }
     }
 }
