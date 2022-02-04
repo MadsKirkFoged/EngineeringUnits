f635f9c50c9e86f508a84a6ecce8542d72d4838d
Mads Foged <jfogedm@jci.com>
2022-02-04 16:03:52 +0100

    ..

--- EngineeringUnits/CombinedUnits/Entropy/Entropy.cs
+++ EngineeringUnits/CombinedUnits/Entropy/Entropy.cs
@@ -19,7 +19,7 @@ namespace EngineeringUnits
         public Entropy ToUnit(EntropyUnit selectedUnit) => new(ToTheOutSide(selectedUnit.Unit), selectedUnit);
         public static Entropy Zero => new(0, EntropyUnit.SI);
 
-        public static implicit operator Entropy(UnknownUnit Unit) => new(Unit, EntropyUnit.SI);
+        public static implicit operator Entropy(UnknownUnit Unit) => new(Unit);
 
         public static implicit operator Entropy(int zero)
         {
