7f02ac3e3d01a73f7f2b93ebfd31c4cf2a3eb770
Mads Foged <jfogedm@jci.com>
2022-02-04 09:40:35 +0100

    ..

--- EngineeringUnits/BaseUnits/Length/LengthEnum.cs
+++ EngineeringUnits/BaseUnits/Length/LengthEnum.cs
@@ -34,7 +34,7 @@ namespace EngineeringUnits.Units
         public static readonly LengthUnit Hand =         new("h",         4 * 0.0254m);
         public static readonly LengthUnit Foot =         new("ft",        12 * 0.0254m);
         public static readonly LengthUnit Yard =         new("yd",        36 * 0.0254m);
-        public static readonly LengthUnit Fathom =       new("fathom",    72,             0.0254m);
+        public static readonly LengthUnit Fathom =       new("fathom",    72 * 0.0254m);
         public static readonly LengthUnit Chain =        new("ch",        792,            0.0254m);
         public static readonly LengthUnit Shackle =      new("shackle",   1080,           0.0254m);
         public static readonly LengthUnit Mile =         new("mi",        63360,          0.0254m);
