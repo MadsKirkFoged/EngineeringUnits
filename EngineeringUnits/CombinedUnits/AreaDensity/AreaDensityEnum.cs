8f10dfc56f713506048f725ab9af2435664757c8
Mads Foged <jfogedm@jci.com>
2022-02-12 23:39:23 +0100

    ..

--- EngineeringUnits/CombinedUnits/AreaDensity/AreaDensityEnum.cs
+++ EngineeringUnits/CombinedUnits/AreaDensity/AreaDensityEnum.cs
@@ -16,26 +16,13 @@ namespace EngineeringUnits.Units
 
 
 
-        public AreaDensityUnit(MassUnit mass, AreaUnit area, string NewSymbol = "Empty", decimal correction = 1)
+        public AreaDensityUnit(MassUnit mass, AreaUnit area, string NewSymbol = "Empty")
         {
             Unit = mass / area;
-            SetCombined(correction);
             SetNewSymbol(NewSymbol);
         }
 
-        public AreaDensityUnit(PreFix SI, AreaDensityUnit unit)
-        {
-            Unit = unit.Unit.Copy();
-            SetCombined(SI);
-            SetNewSymbol(SI);
-        }
 
-        public AreaDensityUnit(AreaDensityUnit unit, string NewSymbol = "Empty", decimal correction = 1)
-        {
-            Unit = unit.Unit.Copy();
-            SetCombined(correction);
-            SetNewSymbol(NewSymbol);
-        }
 
     }
 
