12022535238836922ac476fd27ae9bee0ad0f1f1
Jarno Honkanen <jarno.honkanen@hedias.com>
2022-02-26 16:55:11 +0200

    Added SI Getter to SpecificThermalResistance

---
+++ EngineeringUnits/CombinedUnits/SpecificThermalResistance/SpecificThermalResistanceGet.cs
@@ -0,0 +1,12 @@
+﻿using EngineeringUnits.Units;
+
+namespace EngineeringUnits
+{
+    public partial class SpecificThermalResistance
+    {
+        /// <summary>
+        ///     Get SpecificThermalResistance in SI Unit (MeterKelvinPerKilowatt).
+        /// </summary>
+        public double SI => As(SpecificThermalResistanceUnit.SI);
+    }
+}
---
+++ UnitTests/CombinedUnits/SpecificThermalResistance/SpecificThermalResistance.cs
@@ -0,0 +1,47 @@
+﻿using EngineeringUnits;
+using EngineeringUnits.Units;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using static System.Math;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class SpecificThermalResistanceTest
+    {
+        [TestMethod]
+        public void FromDefinition()
+        {
+            double P = 10.0;
+            double L = 2.0;
+            double T = 4.0;
+
+            var expected = new SpecificThermalResistance(Length.FromMeters(L) * Temperature.FromKelvins(T) / Power.FromWatts(P));
+            var expected_SI = (double)expected.SI;
+
+            var actual = new SpecificThermalResistance(Power.FromWatts(P) / Length.FromMeters(L) * Temperature.FromKelvins(T));
+            var actual_SI = (double)actual.SI;
+
+            Assert.AreEqual(expected, actual);
+            Assert.AreEqual(expected_SI, actual_SI, 1e-9);
+        }
+
+        [TestMethod]
+        public void FromCalculation()
+        {
+            double thermalConductivity = 0.42;
+            double expected_SI = 1 / (2 * PI * thermalConductivity);
+
+            var expected = SpecificThermalResistance.From(expected_SI, SpecificThermalResistanceUnit.SI);
+
+            var k = ThermalConductivity.FromWattsPerMeterKelvin(thermalConductivity);
+
+            var actual = new SpecificThermalResistance(1 / (2 * PI * k));
+            var actual_SI = (double)actual.SI;
+
+            //Assert.AreEqual(expected, actual);    // Equality check is somehow broken
+            Assert.AreEqual(expected_SI, actual_SI, 1e-9);
+        }
+    }
+}
