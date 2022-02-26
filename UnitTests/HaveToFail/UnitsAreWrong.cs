c3a825282939496fe39cec63830f87af2cc72da9
Mads Foged <jfogedm@jci.com>
2022-02-26 11:16:35 +0100

    added unit tests for ExpectedException

---
+++ UnitTests/HaveToFail/UnitsAreWrong.cs
@@ -0,0 +1,24 @@
+﻿using EngineeringUnits;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests.HaveToFail
+{
+    [TestClass]
+    public class UnitsAreWrong
+    {
+        [TestMethod]
+        [ExpectedException(typeof(WrongUnitException))]
+        public void AreaCantBeVolume()
+        {
+            var length = Length.FromYards(1);
+            var length2 = Length.FromMeters(5);
+
+            Area area2 = length * length2 * length2;
+        }
+    }
+}
