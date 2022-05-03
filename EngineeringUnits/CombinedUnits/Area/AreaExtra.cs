b78f1e112d5ef7434a51255c3b89f85c62c1d8b3
Mads Foged <jfogedm@jci.com>
2022-05-03 12:20:48 +0200

    ..

--- EngineeringUnits/CombinedUnits/Area/AreaExtra.cs
+++ EngineeringUnits/CombinedUnits/Area/AreaExtra.cs
@@ -5,6 +5,22 @@ using System.Text;
 
 namespace EngineeringUnits
 {
+
+    public partial class Area
+    {
+        /// <summary>Get circle area from a diameter.</summary>
+        public static Area FromCircleDiameter(Length diameter)
+        {
+            return FromCircleRadius(diameter / 2);
+        }
+
+        /// <summary>Get circle area from a radius.</summary>
+        public static Area FromCircleRadius(Length radius) => radius * radius * Math.PI;
+
+
+    }
+
+
     public static class AreaExtra
     {
 
