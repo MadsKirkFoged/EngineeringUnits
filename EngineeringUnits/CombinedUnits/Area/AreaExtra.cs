051200e2599a0c44b6f405e12fe0ad0599874535
Mads Foged <jfogedm@jci.com>
2022-05-03 11:05:42 +0200

    Area extentions class

--- EngineeringUnits/CombinedUnits/Area/AreaExtra.cs
+++ EngineeringUnits/CombinedUnits/Area/AreaExtra.cs
@@ -5,19 +5,20 @@ using System.Text;
 
 namespace EngineeringUnits
 {
-    public partial class Area
+    public static class AreaExtra
     {
 
         /// <summary>Get circle area from a diameter.</summary>
-        public static Area FromCircleDiameter(Length diameter)
+        public static Area FromCircleDiameter(this Length diameter)
         {
             return FromCircleRadius(diameter / 2);
         }
 
         /// <summary>Get circle area from a radius.</summary>
-        public static Area FromCircleRadius(Length radius) => radius * radius * Math.PI;
+        public static Area FromCircleRadius(this Length radius) => radius * radius * Math.PI;
 
 
 
     }
+
 }
