6ac8714b94e65d4507b848627186e2e1c13e074f
Mads Foged <jfogedm@jci.com>
2022-08-16 12:45:35 +0200

    added VolumetricHeatTransferCoefficient

---
+++ EngineeringUnits/CombinedUnits/VolumetricHeatTransferCoefficient/VolumetricHeatTransferCoefficient.cs
@@ -0,0 +1,47 @@
+
+using EngineeringUnits.Units;
+
+
+namespace EngineeringUnits
+{
+    //This class is auto-generated, changes to the file will be overwritten!
+    public partial class VolumetricHeatTransferCoefficient : BaseUnit
+    {
+
+        public VolumetricHeatTransferCoefficient() { }
+        public VolumetricHeatTransferCoefficient(decimal value, VolumetricHeatTransferCoefficientUnit selectedUnit) : base(value, selectedUnit.Unit) { }
+        public VolumetricHeatTransferCoefficient(double value, VolumetricHeatTransferCoefficientUnit selectedUnit) : base(value, selectedUnit.Unit) { }
+        public VolumetricHeatTransferCoefficient(int value, VolumetricHeatTransferCoefficientUnit selectedUnit) : base(value, selectedUnit.Unit) { }
+        public VolumetricHeatTransferCoefficient(UnknownUnit value) : base(value) { }
+
+        public static VolumetricHeatTransferCoefficient From(double value, VolumetricHeatTransferCoefficientUnit unit) => new(value, unit);
+
+        public static VolumetricHeatTransferCoefficient From(double? value, VolumetricHeatTransferCoefficientUnit unit)
+        {
+            if (value is null || unit is null)
+            {
+                return null;
+            }
+
+            return From((double)value, unit);
+        }
+        public double As(VolumetricHeatTransferCoefficientUnit ReturnInThisUnit) => GetValueAsDouble(ReturnInThisUnit.Unit);
+        public VolumetricHeatTransferCoefficient ToUnit(VolumetricHeatTransferCoefficientUnit selectedUnit) => new(GetValueAs(selectedUnit.Unit), selectedUnit);
+        public static VolumetricHeatTransferCoefficient Zero => new(0, VolumetricHeatTransferCoefficientUnit.SI);
+
+        public static implicit operator VolumetricHeatTransferCoefficient(UnknownUnit Unit)
+        {
+            UnitCheck(Unit, VolumetricHeatTransferCoefficientUnit.SI);
+            return new(Unit);        
+        }
+
+        public static implicit operator VolumetricHeatTransferCoefficient(int zero)
+        {
+            if (zero != 0)
+                throw new WrongUnitException("You need to give it a unit unless you set it to 0(zero)!");
+			return Zero;
+		}
+	public override string GetStandardSymbol(UnitSystem _unit) => GetStandardSymbol<VolumetricHeatTransferCoefficientUnit>(_unit);
+	}
+}
+
---
+++ EngineeringUnits/CombinedUnits/VolumetricHeatTransferCoefficient/VolumetricHeatTransferCoefficientEnum.cs
@@ -0,0 +1,45 @@
+﻿using EngineeringUnits.Units;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace EngineeringUnits.Units
+{
+
+
+    public record VolumetricHeatTransferCoefficientUnit : UnitTypebase
+    {
+
+        public static readonly VolumetricHeatTransferCoefficientUnit SI = new VolumetricHeatTransferCoefficientUnit(PowerUnit.SI, VolumeUnit.SI, TemperatureUnit.SI);
+
+        public static readonly VolumetricHeatTransferCoefficientUnit WattPerCubicMeterKelvin = new VolumetricHeatTransferCoefficientUnit(PowerUnit.Watt, VolumeUnit.CubicMeter, TemperatureUnit.Kelvin);
+
+
+
+
+        public VolumetricHeatTransferCoefficientUnit(PowerUnit power, VolumeUnit volume, TemperatureUnit temperature)
+        {
+            base.Unit = new UnitSystem(power / (volume * temperature), $"{power}/{volume}{temperature}");
+        }
+
+        public VolumetricHeatTransferCoefficientUnit(PowerUnit power, VolumeUnit volume, TemperatureUnit temperature, string NewSymbol)
+        {
+            base.Unit = new UnitSystem(power / (volume * temperature), NewSymbol);
+        }
+
+        public override string ToString()
+        {
+            if (base.Unit.Symbol != null)
+            {
+                return base.Unit.Symbol ?? "";
+            }
+
+            return $"{base.Unit}";
+        }
+
+    }
+
+
+
+
+}
