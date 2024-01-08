20df966b79b4d7f31283525005500e7fe0b91ba4
Mads Foged <jfogedm@jci.com>
2024-01-08 10:58:34 +0100

    changed doc

--- EngineeringUnits/UnknownUnit.cs
+++ EngineeringUnits/UnknownUnit.cs
@@ -8,9 +8,7 @@ using System.Globalization;
 
 namespace EngineeringUnits
 {
-
-    // UnknownUnit is just a copy of a BaseUnit
-    // At first we tried not having the class but we got errors from the implicit operators
+    
     // All calculations between two units gets converted into an UnknownUnit
     // --> ex lenght / duration turns into a UnknownUnit
     // --> because at this time the unit is Unknown
@@ -27,63 +25,6 @@ namespace EngineeringUnits
         public UnknownUnit(double valueLocalUnit, UnitSystem unitsystem) : base(valueLocalUnit, unitsystem) { }
         public UnknownUnit(decimal valueLocalUnit, UnitSystem unitsystem) : base(valueLocalUnit, unitsystem) { }
         public UnknownUnit(BaseUnit baseunit) : base(baseunit) { }
-
-        //public static UnknownUnit operator *(UnknownUnit left, UnknownUnit right)
-        //{
-        //    if (left is null || right is null)
-        //        return null;
-
-        //    try
-        //    {
-        //        var NewTestValue = left.NEWValue * right.NEWValue;
-
-        //        return new UnknownUnit(NewTestValue, left.Unit * right.Unit);
-
-        //    }
-        //    catch (OverflowException)
-        //    {
-        //        return new UnknownUnit(double.PositiveInfinity, left.Unit * right.Unit);
-        //    }
-        //}
-        //public static UnknownUnit operator /(UnknownUnit left, UnknownUnit right)
-        //{
-        //    if (left is null || right is null)
-        //        return null;
-
-        //    if (right.NEWValue == 0m)
-        //        return new UnknownUnit(double.PositiveInfinity, left.Unit / right.Unit);
-
-        //    try
-        //    {
-        //        var NewTestValue = left.NEWValue / right.NEWValue;
-
-        //        return new UnknownUnit(NewTestValue, left.Unit / right.Unit);
-
-        //    }
-        //    catch (OverflowException)
-        //    {
-        //        return new UnknownUnit(double.PositiveInfinity, left.Unit / right.Unit);
-        //    }
-        //}
-
-
-        //public static explicit operator double(UnknownUnit Unit)
-        //{
-        //    if (UnitSystemExtensions.UnitsystemForDouble != Unit)            
-        //        throw new WrongUnitException($"This is NOT a double [-] as expected! Your Unit is a [{Unit.Unit}] ");
-            
-
-        //    return (double)Unit.GetValueAs(UnitSystemExtensions.UnitsystemForDouble);
-        //}
-        //public static explicit operator decimal(UnknownUnit Unit)
-        //{
-        //    if (new UnitSystem() != Unit.Unit)            
-        //        throw new WrongUnitException($"This is NOT a decimal [-] as expected! Your Unit is a [{Unit.Unit}] ");
-            
-
-        //    return Unit.GetValueAs(new UnitSystem());
-        //}
-
 
 
         public override string ToString()
