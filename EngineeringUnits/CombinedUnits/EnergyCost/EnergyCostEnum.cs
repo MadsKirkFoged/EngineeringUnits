b1f447eafff0396ae4d1d52117d92769a655ed09
Max Schmitz <max.schmitz@energyvault.com>
2024-01-23 14:29:44 +0000

    Adding new units

--- EngineeringUnits/CombinedUnits/EnergyCost/EnergyCostEnum.cs
+++ EngineeringUnits/CombinedUnits/EnergyCost/EnergyCostEnum.cs
@@ -15,6 +15,12 @@ public partial record EnergyCostUnit : UnitTypebase
     public static readonly EnergyCostUnit USDollarPerGigajoule =    new(CostUnit.USDollar, EnergyUnit.Gigajoule);
     public static readonly EnergyCostUnit USDollarPerKilowattHour = new(CostUnit.USDollar, EnergyUnit.KilowattHour);
     public static readonly EnergyCostUnit USDollarPerMegawattHour = new(CostUnit.USDollar, EnergyUnit.MegawattHour);
+    public static readonly EnergyCostUnit MillionUSDollarPerJoule =        new(CostUnit.MillionUSDollar, EnergyUnit.Joule);
+    public static readonly EnergyCostUnit MillionUSDollarPerKilojoule =    new(CostUnit.MillionUSDollar, EnergyUnit.Kilojoule);
+    public static readonly EnergyCostUnit MillionUSDollarPerMegajoule =    new(CostUnit.MillionUSDollar, EnergyUnit.Megajoule);
+    public static readonly EnergyCostUnit MillionUSDollarPerGigajoule =    new(CostUnit.MillionUSDollar, EnergyUnit.Gigajoule);
+    public static readonly EnergyCostUnit MillionUSDollarPerKilowattHour = new(CostUnit.MillionUSDollar, EnergyUnit.KilowattHour);
+    public static readonly EnergyCostUnit MillionUSDollarPerMegawattHour = new(CostUnit.MillionUSDollar, EnergyUnit.MegawattHour);
 
 
     public EnergyCostUnit(CostUnit cost, EnergyUnit energy)
--- EngineeringUnits/CombinedUnits/MassCost/MassCostEnum.cs
+++ EngineeringUnits/CombinedUnits/MassCost/MassCostEnum.cs
@@ -9,7 +9,9 @@ public partial record MassCostUnit : UnitTypebase
 {
     public static readonly MassCostUnit SI = new(CostUnit.SI, MassUnit.SI);
     public static readonly MassCostUnit EuroPerKilogram = new(CostUnit.Euro, MassUnit.Kilogram);
-    public static readonly MassCostUnit USDollarPerKilogram = new(CostUnit.USDollar, MassUnit.Kilogram);
+    public static readonly MassCostUnit USDollarPerKilogram =     new(CostUnit.USDollar, MassUnit.Kilogram);
+    public static readonly MassCostUnit USDollarPerTonne =        new(CostUnit.USDollar, MassUnit.Tonne);
+    public static readonly MassCostUnit MillionUSDollarPerTonne = new(CostUnit.MillionUSDollar, MassUnit.Tonne);
 
     public MassCostUnit(CostUnit cost, MassUnit mass)
     {
