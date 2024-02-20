b63f6718404eccda53a26d31888e4d7c13f46e6e
Max <schmitz.max@gmail.com>
2024-02-20 16:09:33 +0000

    Extending tests to all other math operators

--- UnitTests/Functionality/IsNaN.cs
+++ UnitTests/Functionality/IsNaN.cs
@@ -46,7 +46,41 @@ public class IsNaN
         Length lengthNan = Length.NaN;
 
         Length lengthSumShouldBeNan = lengthNotNan + lengthNan;
+        Length lengthPlusShouldBeNan = +lengthNan;
+        Length lengthDiffShouldBeNan1 = lengthNotNan - lengthNan;
+        Length lengthDiffShouldBeNan2 = lengthNan - lengthNotNan;
+        Length lengthMinusShouldBeNan = -lengthNan;
+        Area areaProdShouldBeNan1 = lengthNotNan * lengthNan;
+        Area areaProdShouldBeNan2 = lengthNan * lengthNotNan;
+        Ratio ratioDivShouldBeNan1 = lengthNotNan / lengthNan;
+        Ratio ratioDivShouldBeNan2 = lengthNan / lengthNotNan;
 
         Assert.IsTrue(lengthSumShouldBeNan.IsNaN());
+        Assert.IsTrue(lengthPlusShouldBeNan.IsNaN());
+        Assert.IsTrue(lengthDiffShouldBeNan1.IsNaN());
+        Assert.IsTrue(lengthDiffShouldBeNan2.IsNaN());
+        Assert.IsTrue(lengthMinusShouldBeNan.IsNaN());
+        Assert.IsTrue(areaProdShouldBeNan1.IsNaN());
+        Assert.IsTrue(areaProdShouldBeNan2.IsNaN());
+        Assert.IsTrue(ratioDivShouldBeNan1.IsNaN());
+        Assert.IsTrue(ratioDivShouldBeNan2.IsNaN());
+
+        double valNan = double.NaN;
+
+        Length lengthSumShouldBeNanD = lengthNotNan + valNan;
+        Length lengthDiffShouldBeNanD1 = lengthNotNan - valNan;
+        Length lengthDiffShouldBeNanD2 = valNan - lengthNotNan;
+        Length lengthProdShouldBeNanD1 = lengthNotNan * valNan;
+        Length lengthProdShouldBeNanD2 = valNan * lengthNotNan;
+        Length lengthDivShouldBeNanD1 = lengthNotNan / valNan;
+        Length lengthDivShouldBeNanD2 = valNan / lengthNotNan;
+
+        Assert.IsTrue(lengthSumShouldBeNanD.IsNaN());
+        Assert.IsTrue(lengthDiffShouldBeNanD1.IsNaN());
+        Assert.IsTrue(lengthDiffShouldBeNanD2.IsNaN());
+        Assert.IsTrue(lengthProdShouldBeNanD1.IsNaN());
+        Assert.IsTrue(lengthProdShouldBeNanD2.IsNaN());
+        Assert.IsTrue(lengthDivShouldBeNanD1.IsNaN());
+        Assert.IsTrue(lengthDivShouldBeNanD2.IsNaN());
     }
 }
