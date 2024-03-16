7a7572d5379630227bf3df43bc9247f33039338b
Mads Foged <jfogedm@jci.com>
2024-03-16 13:40:48 +0100

    Testing getting netstandard back

---
+++ testing/Program.cs
@@ -0,0 +1,18 @@
+﻿using EngineeringUnits;
+using System;
+using System.Diagnostics;
+
+namespace testing
+{
+    internal class Program
+    {
+        [Obsolete]
+        private static void Main(string[] args)
+        {
+            var test = Power.FromWatts(100);
+
+            Debug.Print(test.ToString());
+
+        }
+    }
+}
