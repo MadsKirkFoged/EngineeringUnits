308a88d63ce71a758f40660fdb0310cd8a3d0149
Max <schmitz.max@gmail.com>
2025-02-10 18:21:19 +0100

    Tests belong to VolumeTests

--- UnitTests/CombinedUnits/RatioTests.VolumeConcentration.cs
+++ UnitTests/CombinedUnits/RatioTests.VolumeConcentration.cs
@@ -64,24 +64,4 @@ public partial class RatioTests
         Assert.AreEqual(20, WorkingCompares);
 
     }
-
-    [TestMethod]
-    public void FromVolume01()
-    {
-        double? value = null;
-
-        var v = EngineeringUnits.Volume.From(value, null);
-        Assert.IsNull(v);
-
-    }
-    [TestMethod]
-    public void FromVolume02()
-    {
-        double? value = 100;
-        var v = EngineeringUnits.Volume.From(value, VolumeUnit.CubicFoot);
-        var v2 = EngineeringUnits.Volume.From(value, null);
-        Assert.IsNotNull(v);
-        Assert.IsNull(v2);
-
-    }
 }
---
+++ UnitTests/CombinedUnits/VolumeTests.cs
@@ -0,0 +1,30 @@
+﻿using EngineeringUnits.Units;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests.CombinedUnits;
+
+[TestClass]
+public class VolumeTests
+{
+
+    [TestMethod]
+    public void FromVolume01()
+    {
+        double? value = null;
+
+        var v = EngineeringUnits.Volume.From(value, null);
+        Assert.IsNull(v);
+
+    }
+
+    [TestMethod]
+    public void FromVolume02()
+    {
+        double? value = 100;
+        var v = EngineeringUnits.Volume.From(value, VolumeUnit.CubicFoot);
+        var v2 = EngineeringUnits.Volume.From(value, null);
+        Assert.IsNotNull(v);
+        Assert.IsNull(v2);
+
+    }
+}
