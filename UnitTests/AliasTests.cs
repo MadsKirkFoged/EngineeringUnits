481bab2426478727fe9048575d2477db6d9121ee
Max <schmitz.max@gmail.com>
2025-02-11 10:37:26 +0100

    AliasTests

--- UnitTests/AliasTests.cs
+++ UnitTests/AliasTests.cs
@@ -4,7 +4,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace UnitTests;
 
 [TestClass()]
-public class BaseUnitAlias
+public class AliasTests
 {
     [TestMethod()]
     public void SwitchBetweenAlias()
