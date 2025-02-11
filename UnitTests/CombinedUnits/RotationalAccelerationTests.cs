af88a0e577ba9f4ecbc3b9576fd64d3f6c390ca6
Max <schmitz.max@gmail.com>
2025-02-11 11:27:06 +0100

    Adding commented out test for RotationalAcceleration, currently commented out itself

--- UnitTests/CombinedUnits/RotationalAccelerationTests.cs
+++ UnitTests/CombinedUnits/RotationalAccelerationTests.cs
@@ -1,8 +1,19 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using EngineeringUnits;
+using EngineeringUnits.Units;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace UnitTests.CombinedUnits;
 
 [TestClass]
 public class RotationalAccelerationTests
 {
+    //[TestMethod()]
+    //public void Simple()
+    //{
+    //    //Arrange
+    //    var test = new RotationalAcceleration(10, RotationalAccelerationUnit.XXX);
+
+    //    // Assert
+    //    Assert.AreEqual("10 $/kWh", test.ToString());
+    //}
 }
