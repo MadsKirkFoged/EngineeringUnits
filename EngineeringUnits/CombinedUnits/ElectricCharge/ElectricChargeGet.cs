13b9828d93b4db34eab693873b2578d82ec69f3c
Mirijan Ristic <ristic.mirijan@gmail.com>
2025-11-04 10:15:08 +0100

    Run code regeneration

--- EngineeringUnits/CombinedUnits/ElectricCharge/ElectricChargeGet.cs
+++ EngineeringUnits/CombinedUnits/ElectricCharge/ElectricChargeGet.cs
@@ -15,6 +15,38 @@ public partial class ElectricCharge
     /// </summary>
     public double Coulomb => As(ElectricChargeUnit.Coulomb);
     /// <summary>
+    /// Get ElectricCharge in Teracoulomb.
+    /// </summary>
+    public double Teracoulomb => As(ElectricChargeUnit.Teracoulomb);
+    /// <summary>
+    /// Get ElectricCharge in Gigacoulomb.
+    /// </summary>
+    public double Gigacoulomb => As(ElectricChargeUnit.Gigacoulomb);
+    /// <summary>
+    /// Get ElectricCharge in Megacoulomb.
+    /// </summary>
+    public double Megacoulomb => As(ElectricChargeUnit.Megacoulomb);
+    /// <summary>
+    /// Get ElectricCharge in Kilocoulomb.
+    /// </summary>
+    public double Kilocoulomb => As(ElectricChargeUnit.Kilocoulomb);
+    /// <summary>
+    /// Get ElectricCharge in Millicoulomb.
+    /// </summary>
+    public double Millicoulomb => As(ElectricChargeUnit.Millicoulomb);
+    /// <summary>
+    /// Get ElectricCharge in Microcoulomb.
+    /// </summary>
+    public double Microcoulomb => As(ElectricChargeUnit.Microcoulomb);
+    /// <summary>
+    /// Get ElectricCharge in Nanocoulomb.
+    /// </summary>
+    public double Nanocoulomb => As(ElectricChargeUnit.Nanocoulomb);
+    /// <summary>
+    /// Get ElectricCharge in Picocoulomb.
+    /// </summary>
+    public double Picocoulomb => As(ElectricChargeUnit.Picocoulomb);
+    /// <summary>
     /// Get ElectricCharge in AmpereHour.
     /// </summary>
     public double AmpereHour => As(ElectricChargeUnit.AmpereHour);
