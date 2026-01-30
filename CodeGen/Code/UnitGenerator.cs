63de5356f224059e325543d8b10f790fc002351a
Mads Foged <jfogedm@jci.com>
2026-01-30 21:48:20 +0100

    ready to codegen again

--- CodeGen/Code/UnitGenerator.cs
+++ CodeGen/Code/UnitGenerator.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EngineeringUnits;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -15,30 +16,13 @@ internal class UnitGenerator
         List<string> list = ListOfUnitsForDifferentGenerators.GetListOfCombinedUnits();
         foreach (var item in list)
         {
-
-            var unitSystem = UnitReflection.GetSiUnitSystem(item);
-            var listOfUnits = UnitReflection.GetListOfUnits(unitSystem);
-
-            List<string> UnitDimension = [];
-
-            foreach (var rawUnit in listOfUnits)
-            {
-                var unitType = rawUnit.GetType().GetProperty("UnitType")?.GetValue(rawUnit);
-                var count = rawUnit.GetType().GetProperty("Count")?.GetValue(rawUnit);
-                UnitDimension.Add($"BaseunitType.{unitType}, {count}");
-            }
-
-            string UnitDimensionText = $"[UnitDimension({string.Join(", ", UnitDimension)})]";
-
-
-            var sb = Generate().Replace("Variable", $"{item}")
-                               .Replace("UnitDimension", UnitDimensionText);
+            var sb = Generate(item, GetDimList(item));
 
             var projectPathWithUnit = Path.Combine(projectPath, "CombinedUnits", item);
 
             if (!Directory.Exists(projectPathWithUnit))
             {
-                _=Directory.CreateDirectory(projectPathWithUnit);
+                _ = Directory.CreateDirectory(projectPathWithUnit);
             }
 
             File.WriteAllText(Path.Combine(projectPathWithUnit, $"{item}.cs"), sb.ToString());
@@ -48,7 +32,7 @@ internal class UnitGenerator
         foreach (var item in ListOfUnitsForDifferentGenerators.GetListOfBaseUnits())
         {
 
-            var sb = Generate().Replace("Variable", $"{item}");
+            var sb = Generate(item, GetDimList(item));
 
             var projectPathWithUnit = Path.Combine(projectPath, "BaseUnits", item);
             if (!Directory.Exists(projectPathWithUnit))
@@ -61,7 +45,37 @@ internal class UnitGenerator
         }
     }
 
-    public static string Generate()
+    private static string GetDimList(string item)
+    {
+        var unitSystem = UnitReflection.GetSiUnitSystem(item);
+        var listOfUnits = UnitReflection.GetListOfUnits(unitSystem);
+
+        List<string> UnitDimension = [];
+
+        var dims = Enum.GetValues<BaseunitType>()
+                       .Where(k => k != BaseunitType.CombinedUnit)
+                       .ToDictionary(k => k, _ => 0);
+
+        foreach (var rawUnit in listOfUnits)
+        {
+            BaseunitType unitType = (BaseunitType)rawUnit.GetType().GetProperty("UnitType")?.GetValue(rawUnit);
+            int count = (int)rawUnit.GetType().GetProperty("Count")?.GetValue(rawUnit);
+
+            dims[unitType] = count;
+
+            UnitDimension.Add($"BaseunitType.{unitType}, {count}");
+        }
+
+        var parts = Enum.GetValues<BaseunitType>()
+                        .Select(t => $"BaseunitType.{t}, {dims.GetValueOrDefault(t)}");
+
+        var test = $"[UnitDimension({string.Join(", ", parts)})]";
+
+        string UnitDimensionText = $"[UnitDimension({string.Join(", ", UnitDimension)})]";
+        return UnitDimensionText;
+    }
+
+    public static string Generate(string Variable, string DimensionText)
     {
 
         return $$"""
@@ -71,42 +85,42 @@ internal class UnitGenerator
                    namespace EngineeringUnits;
                    
                    // This class is auto-generated, changes to the file will be overwritten!
-                   UnitDimension
-                   public partial class Variable : BaseUnit
+                   {{DimensionText}}
+                   public partial class {{Variable}} : BaseUnit
                    {
-                       public Variable() { }
-                       public Variable(decimal value, VariableUnit selectedUnit) : base(value, selectedUnit.Unit) { }
-                       public Variable(double value, VariableUnit selectedUnit) : base(value, selectedUnit.Unit) { }
-                       public Variable(int value, VariableUnit selectedUnit) : base(value, selectedUnit.Unit) { }
-                       public Variable(UnknownUnit value) : base(value) { }
+                       public {{Variable}}() { }
+                       public {{Variable}}(decimal value, {{Variable}}Unit selectedUnit) : base(value, selectedUnit.Unit) { }
+                       public {{Variable}}(double value, {{Variable}}Unit selectedUnit) : base(value, selectedUnit.Unit) { }
+                       public {{Variable}}(int value, {{Variable}}Unit selectedUnit) : base(value, selectedUnit.Unit) { }
+                       public {{Variable}}(UnknownUnit value) : base(value) { }
                    
-                       public static Variable From(double value, VariableUnit unit) => new(value, unit);
+                       public static {{Variable}} From(double value, {{Variable}}Unit unit) => new(value, unit);
                    
                        [return: NotNullIfNotNull(nameof(value))]
-                       public static Variable? From(double? value, VariableUnit? unit)
+                       public static {{Variable}}? From(double? value, {{Variable}}Unit? unit)
                        {
                            if (value is null || unit is null)
                                return null;
                    
                            return From((double)value, unit);
                        }
-                       public double As(VariableUnit ReturnInThisUnit) => this.GetValueAsDouble(ReturnInThisUnit);
-                       public Variable ToUnit(VariableUnit selectedUnit) => new(this.GetValueAs(selectedUnit.Unit), selectedUnit);
-                       public static Variable Zero => new(0, VariableUnit.SI);
-                       public static Variable NaN => new(double.NaN, VariableUnit.SI);
+                       public double As({{Variable}}Unit ReturnInThisUnit) => this.GetValueAsDouble(ReturnInThisUnit);
+                       public {{Variable}} ToUnit({{Variable}}Unit selectedUnit) => new(this.GetValueAs(selectedUnit.Unit), selectedUnit);
+                       public static {{Variable}} Zero => new(0, {{Variable}}Unit.SI);
+                       public static {{Variable}} NaN => new(double.NaN, {{Variable}}Unit.SI);
                    
                        [return: NotNullIfNotNull(nameof(Unit))]
-                       public static implicit operator Variable?(UnknownUnit? Unit)
+                       public static implicit operator {{Variable}}?(UnknownUnit? Unit)
                        {
                            if (Unit is null)
                                return null;
                    
-                           GuardAgainst.DifferentUnits(Unit, VariableUnit.SI);
+                           GuardAgainst.DifferentUnits(Unit, {{Variable}}Unit.SI);
                            return new(Unit);
                        }
                    
                        [return: NotNullIfNotNull(nameof(Unit))]
-                       public static implicit operator UnknownUnit?(Variable? Unit)
+                       public static implicit operator UnknownUnit?({{Variable}}? Unit)
                        {
                            if (Unit is null)
                                return null;
@@ -114,7 +128,7 @@ internal class UnitGenerator
                            return new(Unit);
                        }
                    
-                       public override string? GetStandardSymbol(UnitSystem _unit) => GetStandardSymbol<VariableUnit>(_unit);
+                       public override string? GetStandardSymbol(UnitSystem _unit) => GetStandardSymbol<{{Variable}}Unit>(_unit);
                    }
                    
                    """;
--- EngineeringUnits/UnitDimensionAttribute.cs
+++ EngineeringUnits/UnitDimensionAttribute.cs
@@ -20,5 +20,11 @@ namespace EngineeringUnits
             Types = new[] { t1, t2 };
             Exponents = new[] { e1, e2 };
         }
+
+        public UnitDimensionAttribute(BaseunitType t1, int e1, BaseunitType t2, int e2, BaseunitType t3, int e3, BaseunitType t4, int e4, BaseunitType t5, int e5, BaseunitType t6, int e6, BaseunitType t7, int e7, BaseunitType t8, int e8)
+        {
+            Types = new[] { t1, t2, t3, t4, t5, t6, t7 ,t8 };
+            Exponents = new[] { e1, e2, e3, e4, e5, e6, e7, e8 };
+        }
     }
 }
